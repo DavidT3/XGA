cf3442397fa0d39bdc51056ca71a1fda42914c20
dt237 <david.turner@sussex.ac.uk>
2020-04-29 21:47:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import versioneer
+
+# TODO Add something here that checks for the presence of SAS environment variables
+
+setup(name='xga',
+      packages=find_packages(),
+      version=versioneer.get_version(),
+      description='Python package to easily generate and analyse XMM data products',
+      author='David Turner',
+      author_email='david.turner@sussex.ac.uk',
+      url='http://github.com/DavidT3/XGA',
+      setup_requires=[],
+      install_requires=["astropy", "numpy", "tqdm", "regions=>0.3", "xspec"],
+      include_package_data=False,
+      python_requires='>=3')
+
