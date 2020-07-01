31dfe9682379bb1fd0ff33894ea3423363a357c0
dt237 <david.turner@sussex.ac.uk>
2020-07-01 19:16:59 +0100

    Added inits to include folders in the setup.py install (hopefully)

---
+++ xga/xspec_scripts/__init__.py
@@ -0,0 +1,3 @@
+#  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 01/07/2020, 19:16. Copyright (c) David J Turner
+
