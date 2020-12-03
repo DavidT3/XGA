2fb178d85ac6112e1bcaeff39b675ec0a9a76f02
DavidT3 <dt237@sussex.ac.uk>
2020-12-03 14:21:53 +0000

    Added the new 'relations' package, which is mentioned in issue #281 and issue #282. Common cluster scaling relations (along with other types of object if requested), will live here, as well as the ability to fit and make new scaling relations.

---
+++ xga/relations/fit.py
@@ -0,0 +1,8 @@
+#  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 03/12/2020, 14:21. Copyright (c) David J Turner
+
+# TODO Add some different fit methods for scaling relations here, see issue #282 on the GitHub repo
+
+
+
+
