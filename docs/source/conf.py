8b0d07304a0dbef3e19c0331fd9d462f0a5d50a7
DavidT3 <dt237@sussex.ac.uk>
2021-01-05 20:05:43 +0000

    Copied logos to the _static folder and added that information to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,16 +106,18 @@ autodoc_typehints = 'none'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = "https://github.com/DavidT3/XGA/blob/master/xga/files/long_xga_logo.png"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "long_xga_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
 
 autodoc_mock_imports = ["fitsio", 'regions', 'corner', 'emcee', 'abel']
 
