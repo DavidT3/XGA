d40d8923c5889aac39f51dec660c8a79d71045f3
DavidT3 <dt237@sussex.ac.uk>
2021-01-05 19:59:12 +0000

    This should close issue #321, by adding a logo to my documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,6 +106,11 @@ autodoc_typehints = 'none'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = "https://github.com/DavidT3/XGA/blob/master/xga/files/long_xga_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
