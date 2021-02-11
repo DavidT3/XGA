5971dcea2f4fc150611ee0bec4b6e61c7793b140
DavidT3 <dt237@sussex.ac.uk>
2021-02-11 12:30:27 +0000

    Corrected bugs where temperature profiles weren't being generated for individual clusters passed in

--- xga/products/__init__.py
+++ xga/products/__init__.py
@@ -1,9 +1,10 @@
 #  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 05/11/2020, 13:24. Copyright (c) David J Turner
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 11/02/2021, 12:30. Copyright (c) David J Turner
 
 from .base import BaseProduct, BaseAggregateProduct, BaseProfile1D, BaseAggregateProfile1D
 from .misc import EventList
 from .phot import Image, ExpMap, RateMap, PSF, PSFGrid
+from .relation import ScalingRelation
 from .spec import Spectrum, AnnularSpectra
 
 # Defining a dictionary to map from string product names to their associated classes
