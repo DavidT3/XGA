84193f05a63a78436d90195ace22b1dae14569a7
DavidT3 <dt237@sussex.ac.uk>
2021-02-15 16:33:33 +0000

    Changed the default number of data realisations generated for error propagation from spectral normalisation profile in temperature and density

--- xga/sourcetools/temperature.py
+++ xga/sourcetools/temperature.py
@@ -1,5 +1,5 @@
 #  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 11/02/2021, 12:30. Copyright (c) David J Turner
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 15/02/2021, 16:33. Copyright (c) David J Turner
 
 from typing import Tuple, Union, List
 from warnings import warn
@@ -340,7 +340,7 @@ def onion_deproj_temp_prof(sources: Union[GalaxyCluster, ClusterSample], outer_r
                            psf_bins: int = 4, psf_algo: str = "rl", psf_iter: int = 15, allow_negative: bool = False,
                            exp_corr: bool = True, group_spec: bool = True, min_counts: int = 5, min_sn: float = None,
                            over_sample: float = None, one_rmf: bool = True, link_norm: bool = True,
-                           abund_table: str = "angr", num_data_real: int = 100, sigma: int = 2,
+                           abund_table: str = "angr", num_data_real: int = 300, sigma: int = 2,
                            num_cores: int = NUM_CORES):
     """
     This function will generate de-projected, three-dimensional, gas temperature profiles of galaxy clusters using
