e86d63239013e88215f02620940ad930853b6f50
DavidT3 <dt237@sussex.ac.uk>
2021-03-08 14:01:38 +0000

    Added the full Vikhlinin temperature model, though haven't done the analytical derivative yet as I have gotten VERY bored of this now

--- xga/models/temperature.py
+++ xga/models/temperature.py
@@ -1,5 +1,5 @@
 #  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 08/03/2021, 13:20. Copyright (c) David J Turner
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 08/03/2021, 14:01. Copyright (c) David J Turner
 
 from typing import Union
 
@@ -72,8 +72,8 @@ class SimpleVikhlininTemperature1D(BaseModel1D):
                          'Simplified Vikhlinin Profile', nice_pars, 'Gas Temperature', info_dict)
 
     @staticmethod
-    def model(x: Quantity, r_cool: Quantity, a_cool: Quantity, t_min: Quantity, t_zero: Quantity,
-              r_transition: Quantity, c_power: Quantity) -> Quantity:
+    def model(x: Quantity, r_cool: Quantity, a_cool: Quantity, t_min: Quantity, t_zero: Quantity,  r_tran: Quantity,
+              c_power: Quantity) -> Quantity:
         """
         The model function for the simplified Vikhlinin temperature profile.
 
@@ -82,14 +82,14 @@ class SimpleVikhlininTemperature1D(BaseModel1D):
         :param Quantity a_cool: Power law parameter for the cooler core region.
         :param Quantity t_min: A minimum temperature parameter for the model.
         :param Quantity t_zero: A normalising temperature parameter for the model.
-        :param Quantity r_transition: The radius of the transition region of this broken power law model.
+        :param Quantity r_tran: The radius of the transition region of this broken power law model.
         :param Quantity c_power: The power law index for the part of the model which describes the outer region of
             the cluster.
         :return: The y values corresponding to the input x values.
         :rtype: Quantity
         """
         cool_expr = ((t_min / t_zero) + np.power(x / r_cool, a_cool)) / (1 + np.power(x / r_cool, a_cool))
-        out_expr = 1 / np.power(1 + np.power(x / r_transition, 2), c_power / 2)
+        out_expr = 1 / np.power(1 + np.power(x / r_tran, 2), c_power / 2)
 
         return t_zero * cool_expr * out_expr
 
@@ -110,6 +110,96 @@ class SimpleVikhlininTemperature1D(BaseModel1D):
         p2 = x*(x**2+r_t**2)*(((x/r_c)**a) + 1)**2
 
         return p1/p2
+
+
+class VikhlininTemperature1D(BaseModel1D):
+    """
+    An XGA model implementation of the full version of Vikhlinin's temperature model. This is for the
+    description of 3D temperature profiles of galaxy clusters.
+    """
+    def __init__(self, x_unit: Union[str, Unit] = 'kpc', y_unit: Union[str, Unit] = Unit('keV')):
+        # If a string representation of a unit was passed then we make it an astropy unit
+        if isinstance(x_unit, str):
+            x_unit = Unit(x_unit)
+        if isinstance(y_unit, str):
+            y_unit = Unit(y_unit)
+
+        poss_y_units = [Unit('keV'), Unit('K')]
+        y_convertible = [u.is_equivalent(y_unit) for u in poss_y_units]
+        if not any(y_convertible):
+            allowed = ", ".join([u.to_string() for u in poss_y_units])
+            raise UnitConversionError("{p} is not convertible to any of the allowed units; "
+                                      "{a}".format(p=y_unit.to_string(), a=allowed))
+        else:
+            yu_ind = y_convertible.index(True)
+
+        poss_x_units = [kpc, deg, r200, r500, r2500]
+        x_convertible = [u.is_equivalent(x_unit) for u in poss_x_units]
+        if not any(x_convertible):
+            allowed = ", ".join([u.to_string() for u in poss_x_units])
+            raise UnitConversionError("{p} is not convertible to any of the allowed units; "
+                                      "{a}".format(p=x_unit.to_string(), a=allowed))
+        else:
+            xu_ind = x_convertible.index(True)
+
+        r_cool_starts = [Quantity(100, 'kpc'), Quantity(0.2, 'deg'), Quantity(0.05, r200), Quantity(0.1, r500),
+                         Quantity(0.5, r2500)]
+        r_tran_starts = [Quantity(400, 'kpc'), Quantity(0.6, 'deg'), Quantity(0.2, r200), Quantity(0.4, r500),
+                         Quantity(1, r2500)]
+        t_min_starts = [Quantity(1, 'keV'), (Quantity(1, 'keV')/k_B).to('K')]
+        t_zero_starts = [Quantity(5, 'keV'), (Quantity(5, 'keV')/k_B).to('K')]
+
+        start_pars = [r_cool_starts[xu_ind], Quantity(1, ''), t_min_starts[yu_ind], t_zero_starts[yu_ind],
+                      r_tran_starts[xu_ind], Quantity(1, ''), Quantity(1, ''), Quantity(1, '')]
+
+        r_priors = [{'prior': Quantity([0, 2000], 'kpc'), 'type': 'uniform'},
+                    {'prior': Quantity([0, 2], 'deg'), 'type': 'uniform'},
+                    {'prior': Quantity([0, 1], r200), 'type': 'uniform'},
+                    {'prior': Quantity([0, 1.5], r500), 'type': 'uniform'},
+                    {'prior': Quantity([0, 3], r2500), 'type': 'uniform'}]
+        t_priors = [{'prior': Quantity([0, 15], 'keV'), 'type': 'uniform'},
+                    {'prior': (Quantity([0, 15], 'keV')/k_B).to('K'), 'type': 'uniform'}]
+
+        priors = [r_priors[xu_ind], Quantity([-10, 10]), t_priors[yu_ind], t_priors[yu_ind], r_priors[xu_ind],
+                  Quantity([-10, 10]), Quantity([-10, 10]), Quantity([-10, 10])]
+
+        nice_pars = [r"R$_{\rm{cool}}$", r"a$_{\rm{cool}}$", r"T$_{\rm{min}}$", "T$_{0}$", r"R$_{\rm{T}}$", "a", "b",
+                     "c"]
+        info_dict = {'author': 'Vikhlinin et al.', 'year': 2005,
+                     'reference': 'https://doi.org/10.1086/500288',
+                     'general': "The full form of Vikhlinin's temperature model, describes a\n"
+                                "cluster temperature profile from the core to the outskirts."}
+        super().__init__(x_unit, y_unit, start_pars, priors, 'vikhlinin_temp',
+                         'Vikhlinin Profile', nice_pars, 'Gas Temperature', info_dict)
+
+    @staticmethod
+    def model(x: Quantity, r_cool: Quantity, a_cool: Quantity, t_min: Quantity, t_zero: Quantity, r_tran: Quantity,
+              a_power: Quantity, b_power: Quantity, c_power: Quantity) -> Quantity:
+        """
+        The model function for the full Vikhlinin temperature profile.
+
+        :param Quantity x: The radii to calculate y values for.
+        :param float r_cool: Parameter describing the radius of the cooling region (I THINK - NOT CERTAIN YET).
+        :param float a_cool: Power law parameter for the cooling region (I THINK - NOT CERTAIN YET).
+        :param float t_min: A minimum temperature parameter for the model (I THINK - NOT CERTAIN YET).
+        :param float t_zero: A normalising temperature parameter for the model (I THINK - NOT CERTAIN YET).
+        :param float r_tran: The radius of the transition region of this broken power law model.
+        :param float a_power: The first power law index.
+        :param float b_power: The second power law index.
+        :param float c_power: the third power law index.
+        :return: The y values corresponding to the input x values.
+        :rtype: Quantity
+        """
+        power_rad_ratio = np.power((x / r_cool), a_cool)
+        # The rest of the model expression
+        t_cool = (power_rad_ratio + (t_min / t_zero)) / (power_rad_ratio + 1)
+
+        # The ratio of the input radius (or radii) to the transition radius
+        rad_ratio = x / r_tran
+        t_outer = np.power(rad_ratio, -a_power) / np.power((1 + np.power(rad_ratio, b_power)),
+                                                           (c_power / b_power))
+
+        return t_zero * t_cool * t_outer
 
 
 def central_region(r_values: Union[np.ndarray, float], r_cool: float, a_cool: float, t_min: float, t_zero: float) \
