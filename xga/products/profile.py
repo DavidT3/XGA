85fe934aa337331aeb2b18d488919d395dcfd9cd
DavidT3 <dt237@sussex.ac.uk>
2021-05-25 12:24:03 +0100

    Fixed an annoying bug where all mass profiles were incorrectly being flagged as having negative masses in them

--- xga/products/profile.py
+++ xga/products/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of XMM: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 14/05/2021, 16:25. Copyright (c) David J Turner
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 25/05/2021, 12:24. Copyright (c) David J Turner
 from copy import copy
 from typing import Tuple, Union, List
 from warnings import warn
@@ -482,7 +482,7 @@ class GasDensity3D(BaseProfile1D):
         low_mass = np.percentile(mass_dist, 50 - (conf_level/2)).value
         gas_mass = Quantity([med_mass, med_mass-low_mass, upp_mass-med_mass], mass_dist.unit)
 
-        if gas_mass[0] < 0:
+        if np.any(gas_mass[0] < 0):
             raise ValueError("A gas mass of less than zero has been measured, which is not physical.")
 
         # This method means that a change has happened to the model, so it should be re-saved
@@ -1231,7 +1231,7 @@ class HydrostaticMass(BaseProfile1D):
 
         mass_res = Quantity(np.array([mass_med.value, mass_lower.value, mass_upper.value]), mass_dist.unit)
 
-        if mass_res[0] < 0:
+        if np.any(mass_res[0] < 0):
             raise ValueError("A mass of less than zero has been measured, which is not physical.")
 
         return mass_res, mass_dist
