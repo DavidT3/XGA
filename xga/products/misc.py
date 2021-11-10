fc3e6e13479f26a5ff6f01d7975f63707c8c725a
DavidT3 <dt237@sussex.ac.uk>
2021-11-10 10:31:34 +0000

    Altered docstring of EventList product for issue #676

--- xga/products/misc.py
+++ xga/products/misc.py
@@ -6,8 +6,21 @@ from . import BaseProduct
 
 
 class EventList(BaseProduct):
+    """
+    A product class for event lists, largely just used as a means to store information about the event list.
+
+    :param str path: The path to where the event list file SHOULD be located.
+    :param str obs_id: The ObsID related to the event list being declared.
+    :param str instrument: The instrument related to the event list being declared.
+    :param str stdout_str: The stdout from calling the terminal command.
+    :param str stderr_str: The stderr from calling the terminal command.
+    :param str gen_cmd: The command used to generate the event list.
+    """
     def __init__(self, path: str, obs_id: str, instrument: str, stdout_str: str, stderr_str: str,
                  gen_cmd: str):
+        """
+        The init method of the EventList class.
+        """
         super().__init__(path, obs_id, instrument, stdout_str, stderr_str, gen_cmd)
         self._prod_type = "events"
 
