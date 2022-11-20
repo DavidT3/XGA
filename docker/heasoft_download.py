e0ddfa225007d6b98875a7b3a2895b2ded41d40d
DavidT3 <dt237@sussex.ac.uk>
2022-11-19 20:09:33 -0500

    Finished the heasoft download script, for issue #521

--- docker/heasoft_download.py
+++ docker/heasoft_download.py
@@ -1,11 +1,22 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 19/11/2022, 20:00. Copyright (c) The Contributors
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 19/11/2022, 20:09. Copyright (c) The Contributors
 
 import shutil
 
 import requests
 
-with requests.get('https://heasarc.gsfc.nasa.gov/cgi-bin/Tools/tarit/tarit.pl?mode=download&arch=src&src_pc_linux_centos=Y&src_other_specify=&general=attitude&general=heasarc&general=heasptools&general=heatools&general=heagen&xanadu=xspec', stream=True) as streamo:
-    with open('testo.tar.gz', 'wb') as writo:
-        shutil.copyfileobj(streamo.raw, writo)
+# This is a very cheesy script which uses the HEASoft source-code-assembly script to download a minimal
+#  version of HEASoft with XSPEC + the base high energy tools. I could download the whole package using
+#  wget and a normal URL in the dockerfile, but I'm really trying to minimise the size of the final image so
+#  this is the solution I ended up with.
+heasoft_url = 'https://heasarc.gsfc.nasa.gov/cgi-bin/Tools/tarit/tarit.pl?mode=download&arch=src&src_pc_linux_' \
+              'centos=Y&src_other_specify=&general=attitude&general=heasarc&general=heasptools&general=heatools&' \
+              'general=heagen&xanadu=xspec'
 
+#  Opens a get connection to that URL, and the data are streamed (so it starts to write the file before
+#  everything has been downloaded into memory)
+with requests.get(heasoft_url, stream=True) as streamo:
+    # The download is written to this compressed tar, once decompressed the folder name will have the version
+    #  of HEASoft in it
+    with open('heasoft.tar.gz', 'wb') as writo:
+        shutil.copyfileobj(streamo.raw, writo)
