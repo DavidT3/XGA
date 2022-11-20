253d2b2bf3b0480844d35fae9f4d5668406296dc
DavidT3 <dt237@sussex.ac.uk>
2022-11-19 21:01:55 -0500

    Changed heasoft_download.py to download the Ubuntu flavour of HEASoft source code rather than the CENTOS version I was playing with before

--- docker/heasoft_download.py
+++ docker/heasoft_download.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (david.turner@sussex.ac.uk) 19/11/2022, 20:09. Copyright (c) The Contributors
+#  Last modified by David J Turner (david.turner@sussex.ac.uk) 19/11/2022, 21:01. Copyright (c) The Contributors
 
 import shutil
 
@@ -10,7 +10,7 @@ import requests
 #  wget and a normal URL in the dockerfile, but I'm really trying to minimise the size of the final image so
 #  this is the solution I ended up with.
 heasoft_url = 'https://heasarc.gsfc.nasa.gov/cgi-bin/Tools/tarit/tarit.pl?mode=download&arch=src&src_pc_linux_' \
-              'centos=Y&src_other_specify=&general=attitude&general=heasarc&general=heasptools&general=heatools&' \
+              'ubuntu=Y&src_other_specify=&general=attitude&general=heasarc&general=heasptools&general=heatools&' \
               'general=heagen&xanadu=xspec'
 
 #  Opens a get connection to that URL, and the data are streamed (so it starts to write the file before
