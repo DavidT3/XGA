4c111530b4eb7d5b9570391e53695a3c4c5882b2
DavidT3 <dt237@sussex.ac.uk>
2023-03-14 16:06:41 -0400

    Hopefully fixed the header problem. For issue #745

--- xga/products/spec.py
+++ xga/products/spec.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 14/03/2023, 16:06. Copyright (c) The Contributors
 
 
 import os
@@ -200,6 +200,13 @@ class Spectrum(BaseProduct):
         if which_spec == "main" and self.usable:
             # Currently having to use astropy's fits interface, I don't really want to because of risk of segfaults
             with fits.open(self._path, mode='update') as spec_fits:
+                # I delete the headers first, as I've found issues with XSPEC not being able to read the
+                #  path if the SAS version I'm using adds entries for the headers before I do. See issue #745
+                del spec_fits["SPECTRUM"].header["RESPFILE"]
+                del spec_fits["SPECTRUM"].header["ANCRFILE"]
+                del spec_fits["SPECTRUM"].header["BACKFILE"]
+
+                # This writes the new response file paths to the headers.
                 spec_fits["SPECTRUM"].header["RESPFILE"] = self._rmf
                 spec_fits["SPECTRUM"].header["ANCRFILE"] = self._arf
                 spec_fits["SPECTRUM"].header["BACKFILE"] = self._back_spec
