2b02d9a45f15842e8ce51bfc3ecd81e88b9b7394
DavidT3 <dt237@sussex.ac.uk>
2023-04-13 11:18:58 -0400

    Added a LT file to tools.clusters - this will house the luminosity temperature pipeline implementations. I envisage two of them, the first for detected clusters that you might think you can measure a temperature for, the other for undetected clusters that need to use some middle-man relation to get to temperature. For issue #954

---
+++ xga/tools/clusters/LT.py
@@ -0,0 +1,4 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (turne540@msu.edu) 13/04/2023, 11:18. Copyright (c) The Contributors
+
+
