dda0d1f9cb2480d636a0bc54efd338f2d35b3406
DavidT3 <dt237@sussex.ac.uk>
2023-04-13 11:15:58 -0400

    Added a 'tools' sub-package, with a specific clusters sub-package to that. For issue #953

---
+++ xga/tools/__init__.py
@@ -0,0 +1,2 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (turne540@msu.edu) 13/04/2023, 11:15. Copyright (c) The Contributors
