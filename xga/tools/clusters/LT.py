bbe196b69947c88498c6d8cf7ceca4487d89ea81
DavidT3 <dt237@sussex.ac.uk>
2023-04-17 12:04:30 -0400

    In theory I now have a functional LT pipeline, though it isn't docstringed, doesn't have all the options that I want to give the user, and can't handle spectral generation failure yet. For issue #954

--- xga/tools/clusters/LT.py
+++ xga/tools/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 15/04/2023, 12:11. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 17/04/2023, 12:04. Copyright (c) The Contributors
 import numpy as np
 import pandas as pd
 from astropy.cosmology import Cosmology
@@ -17,7 +17,8 @@ LT_REQUIRED_COLS = ['ra', 'dec', 'name', 'redshift']
 
 def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Quantity, convergence_frac: float = 0.1,
                                     min_iter: int = 3, max_iter: int = 10, rad_temp_rel: ScalingRelation = arnaud_r500,
-                                    cosmo: Cosmology = DEFAULT_COSMO, num_cores: int = NUM_CORES):
+                                    cosmo: Cosmology = DEFAULT_COSMO, timeout: Quantity = Quantity(1, 'hr'),
+                                    num_cores: int = NUM_CORES):
 
     # I want the sample to be passed in as a DataFrame, so I can easily extract the information I need
     if not isinstance(sample_data, pd.DataFrame):
@@ -62,24 +63,42 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
     iter_num = 0
 
     # Set up the ClusterSample to be used for this process (I did consider setting up a new one each time but that
-    #  adds overhead and I think that this way should work fine).
+    #  adds overhead, and I think that this way should work fine).
     samp = ClusterSample(sample_data['ra'].values, sample_data['dec'].values, sample_data['redshift'].values,
-                         sample_data['name'].values, r500=start_aperture, use_peak=False, clean_obs_threshold=0.7)
+                         sample_data['name'].values, r500=start_aperture, use_peak=False, clean_obs_threshold=0.7,
+                         load_fits=True)
 
     # This is a boolean array of whether the current radius has been accepted or not - starts off False
     acc_rad = np.full(len(samp), False)
+
+    # Here we keep track of the clusters which have had some sort of failure during the iterative process
+    fail_names = []
 
     # This while loop (never thought I'd be using one of them in XGA!) will keep going either until all radii have been
     #  accepted OR until we reach the maximum number  of iterations
     while acc_rad.sum() != len(samp) or iter_num < max_iter:
 
         # We generate and fit spectra for the current value of R500
-        single_temp_apec(samp, samp.r500, one_rmf=False, num_cores=num_cores)
+        single_temp_apec(samp, samp.r500, one_rmf=False, num_cores=num_cores, timeout = timeout)
 
         # Just reading out the temperatures, not the uncertainties at the moment
-        txs = samp.Tx(quality_checks=False)[:, 0]
+        txs = samp.Tx(samp.r500, quality_checks=False)[:, 0]
+
         # This uses the scaling relation to predict R500 from the measured temperatures
         pr_rs = rad_temp_rel.predict(txs)
+
+        # It is possible that some of these radius entries are going to be NaN - the result of NaN temperature values
+        #  passed through the 'predict' method of the scaling relation. As such we identify any NaN results, flag
+        #  those clusters by storing their name in the 'fail_names' list, and replace the NaN results with the last
+        #  value for those clusters.
+        bad_pr_rs = np.where(np.isnan(pr_rs))[0]
+        pr_rs[bad_pr_rs] = samp.r500[bad_pr_rs]
+        fail_names += list(samp.names[bad_pr_rs])
+        # I am also actually going to remove the clusters with NaN results from the sample - if the NaN was caused
+        #  by something like a fit not converging then it's going to keep trying over and over again and that could
+        #  slow everything down.
+        for name in samp.names[bad_pr_rs]:
+            del samp[name]
 
         # The basis of this method is that we measure a temperature, starting in some user-specified fixed aperture,
         #  and then use that to predict an overdensity radius (something far more useful than a fixed aperture). This
@@ -88,17 +107,24 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
         #  allowed to just accept the first result
         rad_rat = pr_rs / samp.r500
 
-        # Make a copy of the new R500s inferred from the scaling relation - this will be modified by replacing new
-        new_rads = pr_rs.copy()
+        # Make a copy of the currently set radius values from the sample - these will then be modified with the
+        #  new predicted values if the particular cluster's radius isn't already considered 'accepted' - i.e. it
+        #  reached the required convergence in a previous iteration
+        new_rads = samp.r500.copy()
 
-        # We need to be checking to see if there are some
-        if iter_num > min_iter:
-            acc_rad *= (rad_rat > 1 - convergence_frac) & (rad_rat < 1 + convergence_frac)
+        # The clusters which DON'T have previously accepted radii have their radii updated from those predicted from
+        #  temperature
+        new_rads[~acc_rad] = pr_rs[~acc_rad]
 
         # Use the new radius value inferred from the temperature + scaling relation and add it to the ClusterSample (or
         #  just re-adding the same value as is already here if that radius has converged and been accepted).
-        samp.r500 = pr_rs[~acc_rad]
+        samp.r500 = new_rads
 
+        # If there have been enough iterations, then we need to start checking whether any of the radii have
+        #  converged to within the user-specified fraction. If they have then we accept them and those radii won't
+        #  be changed the next time around.
+        if iter_num > min_iter:
+            acc_rad *= (rad_rat > (1 - convergence_frac)) & (rad_rat < (1 + convergence_frac))
         # Got to increment the counter otherwise the while loop may go on and on forever :O
         iter_num += 1
 
