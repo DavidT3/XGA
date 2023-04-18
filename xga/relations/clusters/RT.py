1371a4f33f80e6d861f3457739c8ae00584d18b2
DavidT3 <dt237@sussex.ac.uk>
2023-04-17 21:04:58 -0400

    Added dimensionless hubble info to the RT scaling relations. For issue #959

--- xga/relations/clusters/RT.py
+++ xga/relations/clusters/RT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 17/04/2023, 20:34. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 17/04/2023, 21:04. Copyright (c) The Contributors
 
 import numpy as np
 from astropy.units import Quantity
@@ -11,34 +11,34 @@ from ...products.relation import ScalingRelation
 arnaud_r200 = ScalingRelation(np.array([0.57, 1674]), np.array([0.02, 23]), power_law, Quantity(5, 'keV'),
                               Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{200}$", x_lims=Quantity([1, 12], 'keV'),
                               relation_name=r'R$_{200}$-Temperature', relation_author='Arnaud et al.',
-                              relation_year='2005', relation_doi='10.1051/0004-6361:20052856')
+                              relation_year='2005', relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
 
 arnaud_r500 = ScalingRelation(np.array([0.57, 1104]), np.array([0.02, 13]), power_law, Quantity(5, 'keV'),
                               Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{500}$", x_lims=Quantity([1, 12], 'keV'),
                               relation_name=r'R$_{500}$-Temperature', relation_author='Arnaud et al.',
-                              relation_year='2005', relation_doi='10.1051/0004-6361:20052856')
+                              relation_year='2005', relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
 
 arnaud_r2500 = ScalingRelation(np.array([0.56, 491]), np.array([0.02, 4]), power_law, Quantity(5, 'keV'),
                                Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{2500}$", x_lims=Quantity([1, 12], 'keV'),
                                relation_name=r'R$_{2500}$-Temperature', relation_author='Arnaud et al.',
-                               relation_year='2005', relation_doi='10.1051/0004-6361:20052856')
+                               relation_year='2005', relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
 
 # These are equivelant relations specifically measured from the hot clusters (T > 3.5keV) in their sample
 arnaud_r200_hot = ScalingRelation(np.array([0.5, 1714]), np.array([0.05, 30]), power_law, Quantity(5, 'keV'),
                                   Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{200}$", x_lims=Quantity([1, 12], 'keV'),
                                   relation_name=r'T$_{\rm{x}}>3.5$keV R$_{200}$-Temperature',
                                   relation_author='Arnaud et al.', relation_year='2005',
-                                  relation_doi='10.1051/0004-6361:20052856')
+                                  relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
 
 arnaud_r500_hot = ScalingRelation(np.array([0.5, 1129]), np.array([0.05, 17]), power_law, Quantity(5, 'keV'),
                                   Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{500}$", x_lims=Quantity([1, 12], 'keV'),
                                   relation_name=r'T$_{\rm{x}}>3.5$keV R$_{500}$-Temperature',
                                   relation_author='Arnaud et al.', relation_year='2005',
-                                  relation_doi='10.1051/0004-6361:20052856')
+                                  relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
 
 arnaud_r2500_hot = ScalingRelation(np.array([0.5, 500]), np.array([0.03, 5]), power_law, Quantity(5, 'keV'),
                                    Quantity(1, 'kpc'), r"T$_{\rm{x}}$", "E(z)R$_{2500}$",
                                    x_lims=Quantity([1, 12], 'keV'),
                                    relation_name=r'Overdensity T$_{\rm{x}}>3.5$keV R$_{2500}$-Temperature',
                                    relation_author='Arnaud et al.', relation_year='2005',
-                                   relation_doi='10.1051/0004-6361:20052856')
+                                   relation_doi='10.1051/0004-6361:20052856', dim_hubb_ind=1)
