8c432d5e0cd21e7260401f2b6c70de4866b61aa6
DavidT3 <dt237@sussex.ac.uk>
2023-04-22 16:14:10 -0400

    Corrected an issue where more and more 'pattern{}' flags were being added to the extra name information for the LC files. Seems to have cleared up the generation issues. For issue #970

--- xga/sas/lightcurve.py
+++ xga/sas/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 01:09. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 16:14. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -192,14 +192,14 @@ def _lc_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, Qu
 
             # Some settings depend on the instrument
             if "pn" in inst:
-                extra_name = "_pattern{p}".format(p=pn_patt_name) + extra_name
+                lc_storage_name = "_pattern{p}".format(p=pn_patt_name) + lc_storage_name
                 expr = "expression='#XMMEA_EP && (PATTERN {p}) && (FLAG .eq. 0) && (PI in [{l}:{u}]) && " \
                        "{s}'".format(s=reg, p=pn_patt, l=lo_chan, u=hi_chan)
                 b_expr = "expression='#XMMEA_EP && (PATTERN {p}) && (FLAG .eq. 0) && (PI in [{l}:{u}]) && " \
                          "{s}'".format(s=b_reg, p=pn_patt, l=lo_chan, u=hi_chan)
 
             elif "mos" in inst:
-                extra_name = "_pattern{p}".format(p=mos_patt_name) + extra_name
+                lc_storage_name = "_pattern{p}".format(p=mos_patt_name) + lc_storage_name
 
                 expr = "expression='#XMMEA_EM && (PATTERN {p}) && (FLAG .eq. 0) && (PI in [{l}:{u}]) && " \
                        "{s}'".format(s=reg, p=mos_patt, l=lo_chan, u=hi_chan)
