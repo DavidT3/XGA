a54c005202139a44193637c5f51bc819d8e63489
DavidT3 <dt237@sussex.ac.uk>
2023-04-22 01:09:40 -0400

    Trying to fix circular import so my docs will work again.

--- xga/sas/__init__.py
+++ xga/sas/__init__.py
@@ -1,9 +1,6 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 01:01. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 01:09. Copyright (c) The Contributors
 
 from .lightcurve import *
 from .phot import *
 from .spec import *
-
-
-
--- xga/sas/lightcurve.py
+++ xga/sas/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 00:51. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 22/04/2023, 01:09. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -8,8 +8,8 @@ from typing import Union
 import numpy as np
 from astropy.units import Quantity, UnitConversionError
 
-from . import sas_call
 from ._common import region_setup, check_pattern
+from .run import sas_call
 from .. import OUTPUT, NUM_CORES
 from ..samples.base import BaseSample
 from ..sources import BaseSource
