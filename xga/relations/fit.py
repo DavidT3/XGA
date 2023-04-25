8c85f10bbb8e54e9ea0a420799fb0200b549e8ee
DavidT3 <dt237@sussex.ac.uk>
2023-04-25 15:33:57 -0400

    Fixed a bug where I was checking that one variable was not None but testing anothers length. For issue #983

--- xga/relations/fit.py
+++ xga/relations/fit.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 25/04/2023, 11:52. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 25/04/2023, 15:33. Copyright (c) The Contributors
 import inspect
 from types import FunctionType
 from typing import Tuple, Union
@@ -54,7 +54,7 @@ def _fit_initialise(y_values: Quantity, y_errs: Quantity, x_values: Quantity, x_
     elif point_names is not None and len(point_names) != len(x_values):
         ValueError("The 'point_names' argument is a different length ({p}) to the input data "
                    "({d}).".format(p=len(point_names), d=len(x_values)))
-    elif point_names is not None and len(third_dim) != len(x_values):
+    elif third_dim is not None and len(third_dim) != len(x_values):
         ValueError("The 'third_dim' argument is a different length ({p}) to the input data "
                    "({d}).".format(p=len(third_dim), d=len(x_values)))
     elif y_errs.unit != y_values.unit:
