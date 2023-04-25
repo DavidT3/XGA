296809b18d18d80fc2f28283bc428415699ee0f0
DavidT3 <dt237@sussex.ac.uk>
2023-04-25 09:26:28 -0400

    Hopefully fixed a bug I introduced where I was checking to see whether a name extracted from an error was present in a sample, and I wasn't checking right. For issue #984

--- xga/tools/clusters/LT.py
+++ xga/tools/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 24/04/2023, 22:33. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 25/04/2023, 09:26. Copyright (c) The Contributors
 from typing import Tuple
 from warnings import warn
 
@@ -211,11 +211,11 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
             # Then we can cycle through those names and delete the sources from the sample (throwing a hopefully
             #  useful warning as well).
             for bad_name in bad_gen:
-                if bad_name in samp:
+                if bad_name in samp.names:
                     del samp[bad_name]
                 else:
                     warn("SASGenerationError parsing has recovered a string that is not a source name ({}), a "
-                         "problem may not have been removed from the sample.".format(bad_name))
+                         "problem may not have been removed from the sample.".format(bad_name), stacklevel=2)
 
             warn("Some sources ({}) have been removed because of spectrum generation "
                  "failures.".format(', '.join(bad_gen)), stacklevel=2)
