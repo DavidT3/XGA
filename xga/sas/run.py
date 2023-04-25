c39e66cc20a16b79d06aa06b7e47f0949f859c6c
DavidT3 <dt237@sussex.ac.uk>
2023-04-24 22:37:52 -0400

    I ensured that there will be a '- ' at the beginning of the 'associated source' error message I add to SAS Generation errors. This should now make sure that the LTR pipeline can parse all errors properly. For (and hopefully closing) issue #984

--- xga/sas/run.py
+++ xga/sas/run.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 24/04/2023, 22:37. Copyright (c) The Contributors
 
 from functools import wraps
 from multiprocessing.dummy import Pool
@@ -203,7 +203,7 @@ def sas_call(sas_func):
             to_raise = []
             for product in results[entry]:
                 product: BaseProduct
-                ext_info = " {s} is the associated source, the specific data used is " \
+                ext_info = "- {s} is the associated source, the specific data used is " \
                            "{o}-{i}.".format(s=sources[ind].name, o=product.obs_id, i=product.instrument)
                 if len(product.sas_errors) == 1:
                     to_raise.append(SASGenerationError(product.sas_errors[0] + ext_info))
