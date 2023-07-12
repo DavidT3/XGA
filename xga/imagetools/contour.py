bf2cf521aa1be40f8fdba81da7ab52fe250c8abe
McKenna Leichty <85258611+mckleichty@users.noreply.github.com>
2023-07-12 14:53:37 -0400

    Made a file for adding a contour feature into XGA

---
+++ xga/imagetools/contour.py
@@ -0,0 +1,5 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by McKenna Leichty 20/02/2023, 14:04. Copyright (c) The Contributors
+
+
+
