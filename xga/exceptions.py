c2d584c518efc9eb8405e9a83faa1a4f5001d30b
David Turner <turne540@msu.edu>
2023-09-22 16:27:30 -0400

    Added a new XGA error to raise when there is no telescope data setup.

--- xga/exceptions.py
+++ xga/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 09/03/2023, 23:34. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 22/09/2023, 16:27. Copyright (c) The Contributors
 
 
 class HeasoftError(Exception):
@@ -612,4 +612,24 @@ class NotSampleMemberError(Exception):
             return '{}'.format(self.message)
         else:
             return 'NotSampleMemberError has been raised'
+
+
+class NoTelescopeDataError(Exception):
+    def __init__(self, *args):
+        """
+        Raised when a part of XGA that directly accesses telescope data is used, but there are no relevant
+        telescopes setup in the configuration file.
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{}'.format(self.message)
+        else:
+            return 'NoTelescopeDataError has been raised'
 
