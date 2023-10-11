f4cb37f5a6f5050552dfd32975604b95dfe826c4
Jessica Pilling <jp735@janus.wn.xcat>
2023-10-11 14:19:03 +0100

    adding in draft docstring

---
+++ xga/generate/esass/spec.py
@@ -0,0 +1,53 @@
+# This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+# Last modified by Jessica Pilling (jp735@sussex.ac.uk) Wed Oct 11 2023, 13:51. Copyright (c) The Contributors
+
+from typing import Union, List
+
+from astropy.units import Quantity
+
+from .. import OUTPUT, NUM_CORES
+from ...sources import BaseSource
+from ...samples.base import BaseSample
+
+
+def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, Quantity],
+               inner_radius: Union[str, Quantity] = Quantity(0, 'arcsec'), group_spec: bool = True,
+               min_counts: int = 5, min_sn: float = None, over_sample: int = None, one_rmf: bool = True,
+               num_cores: int = NUM_CORES, disable_progress: bool = False, force_gen: bool = False):
+    """
+    An internal function to generate all the commands necessary to produce a srctool spectrum, but is not
+    decorated by the esass_call function, so the commands aren't immediately run. This means it can be used for
+    srctool functions that generate custom sets of spectra (like a set of annular spectra for instance), as well
+    as for things like the standard srctool_spectrum function which produce relatively boring spectra.
+
+    :param BaseSource/BaseSample sources: A single source object, or a sample of sources.
+    :param str/Quantity outer_radius: The name or value of the outer radius to use for the generation of
+        the spectrum (for instance 'r200' would be acceptable for a GalaxyCluster, or Quantity(1000, 'kpc')). If
+        'region' is chosen (to use the regions in region files), then any inner radius will be ignored.
+    :param str/Quantity inner_radius: The name or value of the inner radius to use for the generation of
+        the spectrum (for instance 'r500' would be acceptable for a GalaxyCluster, or Quantity(300, 'kpc')). By
+        default this is zero arcseconds, resulting in a circular spectrum.
+    :param bool group_spec: A boolean flag that sets whether generated spectra are grouped or not.
+    :param float min_counts: If generating a grouped spectrum, this is the minimum number of counts per channel.
+        To disable minimum counts set this parameter to None.
+    :param float min_sn: If generating a grouped spectrum, this is the minimum signal to noise in each channel.
+        To disable minimum signal to noise set this parameter to None.
+    :param int over_sample: The minimum energy resolution for each group, set to None to disable. e.g. if
+        over_sample=3 then the minimum width of a group is 1/3 of the resolution FWHM at that energy.
+    :param bool one_rmf: This flag tells the method whether it should only generate one RMF for a particular
+        ObsID-instrument combination - this is much faster in some circumstances, however the RMF does depend
+        slightly on position on the detector.
+    :param int num_cores: The number of cores to use, default is set to 90% of available.
+    :param bool disable_progress: Setting this to true will turn off the eSASS generation progress bar.
+    :param bool force_gen: This boolean flag will force the regeneration of spectra, even if they already exist.
+    """
+    # This function supports passing both individual sources and sets of sources
+    if isinstance(sources, BaseSource):
+        sources = [sources]
+
+    if outer_radius != 'region':
+        from_region = False
+        sources, inner_radii, outer_radii = region_setup(sources, outer_radius, inner_radius, disable_progress, '')
+    else:
+        # This is used in the extra information dictionary for when the XGA spectrum object is defined
+        from_region = True
