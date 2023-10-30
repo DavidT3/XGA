a53963a9298d7b1a388374d0239c6344535fc0cb
David Turner <turne540@msu.edu>
2023-10-30 17:34:50 -0400

    Updated cif_build to only grab XMM event lists

--- xga/sas/misc.py
+++ xga/sas/misc.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/10/2023, 17:34. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -47,7 +47,7 @@ def cifbuild(sources: Union[BaseSource, NullSource, BaseSample], num_cores: int 
         extra_info = []
         for obs_id in source.obs_ids:
             # Fetch an events list for this ObsID, doesn't matter which
-            some_evt_lists = source.get_products("events", obs_id=obs_id)
+            some_evt_lists = source.get_products("events", obs_id=obs_id, telescope='xmm')
             obs_date = None
             for evt in some_evt_lists:
                 # Reads in the header of the events list file
