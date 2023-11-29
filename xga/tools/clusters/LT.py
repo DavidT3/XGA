11d803bfc12bf8b113812abcaf162a84d3265378
David Turner <turne540@msu.edu>
2023-11-28 23:23:44 -0500

    Aaaand it stopped working again

--- xga/tools/clusters/LT.py
+++ xga/tools/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 23:20. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 23:23. Copyright (c) The Contributors
 from typing import Tuple
 from warnings import warn
 
@@ -308,9 +308,9 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
             #               min_sn=min_sn, over_sample=over_sample))
             # stop
             # TODO sort out how to identify the right luminosity energy range to perform predictions with
-            lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
-                          min_sn=min_sn, over_sample=over_sample)[:, 0]
-            print(lxs)
+            # lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
+            #               min_sn=min_sn, over_sample=over_sample)[:, 0]
+            # print(lxs)
             print('then with energy')
             lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
                           min_sn=min_sn, over_sample=over_sample, lo_en=Quantity(0.5, 'keV'),
