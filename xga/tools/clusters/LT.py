d4b3d0c6bc0dc6e79beea1e76b03a1977886d4a2
David Turner <turne540@msu.edu>
2023-11-28 23:11:04 -0500

    Resetting all my meddling because it hasn't helped so far

--- xga/tools/clusters/LT.py
+++ xga/tools/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 23:06. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 23:11. Copyright (c) The Contributors
 from typing import Tuple
 from warnings import warn
 
@@ -310,7 +310,12 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
             # TODO sort out how to identify the right luminosity energy range to perform predictions with
             lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
                           min_sn=min_sn, over_sample=over_sample)[:, 0]
-            #lo_en=Quantity(0.5, 'keV'), hi_en=Quantity(2.0, 'keV')
+            print(lxs)
+            print('then with energy')
+            lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
+                          min_sn=min_sn, over_sample=over_sample, lo_en=Quantity(0.5, 'keV'),
+                          hi_en=Quantity(2.0, 'keV'))[:, 0]
+            print('all done')
             stop
             txs = temp_lum_rel.predict(lxs, samp.redshifts, cosmo)
 
