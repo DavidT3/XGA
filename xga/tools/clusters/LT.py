cb53913c0e3bec40b8a4a0f3dd153af897649d78
David Turner <turne540@msu.edu>
2023-11-28 21:00:35 -0500

    Trying to diagnose Lx retrieval not working

--- xga/tools/clusters/LT.py
+++ xga/tools/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 20:56. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/11/2023, 21:00. Copyright (c) The Contributors
 from typing import Tuple
 from warnings import warn
 
@@ -290,6 +290,9 @@ def luminosity_temperature_pipeline(sample_data: pd.DataFrame, start_aperture: Q
         #  the temperature-luminosity scaling relation has to step in for us, and we just need to read out Lxs
         else:
             print(samp[0]._luminosities)
+            print(samp[0].get_luminosities(samp.get_radius(o_dens), group_spec=group_spec, min_counts=min_counts,
+                          min_sn=min_sn, over_sample=over_sample, lo_en=Quantity(0.5, 'keV'),
+                          hi_en=Quantity(2.0, 'keV')))
             stop
             # TODO sort out how to identify the right luminosity energy range to perform predictions with
             lxs = samp.Lx(samp.get_radius(o_dens), quality_checks=False, group_spec=group_spec, min_counts=min_counts,
