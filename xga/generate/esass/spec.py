4c5e9d8a092dd4500e9f2c13828f9f974c34acd0
David Turner <turne540@msu.edu>
2024-01-08 13:28:58 +0000

    Trying to actually make the spectral files be named what I think they should be - that in turn should make spectral fitting work

--- xga/generate/esass/spec.py
+++ xga/generate/esass/spec.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 08/01/2024, 13:26. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 08/01/2024, 13:28. Copyright (c) The Contributors
 
 import os
 import re
@@ -229,7 +229,7 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
                 # Cannot control the naming of spectra from srctool, so need to store
                 # the XGA formatting of the spectra, so that they can be renamed 
                 # TODO put issue, renaming spectra
-                spec_str = "{o}_{i}_{n}_ra{ra}_dec{dec}_ri{ri}_ro{ro}{ex}_spec.fits"
+                spec_str = spec_storage_name + "_spec.fits"
                 rmf_str = "{o}_{i}_{n}_ra{ra}_dec{dec}_ri{ri}_ro{ro}.rmf"
                 arf_str = "{o}_{i}_{n}_ra{ra}_dec{dec}_ri{ri}_ro{ro}.arf"
                 b_spec_str = "{o}_{i}_{n}_ra{ra}_dec{dec}_ri{ri}_ro{ro}{ex}_backspec.fits"
