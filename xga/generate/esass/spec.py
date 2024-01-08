ad1888f9f11b97c9e385b95e6a4913e6d9f199f3
David Turner <turne540@msu.edu>
2024-01-08 12:56:50 +0000

    Removing a print statement in spec

--- xga/generate/esass/spec.py
+++ xga/generate/esass/spec.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 08/01/2024, 12:54. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 08/01/2024, 12:56. Copyright (c) The Contributors
 
 import os
 import re
@@ -322,7 +322,6 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
                     cmd_str += ";rm -r temp_regs"
 
                 cmds.append(cmd_str)  # Adds the full command to the set
-                print(cmds)
                 # Makes sure the whole path to the temporary directory is created
                 os.makedirs(dest_dir)
 
