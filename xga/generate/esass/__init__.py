821b39f29f7bbb8266570deabfb363a705de70c1
David Turner <turne540@msu.edu>
2024-01-11 10:33:53 -0500

    Made sure to import expmap from esass.phot in the esass init file

--- xga/generate/esass/__init__.py
+++ xga/generate/esass/__init__.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 05/01/2024, 11:44. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 11/01/2024, 15:33. Copyright (c) The Contributors
 
-from .phot import evtool_image
+from .phot import evtool_image, expmap
 from .spec import srctool_spectrum
