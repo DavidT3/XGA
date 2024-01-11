a3f3d32e2f0c979cdc1102ab72c890ec0e7bac5f
David Turner <turne540@msu.edu>
2024-01-11 12:23:37 -0500

    Accidentally left some print statements where they shouldn't be

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 11/01/2024, 17:22. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 11/01/2024, 17:23. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1190,8 +1190,6 @@ class BaseSource:
                 prev_fits = [OUTPUT + "{t}/XSPEC/".format(t=tel) + self.name + "/" + f
                              for f in os.listdir(OUTPUT + "{t}/XSPEC/".format(t=tel) + self.name)
                              if ".xcm" not in f and ".fits" in f]
-                print(prev_fits)
-                print('')
                 for fit in prev_fits:
                     fit_name = fit.split("/")[-1]
                     fit_info = fit_name.split("_")
