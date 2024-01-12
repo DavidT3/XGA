0917c6ebb30a5c1b5eb52a2db20f3f707a861ff0
David Turner <turne540@msu.edu>
2024-01-12 13:43:06 -0500

    The single_temp_apec method couldn't deal with stacking being on if it was doing XMM data (because that doesn't have a stacking method yet). It should work now

--- xga/xspec/fit/general.py
+++ xga/xspec/fit/general.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 12/01/2024, 13:11. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 12/01/2024, 13:43. Copyright (c) The Contributors
 
 import warnings
 from typing import List, Union
@@ -102,7 +102,8 @@ def single_temp_apec(sources: Union[BaseSource, BaseSample], outer_radius: Union
         # We do not do simultaneous fits with spectra from different telescopes, they are all fit separately - at
         #  least in this current setup
         for tel in source.telescopes:
-            if stacked_spectra:
+            # TODO This is unsustainable, but hopefully every telescope will soon (ish) have a stacking method
+            if stacked_spectra and tel == 'erosita':
                 search_inst = 'combined'
             else:
                 search_inst = None
