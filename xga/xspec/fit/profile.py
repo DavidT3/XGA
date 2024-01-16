1a94e45032e2535ced8ca67a81397ad702e5ffd9
David Turner <turne540@msu.edu>
2024-01-16 15:37:11 -0500

    I think I've converted the single_temp_apec_profile to support multiple telescopes?

--- xga/xspec/fit/profile.py
+++ xga/xspec/fit/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 16/01/2024, 14:59. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 16/01/2024, 15:37. Copyright (c) The Contributors
 
 from typing import List, Union
 
@@ -25,7 +25,7 @@ def single_temp_apec_profile(sources: Union[BaseSource, BaseSample], radii: Unio
                              abund_table: str = "angr", fit_method: str = "leven", group_spec: bool = True,
                              min_counts: int = 5, min_sn: float = None, over_sample: float = None, one_rmf: bool = True,
                              num_cores: int = NUM_CORES, spectrum_checking: bool = True,
-                             timeout: Quantity = Quantity(1, 'hr')):
+                             timeout: Quantity = Quantity(1, 'hr'), stacked_spectra: bool = False):
     """
     A function that allows for the fitting of sets of annular spectra (generated from objects such as galaxy
     clusters) with an absorbed plasma emission model (tbabs*apec). This function fits the annuli completely
@@ -58,8 +58,8 @@ def single_temp_apec_profile(sources: Union[BaseSource, BaseSample], radii: Unio
     :param bool group_spec: A boolean flag that sets whether generated spectra are grouped or not.
     :param float min_counts: If generating a grouped spectrum, this is the minimum number of counts per channel.
         To disable minimum counts set this parameter to None.
-    :param float min_sn: If generating a grouped spectrum, this is the minimum signal to noise in each channel.
-        To disable minimum signal to noise set this parameter to None.
+    :param float min_sn: If generating a grouped spectrum, this is the minimum signal-to-noise in each channel.
+        To disable minimum signal-to-noise set this parameter to None.
     :param float over_sample: The minimum energy resolution for each group, set to None to disable. e.g. if
         over_sample=3 then the minimum width of a group is 1/3 of the resolution FWHM at that energy.
     :param bool one_rmf: This flag tells the method whether it should only generate one RMF for a particular
@@ -71,10 +71,23 @@ def single_temp_apec_profile(sources: Union[BaseSource, BaseSample], radii: Unio
     :param Quantity timeout: The amount of time each individual fit is allowed to run for, the default is one hour.
         Please note that this is not a timeout for the entire fitting process, but a timeout to individual source
         fits.
+    :param bool stacked_spectra: Whether stacked spectra (of all instruments for an ObsID) should be used for this
+        XSPEC spectral fit. If a stacking procedure for a particular telescope is not supported, this function will
+        instead use individual spectra for an ObsID. The default is False.
     """
+    # TODO Figure out how to do this within the storage framework of XGA - see issue #1098
+    if stacked_spectra:
+        raise NotImplementedError("Stacking spectra is not yet supported for annular spectra generation and fitting.")
 
-    # We make sure the requested sets of annular spectra have actually been generated
-    spectrum_set(sources, radii, group_spec, min_counts, min_sn, over_sample, one_rmf, num_cores)
+    # TODO This will probably be replaced by a centralised function that generates annular spectra for many
+    #  different telescopes at some point
+    if 'xmm' in sources.telescopes:
+        # We make sure the requested sets of annular spectra have actually been generated (for XMM)
+        spectrum_set(sources, radii, group_spec, min_counts, min_sn, over_sample, one_rmf, num_cores)
+
+    if 'erosita' in sources.telescopes:
+        raise NotImplementedError("We haven't yet added annular spectra generation for eROSITA.")
+
     sources = _check_inputs(sources, lum_en, lo_en, hi_en, fit_method, abund_table, timeout)
 
     # Unfortunately, a very great deal of this function is going to be copied from the original single_temp_apec
@@ -94,84 +107,95 @@ def single_temp_apec_profile(sources: Union[BaseSource, BaseSample], radii: Unio
     for src_ind, source in enumerate(sources):
         # Gets the set of radii for this particular source into a variable
         cur_radii = radii[src_ind]
-
         source: BaseSource
-        # This will fetch the annular_spec, get_annular_spectra will throw an error if no matches
-        #  are found, though as we have run spectrum_set that shouldn't happen
-        ann_spec = source.get_annular_spectra(cur_radii, group_spec, min_counts, min_sn, over_sample)
-        deg_rad.append(ann_spec.radii)
 
-        # If source.get_annular_spectra returns a list, it means that multiple matches have been found
-        if isinstance(ann_spec, list):
-            # This shouldn't ever go off I don't think
-            raise ValueError("Multiple annular spectra set matches have been found.")
+        # We do not do simultaneous fits with spectra from different telescopes, they are all fit separately - at
+        #  least in this current setup
+        for tel in source.telescopes:
+            # TODO This is unsustainable, but hopefully every telescope will soon (ish) have a stacking method
+            # if stacked_spectra and tel == 'erosita':
+            #     search_inst = 'combined'
+            # else:
+            #     search_inst = None
 
-        # We step through the annuli and make fitting scripts for them all independently
-        for ann_id in range(ann_spec.num_annuli):
-            # We fetch the spectrum objects for this particular annulus
-            spec_objs = ann_spec.get_spectra(ann_id)
-            if isinstance(spec_objs, Spectrum):
-                spec_objs = [spec_objs]
+            # This will fetch the annular_spec, get_annular_spectra will throw an error if no matches
+            #  are found, though as we have run spectrum_set that shouldn't happen
+            ann_spec = source.get_annular_spectra(cur_radii, group_spec, min_counts, min_sn, over_sample,
+                                                  telescope=tel)
+            deg_rad.append(ann_spec.radii)
 
-            # Turn spectra paths into TCL style list for substitution into template
-            specs = "{" + " ".join([spec.path for spec in spec_objs]) + "}"
-            # For this model, we have to know the redshift of the source.
-            if source.redshift is None:
-                raise ValueError("You cannot supply a source without a redshift to this model.")
+            # If source.get_annular_spectra returns a list, it means that multiple matches have been found
+            if isinstance(ann_spec, list):
+                # This shouldn't ever go off I don't think
+                raise ValueError("Multiple annular spectra set matches have been found.")
 
-            # Whatever start temperature is passed gets converted to keV, this will be put in the template
-            t = start_temp.to("keV", equivalencies=u.temperature_energy()).value
-            # Another TCL list, this time of the parameter start values for this model.
-            par_values = "{{{0} {1} {2} {3} {4} {5}}}".format(1., source.nH.to("10^22 cm^-2").value, t, start_met,
-                                                              source.redshift, 1.)
+            # We step through the annuli and make fitting scripts for them all independently
+            for ann_id in range(ann_spec.num_annuli):
+                # We fetch the spectrum objects for this particular annulus
+                spec_objs = ann_spec.get_spectra(ann_id)
+                if isinstance(spec_objs, Spectrum):
+                    spec_objs = [spec_objs]
 
-            # Set up the TCL list that defines which parameters are frozen, dependant on user input
-            if freeze_nh and freeze_met:
-                freezing = "{F T F T T F}"
-            elif not freeze_nh and freeze_met:
-                freezing = "{F F F T T F}"
-            elif freeze_nh and not freeze_met:
-                freezing = "{F T F F T F}"
-            elif not freeze_nh and not freeze_met:
-                freezing = "{F F F F T F}"
+                # Turn spectra paths into TCL style list for substitution into template
+                specs = "{" + " ".join([spec.path for spec in spec_objs]) + "}"
+                # For this model, we have to know the redshift of the source.
+                if source.redshift is None:
+                    raise ValueError("You cannot supply a source without a redshift to this model.")
 
-            # Set up the TCL list that defines which parameters are linked across different spectra
-            linking = "{F T T T T T}"
+                # Whatever start temperature is passed gets converted to keV, this will be put in the template
+                t = start_temp.to("keV", equivalencies=u.temperature_energy()).value
+                # Another TCL list, this time of the parameter start values for this model.
+                par_values = "{{{0} {1} {2} {3} {4} {5}}}".format(1., source.nH.to("10^22 cm^-2").value, t, start_met,
+                                                                  source.redshift, 1.)
 
-            # If the user wants the spectrum cleaning step to be run, then we have to setup some acceptable
-            #  limits. For this function they will be hardcoded, for simplicities sake, and we're only going to
-            #  check the temperature, as its the main thing we're fitting for with constant*tbabs*apec
-            if spectrum_checking:
-                check_list = "{kT}"
-                check_lo_lims = "{0.01}"
-                check_hi_lims = "{20}"
-                check_err_lims = "{15}"
-            else:
-                check_list = "{}"
-                check_lo_lims = "{}"
-                check_hi_lims = "{}"
-                check_err_lims = "{}"
+                # Set up the TCL list that defines which parameters are frozen, dependant on user input
+                if freeze_nh and freeze_met:
+                    freezing = "{F T F T T F}"
+                elif not freeze_nh and freeze_met:
+                    freezing = "{F F F T T F}"
+                elif freeze_nh and not freeze_met:
+                    freezing = "{F T F F T F}"
+                elif not freeze_nh and not freeze_met:
+                    freezing = "{F F F F T F}"
 
-            # This sets the list of parameter IDs which should be zeroed at the end to calculate unabsorbed
-            #  luminosities. I am only specifying parameter 2 here (though there will likely be multiple models
-            #  because there are likely multiple spectra) because I know that nH of tbabs is linked in this
-            #  setup, so zeroing one will zero them all.
-            nh_to_zero = "{2}"
+                # Set up the TCL list that defines which parameters are linked across different spectra
+                linking = "{F T T T T T}"
 
-            file_prefix = spec_objs[0].storage_key + "_ident{}_".format(spec_objs[0].set_ident) \
-                          + str(spec_objs[0].annulus_ident)
-            out_file, script_file = _write_xspec_script(source, file_prefix, model, abund_table, fit_method,
-                                                        specs, lo_en, hi_en, par_names, par_values, linking, freezing,
-                                                        par_fit_stat, lum_low_lims, lum_upp_lims, lum_conf,
-                                                        source.redshift, spectrum_checking, check_list, check_lo_lims,
-                                                        check_hi_lims, check_err_lims, True, nh_to_zero)
+                # If the user wants the spectrum cleaning step to be run, then we have to set up some acceptable
+                #  limits. For this function they will be hardcoded, for simplicities’ sake, and we're only going to
+                #  check the temperature, as it's the main thing we're fitting for with constant*tbabs*apec
+                if spectrum_checking:
+                    check_list = "{kT}"
+                    check_lo_lims = "{0.01}"
+                    check_hi_lims = "{20}"
+                    check_err_lims = "{15}"
+                else:
+                    check_list = "{}"
+                    check_lo_lims = "{}"
+                    check_hi_lims = "{}"
+                    check_err_lims = "{}"
 
-            try:
-                res = ann_spec.get_results(0, model, 'kT')
-            except ModelNotAssociatedError:
-                script_paths.append(script_file)
-                outfile_paths.append(out_file)
-                src_inds.append(src_ind)
+                # This sets the list of parameter IDs which should be zeroed at the end to calculate unabsorbed
+                #  luminosities. I am only specifying parameter 2 here (though there will likely be multiple models
+                #  because there are likely multiple spectra) because I know that nH of tbabs is linked in this
+                #  setup, so zeroing one will zero them all.
+                nh_to_zero = "{2}"
+
+                file_prefix = (spec_objs[0].storage_key + "_ident{}_".format(spec_objs[0].set_ident)
+                               + str(spec_objs[0].annulus_ident))
+                out_file, script_file = _write_xspec_script(source, file_prefix, model, abund_table, fit_method,
+                                                            specs, lo_en, hi_en, par_names, par_values, linking,
+                                                            freezing, par_fit_stat, lum_low_lims, lum_upp_lims,
+                                                            lum_conf, source.redshift, spectrum_checking, check_list,
+                                                            check_lo_lims, check_hi_lims, check_err_lims, True,
+                                                            nh_to_zero, tel)
+
+                try:
+                    res = ann_spec.get_results(0, model, 'kT')
+                except ModelNotAssociatedError:
+                    script_paths.append(script_file)
+                    outfile_paths.append(out_file)
+                    src_inds.append(src_ind)
 
     run_type = "fit"
     return script_paths, outfile_paths, num_cores, run_type, src_inds, deg_rad, timeout
