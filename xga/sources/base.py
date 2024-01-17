90b5a0b79cf7abff835398d098ba9b1c9b954d0c
David Turner <turne540@msu.edu>
2024-01-17 16:55:48 -0500

    The disassociate_obs method in this version of XGA didn't add the _products[tel][combined] entry back in if it were removed!

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 17/01/2024, 16:38. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 17/01/2024, 16:55. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -3918,6 +3918,7 @@ class BaseSource:
             # If we're disassociating certain observations, odds on the combined products are no longer valid
             if "combined" in self._products[tel]:
                 del self._products[tel]["combined"]
+                self._products[tel]['combined'] = {}
                 if "combined" in self._interloper_masks[tel]:
                     del self._interloper_masks[tel]["combined"]
                     self._interloper_masks[tel]["combined"] = {}
