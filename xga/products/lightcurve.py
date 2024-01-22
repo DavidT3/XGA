ff9f865672c6f96b18105211b0de81027bbeb388
David Turner <turne540@msu.edu>
2024-01-22 00:10:35 -0500

    Should now make the right hand solid line of the plot when we select a subset of time chunks to plot in view method of AggregateLightCurve

--- xga/products/lightcurve.py
+++ xga/products/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 22/01/2024, 00:09. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 22/01/2024, 00:10. Copyright (c) The Contributors
 import re
 from datetime import datetime
 from typing import Union, List, Tuple
@@ -1439,7 +1439,7 @@ class AggregateLightCurve(BaseAggregateProduct):
                 # If there is more than one time chunk, we turn off the line on the right hand side of this initial
                 #  axes - so there is no unsightly barrier between it and the next axes - we also add slanted lines
                 #  to indicate a break in the y-axis
-                if self.num_time_chunks != 1:
+                if len(time_chunk_ids) != 1:
                     axes_dict[tc_id_ind].spines.right.set_visible(False)
                     axes_dict[tc_id_ind].plot([1, 1], [1, 0], transform=axes_dict[tc_id_ind].transAxes, **break_kwargs)
 
