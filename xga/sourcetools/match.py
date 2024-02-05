347071c1b7ce5c2640faabd3a150a4356cb33e9a
David Turner <turne540@msu.edu>
2024-02-05 17:29:18 -0500

    Making sure there is a dist column in the return from census_match, even if every entry is NaN

--- xga/sourcetools/match.py
+++ xga/sourcetools/match.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 05/02/2024, 17:24. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 05/02/2024, 17:29. Copyright (c) The Contributors
 
 import gc
 import os
@@ -581,7 +581,10 @@ def census_match(telescope: Union[str, list] = None, obs_ids: Union[List[str], d
         all_excl = in_bl[np.logical_and.reduce([in_bl[excl] == 'T' for excl in excl_col])]
 
         # These are the observations that have at  least some usable data.
-        results[tel] = rel_census[~rel_census["ObsID"].isin(all_excl["ObsID"])]
+        all_incl = rel_census[~rel_census["ObsID"].isin(all_excl["ObsID"])]
+        # To make the output entirely the same as the matches on position we add a NaN distance column
+        all_incl['dist'] = np.NaN
+        results[tel] = all_incl
         # And we store the fully blacklisted observations in another dictionary
         bl_results[tel] = all_excl
 
