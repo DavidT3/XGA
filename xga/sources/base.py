c3f6d4615802dca20b481975d946397c0df23d50
David Turner <turne540@msu.edu>
2024-02-06 19:37:47 -0500

    Hopefully corrected a bug that made num_inst_obs report incorrect numbers of instruments

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 06/02/2024, 16:12. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 06/02/2024, 19:37. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -636,7 +636,7 @@ class BaseSource:
             are an integer count of the total number of observations for that instrument.
         :rtype: dict
         """
-        return {tel: {i: len([i in self.instruments[tel][o] for o in self.instruments[tel]])
+        return {tel: {i: len([i for o in self.instruments[tel] if i in self.instruments[tel][o]])
                       for i in ALLOWED_INST[tel]} for tel in self.instruments}
 
     # @property
