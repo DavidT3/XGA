515c0f6fb3e7d28db7113fb16f3b11f4ba8093de
David Turner <turne540@msu.edu>
2024-02-14 16:06:23 -0500

    Removed a test statement from eSASS' run.py

--- xga/generate/esass/run.py
+++ xga/generate/esass/run.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 14/02/2024, 15:49. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 14/02/2024, 16:06. Copyright (c) The Contributors
 
 from functools import wraps
 from multiprocessing.dummy import Pool
@@ -133,13 +133,8 @@ def esass_call(esass_func):
             to_raise = []
             for product in results[entry]:
                 product: BaseProduct
-
-                print('')
-                print(product.gen_errors)
-                print('')
-
-                ext_info = "- {s} is the associated source, the specific data used is " \
-                           "{o}-{i}.".format(s=sources[ind].name, o=product.obs_id, i=product.instrument)
+                ext_info = ("- {s} is the associated source, the specific data used is "
+                            "{o}-{i}.").format(s=sources[ind].name, o=product.obs_id, i=product.instrument)
                 if len(product.gen_errors) == 1:
                     to_raise.append(ProductGenerationError(product.gen_errors[0] + ext_info))
                 elif len(product.gen_errors) > 1:
