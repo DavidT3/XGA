First pass attempt at fixing prediction uncertainties in XGA scaling relations

xga/products/relation.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 30/11/2023, 19:50. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 26/02/2024, 18:01. Copyright (c) The Contributors
 
 import inspect
 import pickle
@@ -19,7 +19,7 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS, BASE_COLORS, Colormap, CSS4_COLORS, Normalize
 from matplotlib.ticker import FuncFormatter
 
-from ..models import MODEL_PUBLICATION_NAMES
+from ..models import MODEL_PUBLICATION_NAMES, power_law
 
 # This is the default colour cycle for the AggregateScalingRelation view method
 PRETTY_COLOUR_CYCLE = ['tab:gray', 'tab:blue', 'darkgreen', 'firebrick', 'slateblue', 'goldenrod']
@@ -756,7 +756,7 @@ class ScalingRelation:
         plt.show()
 
     def predict(self, x_values: Quantity, redshift: Union[float, np.ndarray] = None,
-                cosmo: Cosmology = None) -> Quantity:
+                cosmo: Cosmology = None, x_errors: Quantity = None) -> Quantity:
         """
         This method allows for the prediction of y values from this scaling relation, you just need to pass in an
         appropriate set of x values. If a power of E(z) was applied to the y-axis data before fitting, and that
@@ -768,6 +768,9 @@ class ScalingRelation:
             only necessary if the 'dim_hubb_ind' argument was set on declaration. Default is None.
         :param Cosmology cosmo: The cosmology in which we wish to predict values. This is only necessary if the
             'dim_hubb_ind' argument was set on declaration. Default is None.
+        :param Quantity x_errors: The uncertainties for passed x-values. Default is None. If this argument is not None
+            then uncertainties in x-value and the model fit will be propagated to a final prediction uncertainty. If
+            minus and plus uncertainties are passed then they will be averaged before propagation.
         :return: The predicted y values.
         :rtype: Quantity
         """
@@ -776,6 +779,17 @@ class ScalingRelation:
             raise UnitConversionError('Values of x passed to the predict method ({xp}) must be convertible '
                                       'to the x-axis units of this scaling relation '
                                       '({xr}).'.format(xp=x_values.unit.to_string(), xr=self.x_unit.to_string()))
+
+        # Check that if x errors have been passed, they're in the right units
+        if x_errors is not None and x_errors.unit != x_values.unit:
+            raise UnitConversionError("The x errors are not in the same units as 'x_values'.")
+        elif x_errors is not None and len(x_errors) != len(x_values):
+            raise ValueError("The length of the 'x_errors' argument ({xe}) should be the same as the 'x_values' "
+                             "argument({xv}).".format(xe=len(x_errors), xv=len(x_values)))
+
+        # We average the uncertainties if there are minus and plus values (bad I know)
+        if x_errors is not None and x_errors.ndim == 2:
+            x_errors = x_errors.mean(axis=1)
 
         # This is a check that all passed x values are within the validity limits of this relation (if the
         #  user passed those on init) - if they aren't a warning will be issued
@@ -803,7 +817,28 @@ class ScalingRelation:
 
         # If there was a power of E(z) applied to the data, we undo it for the prediction.
         if self._ez_power is not None:
-            predicted_y /= (cosmo.efunc(redshift)**self._ez_power)
+            # We store this so that error propogation can use it later
+            ez = (cosmo.efunc(redshift)**self._ez_power)
+            predicted_y /= ez
+        else:
+            # This means that error propagation doesn't need to keep checking whether there is an ez power stored
+            ez = np.ones(len(predicted_y))
+
+        # errors
+        if x_errors is not None and self.model_func == power_law:
+            # TODO Remove obviously, should be slope
+            print(self.pars[0, 0])
+            term_one = (self.y_norm*(1/ez)*(x_values/self.x_norm)**self.pars[0, 0] * self.pars[1, 1])**2
+            # TODO Remove obviously, should be norm
+            print(self.pars[1, 0])
+            term_two = ((self.y_norm*(1/ez)*self.pars[1, 0]*(1/self.x_norm)**self.pars[0, 0] * self.pars[0, 0] *
+                        x_values**(self.pars[0, 0] - 1))*self.pars[0, 1])**2
+
+            term_three = ((self.y_norm*(self.pars[1, 0]*(x_values/5)**self.pars[0, 0])/ez)*(np.log(x_values)-np.log(self.x_norm)) *
+                          x_errors)**2
+
+            predicted_y_errs = np.sqrt(term_one + term_two + term_three)
+            predicted_y = np.concatenate([predicted_y, predicted_y_errs], axis=1)
 
         return predicted_y
 