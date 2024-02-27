c01da12af25331721614645deb4e1899f0051d13
David Turner <turne540@msu.edu>
2024-02-26 19:46:15 -0500

    Making the prediction method support more forms of error being passed

--- xga/products/relation.py
+++ xga/products/relation.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 26/02/2024, 19:39. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 26/02/2024, 19:46. Copyright (c) The Contributors
 
 import inspect
 import pickle
@@ -794,7 +794,7 @@ class ScalingRelation:
         # We average the uncertainties if there are minus and plus values (bad I know)
         if x_errors is not None and x_errors.ndim == 2:
             x_errors = x_errors.mean(axis=1)
-        elif x_errors is not None and x_values.isscalar and len(x_errors) == 2:
+        elif x_errors is not None and x_values.isscalar and not x_errors.isscalar and len(x_errors) == 2:
             x_errors = x_errors.mean()
 
         print(x_errors)
