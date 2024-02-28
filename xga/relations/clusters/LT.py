ab2c436639bbf4f47961c9dc2fe89edcfcfa1706
David Turner <turne540@msu.edu>
2024-02-28 11:11:59 -0500

    Updated the SDSSRM-XCS LT relations

--- xga/relations/clusters/LT.py
+++ xga/relations/clusters/LT.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 17/04/2023, 21:00. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2024, 11:11. Copyright (c) The Contributors
 
 import numpy as np
 from astropy.units import Quantity
@@ -11,19 +11,41 @@ from ...products.relation import ScalingRelation
 #  implemented yet(see issue #289)
 
 
-xcs_sdss_r500_52 = ScalingRelation(np.array([2.51, 0.97]), np.array([0.11, 0.06]), power_law, Quantity(4, 'keV'),
+xcs_sdss_r500_52 = ScalingRelation(np.array([2.63, 0.97]), np.array([0.12, 0.06]), power_law, Quantity(4, 'keV'),
                                    Quantity(0.8e+44, 'erg / s'), r"T$_{\rm{x},500}$",
                                    r"E(z)$^{-1}$L$_{\rm{x},500,0.5-2.0}$", x_lims=Quantity([1, 12], 'keV'),
                                    relation_name=r'SDSSRM-XCS$_{T_{\rm{x}},vol}$ 0.5-2.0keV',
-                                   relation_author='Giles et al.', relation_year='In Prep', relation_doi='',
+                                   relation_author='Giles et al.', relation_year='2022',
+                                   relation_doi='https://doi.org/10.1093/mnras/stac2414',
                                    dim_hubb_ind=-1)
 
-xcs_sdss_r500_bol = ScalingRelation(np.array([2.94, 3.06]), np.array([0.12, 0.18]), power_law, Quantity(4, 'keV'),
+xcs_sdss_r500_52_target = ScalingRelation(np.array([2.63, 1.04]), np.array([0.2, 0.09]), power_law, Quantity(4, 'keV'),
+                                          Quantity(0.8e+44, 'erg / s'), r"T$_{\rm{x},500}$",
+                                          r"E(z)$^{-1}$L$_{\rm{x},500,0.5-2.0}$", x_lims=Quantity([1, 12], 'keV'),
+                                          relation_name=r'SDSSRM-XCS$_{T_{\rm{x}},vol,tar}$ 0.5-2.0keV',
+                                          relation_author='Giles et al.', relation_year='2022',
+                                          relation_doi='https://doi.org/10.1093/mnras/stac2414',
+                                          dim_hubb_ind=-1)
+
+xcs_sdss_r500_52_serin = ScalingRelation(np.array([2.0, 0.66]), np.array([0.22, 0.09]), power_law, Quantity(4, 'keV'),
+                                         Quantity(0.8e+44, 'erg / s'), r"T$_{\rm{x},500}$",
+                                         r"E(z)$^{-1}$L$_{\rm{x},500,0.5-2.0}$", x_lims=Quantity([1, 12], 'keV'),
+                                         relation_name=r'SDSSRM-XCS$_{T_{\rm{x}},vol,serin}$ 0.5-2.0keV',
+                                         relation_author='Giles et al.', relation_year='2022',
+                                         relation_doi='https://doi.org/10.1093/mnras/stac2414',
+                                         dim_hubb_ind=-1)
+
+
+xcs_sdss_r500_bol = ScalingRelation(np.array([3.07, 3.05]), np.array([0.12, 0.18]), power_law, Quantity(4, 'keV'),
                                     Quantity(0.8e+44, 'erg / s'), r"T$_{\rm{x},500}$",
                                     r"E(z)$^{-1}$L$_{\rm{x},500,bol}$", x_lims=Quantity([1, 12], 'keV'),
                                     relation_name=r'SDSSRM-XCS$_{T_{\rm{x}},vol}$ Bolometric',
-                                    relation_author='Giles et al.', relation_year='In Prep', relation_doi='',
+                                    relation_author='Giles et al.', relation_year='2022',
+                                    relation_doi='https://doi.org/10.1093/mnras/stac2414',
                                     dim_hubb_ind=-1)
+
+
+
 
 
 
