b373508b6924d6625fa3c68d00e73e619983a0d0
David Turner <turne540@msu.edu>
2024-08-01 16:38:27 -0400

    Nope, it is at some point hallucinating that 0105262401 has PN associated, because that is not how the original _products structure goes. For issue #1178

--- xga/samples/extended.py
+++ xga/samples/extended.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 01/08/2024, 16:14. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 01/08/2024, 16:38. Copyright (c) The Contributors
 
 from typing import List
 
@@ -230,7 +230,9 @@ class ClusterSample(BaseSample):
 
         self._names = final_names
 
-        print(self.r500)
+        print(self['ACCEPT-120'].obs_ids)
+        print('')
+        print(self['ACCEPT-120'].instruments['0105262401'])
 
         # And again I ask XGA to generate the merged images and exposure maps, in case any sources have been
         #  cleaned and had data removed
