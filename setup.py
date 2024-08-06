36e797465779e912f9bd49cd9e2314dbb96a0cc2
David Turner <turne540@msu.edu>
2024-08-06 22:16:17 +0100

    Update setup.py
    Changed matplotlib 3.9.1 to 3.9.0, as the original was yanked

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='xga',
       url='http://github.com/DavidT3/XGA',
       setup_requires=[],
       install_requires=["astropy>=6.1.2", "numpy>=1.26.4", "tqdm>=4.66.4", "regions>=0.9", "pandas>=2.2.2",
-                        "fitsio>=1.2.1", "matplotlib>=3.9.1", "scipy>=1.14.0", "pyabel>=0.9", "corner>=2.2.2",
+                        "fitsio>=1.2.1", "matplotlib>=3.9.0", "scipy>=1.14.0", "pyabel>=0.9", "corner>=2.2.2",
                         "emcee>=3.1.6", "tabulate>=0.9.0", "getdist>=1.4.7", "exceptiongroup>=1.0.4"],
       include_package_data=True,
       python_requires='>=3.10')
