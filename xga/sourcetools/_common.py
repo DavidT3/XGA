108b39d3fc4f9ea381734ea47aed554617db27ca
Jessica Pilling <jp735@janus.wn.xcat>
2024-08-08 16:28:28 +0100

    named some variables incorrectly, commenting more

--- xga/sourcetools/_common.py
+++ xga/sourcetools/_common.py
@@ -99,19 +99,24 @@ def _setup_inv_abel_dens_onion_temp(sources: Union[GalaxyCluster, ClusterSample]
     dens_model_dict = {str(sources[m_ind]): m for m_ind, m in enumerate(dens_model)}
     temp_model_dict = {str(sources[m_ind]): m for m_ind, m in enumerate(temp_model)}
 
-    # Here we take only the sources that have a successful global temperature measurement
-    # the format of has_glob_temp is a dictionary with telescope keys, and then an array of booleans
+    # Here we take only the sources that have a successful global temperature measurement for at 
+    # least one of the associated telescopes
     cut_sources = []
     for sind, src in enumerate(sources):
-        # this collects if the sources has a temp for each telescope
+        # The format of has_glob_temp is a dictionary with telescope keys, and then an array of booleans
+        # ie. has_glob_temp = {'xmm' : [True, True, False], 'erosita' : [True, True, True]}
+        # So we need to cycle through each key and collect the correct indicies to the corresponding source
+        # has_temp is storing the boolean for every telescope 
         has_temp = []
         for key in has_glob_temp:
             has_temp.append(has_glob_temp[key][sind])
 
-        # if a source has a glob temp in at least one telescope it gets parsed on
+        # If a source has a glob temp in at least one telescope it gets passed on
+        # If the sum is 0 that means every element in has_temp was False, and we discard these sources
         if sum(has_temp) > 0:
             cut_sources.append(src)
     
+    # Collecting the abridged radii needed now we have cut the sources
     cut_rads = Quantity([rads_dict[str(src)] for src in cut_sources])
     if len(cut_sources) == 0:
         raise ValueError("No sources have a successful global temperature measurement.")
@@ -123,20 +128,18 @@ def _setup_inv_abel_dens_onion_temp(sources: Union[GalaxyCluster, ClusterSample]
                                         freeze_met=freeze_met, abund_table=abund_table, temp_lo_en=temp_lo_en,
                                         temp_hi_en=temp_hi_en, num_cores=num_cores)
 
-    # TODO case where a source has a measured glob temp in one telescope and not the others
-    # This just allows us to quickly look-up the temperature profile we need later
-    # It is stored with sources as keys, then a dictionary value, this dictionary has a telescope
-    # keys with values that are the profile object, ie. {src1: {'xmm' : Profile}}
+    # DAVID_QUESTION case where a source has a measured glob temp in one telescope and not the others
+    # We are reorganising this temp_profs output so it is easier to cycle through in later functions
+    # temp_prof_dict will have sources as keys, then a dictionary value, this dictionary has
+    # telescope keys with values that are the profile object, ie. {src1: {'xmm' : Profile}}
     temp_prof_dict = {}
     for p_ind, p in enumerate(cut_sources):
-        # this dict will have telescope keys and profile object values
+        # this is the nested dict that will have telescope keys and profile object values
         src_dict = {}
-        for tel in temp_3d_prof:
-            if has_glob_temp[tel][p_ind]
-            src_dict[tel] = temp_3d_prof[tel][p_ind]
-        
-        temp_3d_prof[str(cut_sources[p_ind])] = src_dict
-
+        for tel in temp_profs:
+            # temp_profs is of the form {'xmm': [Profile, Profile, etc.]}
+            src_dict[tel] = temp_profs[tel][p_ind]
+        temp_prof_dict[str(cut_sources[p_ind])] = src_dict
 
     # Now we take only the sources that have successful 3D temperature profiles. We do the temperature profile
     #  stuff first because its more difficult, and why should we waste time on a density profile if the temperature
