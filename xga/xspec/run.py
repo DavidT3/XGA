302ed49dba13174299952098a9e394900a1c5ace
David Turner <turne540@msu.edu>
2024-08-14 22:29:16 -0400

    I think my xspec fit local pfiles thing maybe didn't work after all and I'm trying some diagnostics

--- xga/xspec/run.py
+++ xga/xspec/run.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 14/08/2024, 17:33. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 14/08/2024, 22:29. Copyright (c) The Contributors
 
 import os
 import shutil
@@ -63,6 +63,7 @@ def execute_cmd(x_script: str, out_file: str, src: str, run_type: str, timeout: 
     # This makes sure the process is killed if it does timeout
     try:
         out, err = xspec_proc.communicate(timeout=timeout)
+        print(err)
     except TimeoutExpired:
         xspec_proc.kill()
         out, err = xspec_proc.communicate()
