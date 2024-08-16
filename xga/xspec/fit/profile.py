f8b80c10baf62a6da00d0ffff3a075a6fbb2b7b1
David Turner <turne540@msu.edu>
2024-08-16 11:52:50 -0400

    The annular spectrum get_results and add_fit_results methods both should now support a 'fit configuration' storage key. Also added two new properties (fitted models and fitted_model_configurations) to support these features, as well as to allow the user to easily see what models and fit configurations are available.

--- xga/xspec/fit/profile.py
+++ xga/xspec/fit/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 16/08/2024, 09:30. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 16/08/2024, 11:52. Copyright (c) The Contributors
 
 from typing import List, Union
 
@@ -58,8 +58,8 @@ def single_temp_apec_profile(sources: Union[BaseSource, BaseSample], radii: Unio
     :param bool group_spec: A boolean flag that sets whether generated spectra are grouped or not.
     :param float min_counts: If generating a grouped spectrum, this is the minimum number of counts per channel.
         To disable minimum counts set this parameter to None.
-    :param float min_sn: If generating a grouped spectrum, this is the minimum signal to noise in each channel.
-        To disable minimum signal to noise set this parameter to None.
+    :param float min_sn: If generating a grouped spectrum, this is the minimum signal-to-noise in each channel.
+        To disable minimum signal-to-noise set this parameter to None.
     :param float over_sample: The minimum energy resolution for each group, set to None to disable. e.g. if
         over_sample=3 then the minimum width of a group is 1/3 of the resolution FWHM at that energy.
     :param bool one_rmf: This flag tells the method whether it should only generate one RMF for a particular
