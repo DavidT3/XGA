ae5f05125cbe3b18272c2a980fc6d9cdd41de231
David Turner <turne540@msu.edu>
2024-08-20 11:51:33 -0400

    Added a new error type 'XGADeveloperError'

--- xga/exceptions.py
+++ xga/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 07/11/2023, 10:57. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 20/08/2024, 11:51. Copyright (c) The Contributors
 
 
 class HeasoftError(Exception):
@@ -613,3 +613,21 @@ class NotSampleMemberError(Exception):
         else:
             return 'NotSampleMemberError has been raised'
 
+
+class XGADeveloperError(Exception):
+    def __init__(self, *args):
+        """
+        Raised when an error has occured that needs the attention of developers.
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{}'.format(self.message)
+        else:
+            return 'XGADeveloperError has been raised'
