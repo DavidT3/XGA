8b6040c5d3c0dbde29bb5bfb62a41f56c6f2ad80
David Turner <turne540@msu.edu>
2024-08-26 14:42:49 -0400

    Trying to make sure the XSPEC fit read in finds the correct spectra

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 26/08/2024, 14:39. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 26/08/2024, 14:42. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1094,7 +1094,7 @@ class BaseSource:
                 fit_ois = np.array(row['obs_ids'].split('/'))
                 fit_insts = np.array(row['insts'].split('/'))
 
-                oi_dict = {oi: list(fit_insts[np.argwhere(fit_ois == oi)])
+                oi_dict = {oi: list(fit_insts[np.argwhere(fit_ois == oi)].T)
                            for oi in list(set(fit_ois))}
 
                 print(self.instruments)
@@ -1115,7 +1115,7 @@ class BaseSource:
                     inst_lums = {}
                     obs_order = []
                     for line_ind, line in enumerate(fit_data["SPEC_INFO"]):
-                        sp_oi, sp_inst = line["SPEC_PATH"].strip(" ").split("/")[-1].split("_")[:-1]
+                        sp_oi, sp_inst = line["SPEC_PATH"].strip(" ").split("/")[-1].split("_")[:2]
                         print(sp_oi)
                         print(sp_inst)
                         rel_sp = self.get_products('spectrum', sp_oi, sp_inst, spec_key)
