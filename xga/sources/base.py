bc86e1486b78860b622e28ffeb53161eb07ec5e9
David Turner <turne540@msu.edu>
2024-08-26 14:10:46 -0400

    Really want pandas to read the set_ident column (which may have NaN values) in as integers -trying a different dtype.

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 26/08/2024, 14:05. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 26/08/2024, 14:10. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1080,7 +1080,7 @@ class BaseSource:
 
             # Everything in this file will be relevant to the current source
             cur_fit_inv = pd.read_csv(os.path.join(OUTPUT, 'XSPEC', self.name, 'inventory.csv'),
-                                      dtype={'set_ident': int})
+                                      dtype={'set_ident': 'Int64'})
 
             cur_fit_inv = cur_fit_inv.drop_duplicates(['spec_key', 'fit_conf_key', 'obs_ids', 'insts', 'src_name',
                                                        'type', 'set_ident'])
