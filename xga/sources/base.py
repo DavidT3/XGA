a4cbf380ce461bcc1a44397cb9d8ac309b9f9685
David Turner <turne540@msu.edu>
2024-08-27 22:09:48 -0400

    Was still treating a single spectrum object like a list in the reading in of cross-arf fits. SHould be corrected fully now

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 27/08/2024, 22:08. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 27/08/2024, 22:09. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1288,7 +1288,7 @@ class BaseSource:
                         assign_res = False
                         break
 
-                    rel_sps.append(rel_sp[0])
+                    rel_sps.append(rel_sp)
 
                 if not assign_res:
                     break
