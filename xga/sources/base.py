af47d81660d102c419cca7299f44dd6bc6e99ff7
David Turner <turne540@msu.edu>
2024-08-27 22:08:24 -0400

    Trying to make the reading in of cross-arf fits work - was getting an error because I was trying to run len on a Spectrum object

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 27/08/2024, 21:59. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 27/08/2024, 22:08. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1282,12 +1282,13 @@ class BaseSource:
                     sp_oi, sp_inst = spec_info[:2]
                     sp_ann_id = int(spec_info[-2])
 
-                    rel_sp = rel_ann_sp.get_spectra(sp_ann_id, sp_oi, sp_inst)
-                    if len(rel_sp) != 1:
+                    try:
+                        rel_sp = rel_ann_sp.get_spectra(sp_ann_id, sp_oi, sp_inst)
+                    except NoProductAvailableError:
                         assign_res = False
                         break
-                    else:
-                        rel_sps.append(rel_sp[0])
+
+                    rel_sps.append(rel_sp[0])
 
                 if not assign_res:
                     break
