a1df3dceea259798a20692ad238a6eb7278b97ca
David Turner <turne540@msu.edu>
2024-08-30 10:56:59 -0400

    Still don't understand what is going on

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 30/08/2024, 10:53. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/08/2024, 10:56. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -1173,7 +1173,9 @@ class BaseSource:
 
                 assign_res = True
                 for row_ind, row in fit_ann_inv_ent.iterrows():
+                    print(row)
                     if not assign_res:
+                        print('WHAY THE FUCK?')
                         break
 
                     # We'll read out some key information from the row into variables to make our life a little neater
