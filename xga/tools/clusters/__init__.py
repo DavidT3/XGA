86b2ed2ea09cc56e8eead263749dd9c6e6a3aaff
David Turner <turne540@msu.edu>
2024-10-29 11:24:03 -0400

    Made sure the GMR pipeline is imported in the tools.clusters init - making it easier for the user to import later on. For issue #1137

--- xga/tools/clusters/__init__.py
+++ xga/tools/clusters/__init__.py
@@ -1,4 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 13/04/2023, 14:51. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 29/10/2024, 10:24. Copyright (c) The Contributors
 
+from .GMR import gas_mass_radius_pipeline
 from .LT import luminosity_temperature_pipeline
