2dc01c81a80c12cdec1ed4a48b7424be61f66b2e
David Turner <turne540@msu.edu>
2024-11-19 11:28:12 -0500

    Starting to get the new hydrostatic mass profile to full functionality - the first part of the 'mass' method is essentially the same as the entropy method of SpecificEntropy, handling all the different ways we can get density and temperature information. The next part thing need to is implement derivative calculation, then add the actual hydrostatic mass calculation (making sure to support radius uncertainties like the old one did). For issue #1260

--- xga/products/profile.py
+++ xga/products/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 19/11/2024, 09:57. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 19/11/2024, 11:28. Copyright (c) The Contributors
 
 from copy import copy
 from typing import Tuple, Union, List
@@ -2436,7 +2436,8 @@ class NewHydrostaticMass(BaseProfile1D):
         # Setting up a dictionary to store mass results in.
         self._masses = {}
 
-    def mass(self, radius: Quantity, conf_level: float = 68.2, radius_err: Quantity = None) -> Union[Quantity, Quantity]:
+    def mass(self, radius: Quantity, conf_level: float = 68.2,
+             radius_err: Quantity = None) -> Union[Quantity, Quantity]:
         """
         A method which will measure a hydrostatic mass and hydrostatic mass uncertainty within the given
         radius/radii. No corrections are applied to the values calculated by this method, it is just the vanilla
@@ -2455,7 +2456,10 @@ class NewHydrostaticMass(BaseProfile1D):
             the mass realization distribution.
         :rtype: Union[Quantity, Quantity]
         """
-        raise NotImplementedError
+        # TODO THE MAIN THING HERE THAT WILL BE DIFFERENT FROM THE REVAMPED ENTROPY CALCULATIONS IS THAT WE NEED TO
+        #  CALCULATE DERIVATIVES - SHOULDN'T BE TOO MUCH OF A CHALLENGE AND WE SHOULD BE ABLE TO PRESERVE ALL THE
+        #  OPTIONS THAT WE INCLUDED FOR ENTROPY (INTERPOLATION, DATA DRIVEN, ETC.)
+
         # Setting the upper and lower confidence limits
         upper = 50 + (conf_level / 2)
         lower = 50 - (conf_level / 2)
@@ -2463,36 +2467,65 @@ class NewHydrostaticMass(BaseProfile1D):
         # Prints a warning if the radius at which to calculate the entropy is outside the range of the data
         self.rad_check(radius)
 
-        # If a particular radius already has a result in the profiles storage structure then we'll just grab that
-        #  rather than redoing a calculation unnecessarily.
-        if radius.isscalar and radius in self._entropies:
+        # We need check that, if the user has passed uncertainty information on radii, it is how we expect it to be.
+        #  First off, are there the right number of entries?
+        if not radius.isscalar and radius_err is not None and (radius_err.isscalar or len(radius) != len(radius_err)):
+            raise ValueError("If a set of radii are passed, and radius uncertainty information is provided, the "
+                             "'radius_err' argument must contain the same number of entries as the 'radius' argument.")
+        # Same deal here, if only one radius is passed, only one error may be passed
+        elif radius.isscalar and radius_err is not None and not radius_err.isscalar:
+            raise ValueError("When a radius uncertainty ('radius_err') is passed for a single radius value, "
+                             "'radius_err' must be scalar.")
+        # Now we check that the units of the radius and radius error quantities are compatible
+        elif radius_err is not None and not radius_err.unit.is_equivalent(radius.unit):
+            raise UnitConversionError("The radius_err quantity must be in units that are equivalent to units "
+                                      "of {}.".format(radius.unit.to_string()))
+
+        # Now we make absolutely sure that the radius error(s) are in the correct units
+        if radius_err is not None:
+            radius_err = radius_err.to(self.radii_unit)
+
+        # Here we construct the storage key for the radius passed, and the uncertainty if there is one
+        if radius.isscalar and radius_err is None:
+            stor_key = str(radius.value) + " " + str(radius.unit)
+        elif radius.isscalar and radius_err is not None:
+            stor_key = str(radius.value) + '_' + str(radius_err.value) + " " + str(radius.unit)
+        # In this case, as the radius is not scalar, the masses won't be stored so we don't need a storage key
+        else:
+            stor_key = None
+
+        # If a particular radius+radius error (if passed) already has a result in the profiles storage structure
+        #  then we'll just grab that rather than redoing a calculation unnecessarily.
+
+        # Check to see whether the calculation has to be run again
+        if radius.isscalar and stor_key in self._masses:
             already_run = True
-            ent_dist = self._entropies[radius]
+            mass_dist = self._masses[stor_key]
         else:
             already_run = False
 
-        # Here, if we haven't already identified a previously calculated entropy for the radius, we start to
+        # Here, if we haven't already identified a previously calculated hydrostatic mass for the radius, we start to
         #  prepare the data we need (i.e. temperature and density). This is complicated slightly by the different
-        #  ways of calculating entropy we support (using smooth models, using data points, using interpolated data
-        #  points). First of all we deal with the case of there being a density model to draw from
+        #  ways of calculating the profile that we now support (using smooth models, using data points, using
+        #  interpolated data points). First of all we deal with the case of there being a density model to draw from
         if not already_run and self.density_model is not None:
             # If the density model fit didn't work then we give up and throw an error
             if not self.density_model.success:
-                raise XGAFitError("The density model fit was not successful, as such we cannot calculate entropy "
-                                  "using a smooth density model.")
-            # Getting a bunch of realisations (with the number set by the 'num_samples' argument that was passed on
+                raise XGAFitError("The density model fit was not successful, as such we cannot calculate "
+                                  "hydrostatic mass using a smooth density model.")
+            # Getting a bunch of realizations (with the number set by the 'num_samples' argument that was passed on
             #  the definition of this source of the model.
             dens = self._dens_model.get_realisations(radius)
 
-        # In this rare case (inspired by how ACCEPT packaged their profiles, see issue #1176) the radii for the
-        #  temperature and density profiles are identical, and so we just get some realisations
+        # In this rare case the radii for the temperature and density profiles are identical, and so we just get
+        #  some realizations
         elif (not already_run and (len(self.density_profile) == len(self.temperature_profile)) and
               (self.density_profile.radii == self.temperature_profile.radii).all()):
             dens = self.density_profile.generate_data_realisations(self._num_samples).T
 
         elif not already_run and self._interp_data:
-            # This uses the density profile y-axis values (and their uncertainties) to draw N realisations of the
-            #  data points - we'll use this to create N realisations of the interpolations as well
+            # This uses the density profile y-axis values (and their uncertainties) to draw N realizations of the
+            #  data points - we'll use this to create N realizations of the interpolations as well
             dens_data_real = self.density_profile.generate_data_realisations(self._num_samples)
             # TODO This unfortunately may be removed from scipy soon, but the np.interp linear interpolation method
             #  doesn't currently support interpolating along a particular axis. Also considering more sophisticated
@@ -2501,12 +2534,12 @@ class NewHydrostaticMass(BaseProfile1D):
             # We make sure to turn on extrapolation, and make sure this is no out-of-bounds error issued
             dens_interp = interp1d(self.density_profile.radii, dens_data_real, axis=1, assume_sorted=True,
                                    fill_value='extrapolate', bounds_error=False)
-            # Restore the interpolated density profile realisations to an astropy quantity array
+            # Restore the interpolated density profile realizations to an astropy quantity array
             dens = Quantity(dens_interp(self.radii).T, self.density_profile.values_unit)
 
         # This particular combination means that we are doing a data-point based profile, but without interpolation,
         #  and that the density profile has more bins than the temperature (going to be true in most cases). So we
-        #  just read out the density data points (and make N realisations of them) with no funny business required
+        #  just read out the density data points (and make N realizations of them) with no funny business required
         elif not already_run and not self._interp_data and len(self.density_profile) == len(self.radii):
             dens = self.density_profile.generate_data_realisations(self._num_samples).T
         else:
@@ -2523,25 +2556,24 @@ class NewHydrostaticMass(BaseProfile1D):
             dens = dens / (MEAN_MOL_WEIGHT * m_p)
 
         # We now essentially repeat the process we just did with the density profiles, constructing the temperature
-        #  values that we are going to use in our entropy measurements; from models, data points, or interpolating
-        #  from data points
+        #  values that we are going to use in our hydrostatic mass measurements; from models, data points, or
+        #  interpolating from data points
         if not already_run and self.temperature_model is not None:
             if not self.temperature_model.success:
                 raise XGAFitError("The temperature model fit was not successful, as such we cannot calculate entropy "
                                   "using a smooth temperature model.")
-            # Getting a bunch of realisations (with the number set by the 'num_samples' argument that was passed on
+            # Getting a bunch of realizations (with the number set by the 'num_samples' argument that was passed on
             #  the definition of this source of the model.
             temp = self._temp_model.get_realisations(radius)
 
-        # In this rare case (inspired by how ACCEPT packaged their profiles, see issue #1176) the radii for the
-        #  temperature and density profiles are identical, and so we just get some realisations
+        # In this rare case temperature and density profiles are identical, and so we just get some realizations
         elif (not already_run and (len(self.density_profile) == len(self.temperature_profile)) and
               (self.density_profile.radii == self.temperature_profile.radii).all()):
             temp = self.temperature_profile.generate_data_realisations(self._num_samples).T
 
         elif not already_run and self._interp_data:
-            # This uses the temperature profile y-axis values (and their uncertainties) to draw N realisations of the
-            #  data points - we'll use this to create N realisations of the interpolations as well
+            # This uses the temperature profile y-axis values (and their uncertainties) to draw N realizations of the
+            #  data points - we'll use this to create N realizations of the interpolations as well
             temp_data_real = self.temperature_profile.generate_data_realisations(self._num_samples)
             temp_interp = interp1d(self.temperature_profile.radii, temp_data_real, axis=1, assume_sorted=True,
                                    fill_value='extrapolate', bounds_error=False)
@@ -2554,7 +2586,7 @@ class NewHydrostaticMass(BaseProfile1D):
         # And here, the final option, we're doing a data-point based profile without interpolation, and we need
         #  to make sure that the density values (here N_denspoints > N_temppoints) each have a corresponding
         #  temperature value - in practise this means that each density will be paired with the temperature
-        #  realisations whose radial coverage they fall within.
+        #  realizations whose radial coverage they fall within.
         else:
             t_bnds = np.vstack([self.temperature_profile.annulus_bounds[0:-1],
                                 self.temperature_profile.annulus_bounds[1:]]).T
@@ -2567,6 +2599,10 @@ class NewHydrostaticMass(BaseProfile1D):
         # We ensure the temperatures are in the right unit
         if not already_run and not temp.unit.is_equivalent('keV'):
             temp = (temp * k_B).to('keV')
+
+
+        raise NotImplementedError("The method is not complete beyond this point")
+
 
         # And now we do the actual entropy calculation
         if not already_run:
