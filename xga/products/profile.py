ae110a4d20ccbe52633ecdf8845aa7b1853dd84d
David Turner <turne540@msu.edu>
2024-11-20 10:00:06 -0500

    Added an 'allow_unphysical' flag to the declaration of the new mass profile class - means no error will be raised if negative mass is measured. Indirectly for issue #1260 (will now do the same for entropy whilst I think about it).

--- xga/products/profile.py
+++ xga/products/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/11/2024, 09:48. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 20/11/2024, 10:00. Copyright (c) The Contributors
 
 from copy import copy
 from typing import Tuple, Union, List
@@ -2184,6 +2184,8 @@ class NewHydrostaticMass(BaseProfile1D):
         models, this controls whether the data profile with the coarser bins is interpolated, or whether the other
         profile's data points are matched with the value that was measured for the radial region they
         are in (the default).
+    :param bool allow_unphysical: This controls whether unphysical mass results are 'allowed' without an
+            exception being raised (e.g. if a calculated mass value is negative). Default is False.
     :param bool auto_save: Whether the profile should automatically save itself to disk at any point. The default is
         False, but all profiles generated through XGA processes acting on XGA sources will auto-save.
     """
@@ -2193,7 +2195,8 @@ class NewHydrostaticMass(BaseProfile1D):
                  density_model: Union[str, BaseModel1D] = None, radii: Quantity = None, radii_err: Quantity = None,
                  deg_radii: Quantity = None, fit_method: str = "mcmc", num_walkers: int = 20,
                  num_steps: [int, List[int]] = 20000, num_samples: int = 1000, show_warn: bool = True,
-                 progress: bool = True, interp_data: bool = False, auto_save: bool = False):
+                 progress: bool = True, interp_data: bool = False, allow_unphysical: bool = False,
+                 auto_save: bool = False):
         """
         A profile product which uses input temperature and density profiles to calculate a cumulative hydrostatic mass
         profile - used in galaxy cluster analyses (https://ui.adsabs.harvard.edu/abs/2024arXiv240307982T/abstract
@@ -2248,6 +2251,8 @@ class NewHydrostaticMass(BaseProfile1D):
             fitted models, this controls whether the data profile with the coarser bins is interpolated, or whether
             the other profile's data points are matched with the value that was measured for the radial region they
             are in (the default).
+        :param bool allow_unphysical: This controls whether unphysical mass results are 'allowed' without an
+            exception being raised (e.g. if a calculated mass value is negative). Default is False.
         :param bool auto_save: Whether the profile should automatically save itself to disk at any point. The
             default is False, but all profiles generated through XGA processes acting on XGA sources will auto-save.
         """
@@ -2407,6 +2412,10 @@ class NewHydrostaticMass(BaseProfile1D):
         #  we also use that value for the number of data realizations if the user has opted for a data point derived
         #  hydrostatic mass profile rather than model derived.
         self._num_samples = num_samples
+
+        # A simple flag that controls whether the 'mass()' method will raise an exception if an unphysical mass is
+        #  calculated, or if it will let it go through without an exception
+        self._allow_unphysical = allow_unphysical
 
         mass, mass_dist = self.mass(radii, conf_level=68, radius_err=radii_err)
         mass_vals = mass[0, :]
@@ -2686,8 +2695,8 @@ class NewHydrostaticMass(BaseProfile1D):
         mass_res = Quantity(np.array([mass_med.value, mass_lower.value, mass_upper.value]), mass_dist.unit)
 
         # We check to see if any of the upper limits (i.e. measured value plus +ve error) are below zero, and if so
-        #  then we throw an exception up
-        if np.any((mass_res[0] + mass_res[1]) < 0):
+        #  then we throw an exception up (if the profile is set to do that - it is the default behaviour).
+        if not self._allow_unphysical and np.any((mass_res[0] + mass_res[1]) < 0):
             raise ValueError("A mass upper limit (i.e. measured value plus +ve error) of less than zero has been "
                              "measured, which is not physical.")
 
