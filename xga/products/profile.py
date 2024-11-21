ae931963cf0dc2e184bbc29c9bb5c18746cb58de
David Turner <turne540@msu.edu>
2024-11-21 00:04:55 -0500

    nevermind - anyway just need to add the last derivatives of temperature and density. These are for the cases where there are more radii points for the mass profile than there are radii or density, and no interpolation. For issue #1260

--- xga/products/profile.py
+++ xga/products/profile.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/11/2024, 23:59. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 21/11/2024, 00:04. Copyright (c) The Contributors
 
 from copy import copy
 from typing import Tuple, Union, List
@@ -2671,10 +2671,7 @@ class NewHydrostaticMass(BaseProfile1D):
             mass_dist = (((-1 * k_B * np.power(radius[..., None], 2)) / (dens * (MEAN_MOL_WEIGHT * m_p) * G))
                             * ((dens * temp_der) + (temp * dens_der)))
 
-            # TODO THIS IS A TEST OF A THOUGHT I HAD - MAYBE REMOVE?
-            mass_dist[mass_dist < 0] = np.nan
             # Just converts the mass/masses to the unit we normally use for them
-
             mass_dist = mass_dist.to('Msun').T
 
             # Storing the result if it is for a single radius
