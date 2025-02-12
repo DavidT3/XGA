9ed650b6f93d4d22964211c7fceec952a2678430
Jessica Pilling <jp735@sussex.ac.uk>
2025-02-12 17:10:51 +0000

    seperating code into easier to read functions

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,17 +16,10 @@ from daxa.process.simple import full_process_xmm, full_process_erosita
 # place, it will also not delete the xga_output folder in /tests/test_data
 # TEST_MODE = RUN, this assumes that no data has been downloaded and will delete the xga_output
 # folder after running the tests, and will delete all the data that has been downloaded
-
 TEST_MODE = 'DEV'
 
-#sys.path.append(os.path.abspath("..") + 'xga/')
-
-# Any useful constants
-A907_LOC = Quantity([149.59209, -11.05972], 'deg')
-im_path = os.path.join(os.path.abspath("."), "test_data/0201903501/images/0201903501_pn_exp1-0.50-2.00keVimg.fits")
-exp_path = os.path.join(os.path.abspath("."), "test_data/0201903501/images/0201903501_pn_exp1-0.50-2.00keVexpmap.fits")
-A907_IM_PN_INFO = [im_path, '0201903501', 'pn', '', '', '', Quantity(0.5, 'keV'), Quantity(2.0, 'keV')]
-A907_EX_PN_INFO = [exp_path, '0201903501', 'pn', '', '', '', Quantity(0.5, 'keV'), Quantity(2.0, 'keV')]
+# This is the main source for running the tests on
+SRC_INFO = {'ra': 226.0318, 'dec': -2.8046, 'z': 0.2093, 'name': "1eRASS_J150407.6-024816"}
 
 def write_config(cwd, module):
     """
@@ -106,35 +99,29 @@ def move_og_cfg(module):
     # Then move the test config file there
     shutil.move(f'./tests/test_data/{module}.cfg', config_path)
 
-def obtain_test_data(src_ra, src_dec):
-    # For some reason error where happening when I used the path '~/.config' so I need to use expanduser
-    # to return the absolute path to the home directory
-    home_dir = os.path.expanduser("~")
-    # xga_config_path is the absolute path to ~/.config/daxa
-    daxa_config_path = home_dir + "/.config/daxa"
-
-    # Now we need to move the original files that where in ~/.config/daxa elsewhere, because the testing
-    # files will have to go into ~/.config/daxa
-    if os.path.exists(daxa_config_path):
-        # this will move the original file to the tests directory
-        print('moving original /daxa to tests dir')
-        shutil.move(daxaconfig_path, './tests/')
-
-    print('remaking the xga dir')
-    # Then we want to remake the config/xga directory
-    os.makedirs(daxa_config_path)
-    print('moving the test config file to the .config/daxa dir')
-    # Then move the test config file there
-    shutil.move('./tests/test_data/daxa.cfg', daxa_config_path)
-
+def obtain_test_data():
+    """
+    Uses DAXA to download the test data for the source. This processes the data too.
+    """
     xm = XMMPointed()
     er = eRASS1DE()
-    position = SkyCoord(src_ra, src_dec, unit='deg')
-    xm.filter_on_positions(position)
+    position = SkyCoord(SRC_INFO['ra'], SRC_INFO['dec'], unit='deg')
 
+    xm.filter_on_positions(position)
+    er.filter_on_positions(position)
+
+    arch = Archive('xga_tests')
+
+    full_process_erosita(arch)
+    full_process_xmm(arch)
+    
 # TODO work out how to run this before all the tests in different files
 # This should run before all of the tests are run
-def set_up_tests(TEST_MODE):
+def set_up_tests():
+    """
+    This moves original config files, creates test config files. It then also downloads data if
+    TEST_MODE is set to RUN.
+    """
     cwd = os.getcwd()
 
     move_og_cfg('xga')
@@ -145,8 +132,7 @@ def set_up_tests(TEST_MODE):
         write_config(cwd, 'daxa')
         obtain_test_data()
 
-# TODO work out how to run this after all the tests have been run in different files
-def restore_og_cfg():
+def restore_og_cfg(module):
     """
     Restore the user's original xga config setup after tests are run.
     """
@@ -154,12 +140,31 @@ def restore_og_cfg():
     # to return the absolute path to the home directory
     home_dir = os.path.expanduser("~")
     # xga_config_path is the absolute path to ~/.config/xga
-    xga_config_path = home_dir + "/.config/xga"
+    config_path = home_dir + f"/.config/{module}"
 
-    if os.path.exists(xga_config_path):
+    if os.path.exists(config_path):
         print('deleting test config dir')
         # Then we delete the test config file
-        shutil.rmtree(xga_config_path)
+        shutil.rmtree(config_path)
         print('moving the original config file back')
         # And then move the original back
-        shutil.move('./tests/xga', home_dir + '/.config')
+        shutil.move(f'./tests/{module}', home_dir + '/.config')
+
+def clean_up_test_files():
+    """
+    This is run after all the tests to remove the extra files that have been created during the 
+    tests, only if TEST_MODE == RUN.
+    """
+    if os.path.exists("./tests/xga_output"):
+        shutil.rmtree("./tests/xga_output")
+    if os.path.exists("./tests/test_data/xga_tests"):
+        shutil.rmtree("./tests/test_data/xga_tests")
+
+# TODO work out how to run this after all the tests have been run in different files
+def clean_up_tests():
+    restore_og_cfg('xga')
+
+    if TEST_MODE == 'RUN':
+        restore_og_cfg('daxa')
+        clean_up_test_files()
+
