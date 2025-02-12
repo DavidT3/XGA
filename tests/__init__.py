f11453a83c392c1d3fad06935d2302f35be055db
Jessica Pilling <jp735@sussex.ac.uk>
2025-02-12 16:33:09 +0000

    first draft of test __init__ which needs to be neatened up before official release

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,6 +9,15 @@ from astropy.units import Quantity
 from astropy.coordinates import SkyCoord
 from daxa.mission import XMMPointed, eRASS1DE
 from daxa.archive import Archive
+from daxa.process.simple import full_process_xmm, full_process_erosita
+
+# Tests can be run on two modes which is controlled here
+# TEST_MODE = DEV, this assumes that the data to test on is already downloaded and in the right 
+# place, it will also not delete the xga_output folder in /tests/test_data
+# TEST_MODE = RUN, this assumes that no data has been downloaded and will delete the xga_output
+# folder after running the tests, and will delete all the data that has been downloaded
+
+TEST_MODE = 'DEV'
 
 #sys.path.append(os.path.abspath("..") + 'xga/')
 
@@ -19,78 +28,85 @@ exp_path = os.path.join(os.path.abspath("."), "test_data/0201903501/images/02019
 A907_IM_PN_INFO = [im_path, '0201903501', 'pn', '', '', '', Quantity(0.5, 'keV'), Quantity(2.0, 'keV')]
 A907_EX_PN_INFO = [exp_path, '0201903501', 'pn', '', '', '', Quantity(0.5, 'keV'), Quantity(2.0, 'keV')]
 
-def set_up_test_config():
-    def write_xga_config(cwd):
-        """
-        Writes an xga config file with paths specifically for the unit tests.
+def write_config(cwd, module):
+    """
+    Writes a config file written for the unit tests for daxa or xga.
 
-        :param str cwd: The absolute path of the current working directory - which when running tests
-            should be the level above the tests directory.
-        """
-        # Creating the configuration content
+    :param str cwd: The absolute path of the current working directory - which when running tests
+        should be the level above the tests directory.
+    :param str module: The module to write the config for - either 'daxa' or 'xga'
+    """
+
+    if module == 'xga':
         config_content = f"""[XGA_SETUP]
         xga_save_path = {cwd}/tests/test_data/xga_output
         num_cores = 1
 
         [XMM_FILES]
-        root_xmm_dir = {cwd}/tests/test_data/xmm/
-        clean_pn_evts = {{obs_id}}/eclean/pn_exp1_clean_evts.fits
-        clean_mos1_evts = {{obs_id}}/eclean/mos1_exp1_clean_evts.fits
-        clean_mos2_evts = {{obs_id}}/eclean/mos2_exp1_clean_evts.fits
-        attitude_file = {{obs_id}}/epchain/P{{obs_id}}OBX000ATTTSR0000.FIT
+        root_xmm_dir = {cwd}/tests/test_data/xga_tests/archives/processed_data/xmm_pointed/
+        clean_pn_evts = {{obs_id}}/events/obsid{{obs_id}}-instPN-subexpALL-en-finalevents.fits
+        clean_mos1_evts = {{obs_id}}/obsid{{obs_id}}-instM1-subexpALL-en-finalevents.fits
+        clean_mos2_evts = {{obs_id}}/obsid{{obs_id}}-instM2-subexpALL-en-finalevents.fits
+        attitude_file = {{obs_id}}/P{{obs_id}}OBX000ATTTSR0000.FIT
         lo_en = ['0.50', '2.00']
         hi_en = ['2.00', '10.00']
-        pn_image = {{obs_id}}/images/{{obs_id}}_pn_exp1-{{lo_en}}-{{hi_en}}keVimg.fits
-        mos1_image = {{obs_id}}/images/{{obs_id}}_mos1_exp1-{{lo_en}}-{{hi_en}}keVimg.fits
-        mos2_image = {{obs_id}}/images/{{obs_id}}_mos2_exp1-{{lo_en}}-{{hi_en}}keVimg.fits
-        pn_expmap = {{obs_id}}/images/{{obs_id}}_pn_exp1-{{lo_en}}-{{hi_en}}keVexpmap.fits
-        mos1_expmap = {{obs_id}}/images/{{obs_id}}_mos1_exp1-{{lo_en}}-{{hi_en}}keVexpmap.fits
-        mos2_expmap = {{obs_id}}/images/{{obs_id}}_mos2_exp1-{{lo_en}}-{{hi_en}}keVexpmap.fits
-        region_file = {{obs_id}}_sources.reg
+        pn_image = {{obs_id}}/images/obsid{{obs_id}}-instPN-subexpALL-en{{lo_en}}_{{hi_en}}keV-image.fits
+        mos1_image = {{obs_id}}/images/obsid{{obs_id}}-instM1-subexpALL-en{{lo_en}}_{{hi_en}}keV-image.fits
+        mos2_image = {{obs_id}}/images/obsid{{obs_id}}-instM2-subexpALL-en{{lo_en}}_{{hi_en}}keV-image.fits
+        pn_expmap = {{obs_id}}/images/obsid{{obs_id}}-instPN-subexpALL-en{{lo_en}}_{{hi_en}}keV-expmap.fits
+        mos1_expmap = {{obs_id}}/images/obsid{{obs_id}}-instM1-subexpALL-en{{lo_en}}_{{hi_en}}keV-expmap.fits
+        mos2_expmap = {{obs_id}}/images/obsid{{obs_id}}-instM2-subexpALL-en{{lo_en}}_{{hi_en}}keV-expmap.fits
+        region_file = {cwd}/tests/test_data/src_regs/xmm/{{obs_id}}.reg
 
         [EROSITA_FILES]
-        root_erosita_dir = {cwd}/tests/test_data/erosita/
-        clean_erosita_evts = {{obs_id}}/{{obs_id}}-clean_evts.fits
+        root_erosita_dir = {cwd}/tests/test_data/xga_tests/archives/processed_data/erosita_all_sky_de_dr1/
+        clean_erosita_evts = {{obs_id}}/events/obsid{{obs_id}}-instTM1_TM2_TM3_TM4_TM5_TM6_TM7-subexpALL-en0.2_10.0keV-finalevents.fits
         lo_en = ['0.5']
         hi_en = ['2.0']
-        erosita_image = {{obs_id}}/{{obs_id}}_bin_87_{{lo_en}}-{{hi_en}}keVimg.fits
-        erosita_expmap = {{obs_id}}/{{obs_id}}_combined_{{lo_en}}-{{hi_en}}keVexpmap.fits
-        region_file = {{obs_id}}/{{obs_id}}.reg
+        erosita_image = /this/is/optional/
+        erosita_expmap = /this/is/optional/
+        region_file = {cwd}/tests/test_data/src_regs/erosita/{{obs_id}}.reg
         """
+    elif module == 'daxa':
+        config_content = f"""[DAXA_SETUP]
+        daxa_save_path = {cwd}/tests/test_data/
+        num_cores = 1"""
+    else:
+        raise ValueError('Something has gone wrong')
 
-        # Writing the content to the specified file
-        with open('tests/test_data/xga.cfg', 'w') as file:
-            file.write(config_content)
+    # Writing the content to the specified file
+    with open(f'tests/test_data/{module}.cfg', 'w') as file:
+        file.write(config_content)    
 
-    # Actually writing the test config file
-    cwd = os.getcwd()
-    write_xga_config(cwd)
+def move_og_cfg(module):
+    """
+    If the user already has XGA and DAXA installed, this function will move them to 
+    /tests/og_configs so that new configs for unit tests can be written there.
 
-    # For some reason error where happening when I used the path '~/.config' so I need to use expanduser
-    # to return the absolute path to the home directory
+    :param str module:
+        This is either xga or daxa.
+    """
+    # For some reason an error is happening when I used the path '~/.config' so I need to use 
+    # expanduser to return the absolute path to the home directory
     home_dir = os.path.expanduser("~")
-    # xga_config_path is the absolute path to ~/.config/xga
-    xga_config_path = home_dir + "/.config/xga"
+    # config_path is the absolute path to ~/.config/{module}
+    config_path = home_dir + "/.config/{module}"
 
-    # Now we need to move the original files that where in ~/.config/xga elsewhere, because the testing
-    # files will have to go into ~/.config/xga
-    if os.path.exists(xga_config_path):
-        # this will move the original file to the tests directory
-        print('moving original /xga to tests dir')
-        shutil.move(xga_config_path, './tests/')
+    # Now we need to move the original files that where in ~/.config/{module} elsewhere, because the 
+    # testing files will have to go into ~/.config/{module}
+    if os.path.exists(config_path):
+        # this will move the original file to /tests/og_configs
+        print(f'moving original {module} to /tests/og_configs')
+        shutil.move(config_path, './tests/og_configs')
 
-    print('remaking the xga dir')
+    print(f'remaking the {module} dir')
     # Then we want to remake the config/xga directory
-    os.makedirs(xga_config_path)
-    print('moving the test config file to the .config/xga dir')
+    os.makedirs(config_path)
+    print(f'moving the test config file to the .config/{module} dir')
     # Then move the test config file there
-    shutil.move('./tests/test_data/xga.cfg', xga_config_path)
+    shutil.move(f'./tests/test_data/{module}.cfg', config_path)
 
 def obtain_test_data(src_ra, src_dec):
-    # The user might have daxa installed already, so we will temporarily make a new cfg file if ythe
-    cwd = os.getcwd()
-    write_daxa_config(cwd)
-
     # For some reason error where happening when I used the path '~/.config' so I need to use expanduser
     # to return the absolute path to the home directory
     home_dir = os.path.expanduser("~")
@@ -115,11 +131,21 @@ def obtain_test_data(src_ra, src_dec):
     er = eRASS1DE()
     position = SkyCoord(src_ra, src_dec, unit='deg')
     xm.filter_on_positions(position)
-    er.filter_on_positions(position, search_distance=Quantity(3.6, 'deg'))
-    arch = Archive('xga_tests', [er, xm])
 
+# TODO work out how to run this before all the tests in different files
+# This should run before all of the tests are run
+def set_up_tests(TEST_MODE):
+    cwd = os.getcwd()
 
-# This will be run in the tearDownClass method, which will happen even if tests fail
+    move_og_cfg('xga')
+    write_config(cwd, 'xga')
+
+    if TEST_MODE == 'RUN':
+        move_og_cfg('daxa')
+        write_config(cwd, 'daxa')
+        obtain_test_data()
+
+# TODO work out how to run this after all the tests have been run in different files
 def restore_og_cfg():
     """
     Restore the user's original xga config setup after tests are run.
