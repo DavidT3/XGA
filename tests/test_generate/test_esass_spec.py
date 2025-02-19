53304a48cf083f14d69a4848229c88b3eca2f4e7
Jessica Pilling <jp735@sussex.ac.uk>
2025-02-19 14:32:36 +0000

    testing esass srctool_spectrum

--- tests/test_generate/test_esass_spec.py
+++ tests/test_generate/test_esass_spec.py
@@ -9,6 +9,7 @@ from astropy.units import Quantity
 import xga
 from xga.sources import GalaxyCluster
 from xga.generate.esass.spec import srctool_spectrum
+from xga.products import Spectrum
 
 from .. import SRC_INFO
 
@@ -27,7 +28,59 @@ class TestTempFuncs(unittest.TestCase):
                                               use_peak=False, search_distance={'erosita': 
                                               Quantity(3.6, 'deg')})
 
-    def test_srctool_spectrum(self):
-        pass
+    def test_srctool_spectrum_combine_insts_f_combine_obs_f(self):
+        """
+        Testing srctool_spectrum with the arguments combine_insts=False and combine_obs=False
+        """
+        srctool_spectrum(self.test_src, 'r500', combine_tm=False, combine_obs=False)
+
+        spec = self.test_src.get_spectra('r500', telescope='erosita', inst='TM1')
+
+        assert isinstance(spec, Spectrum)
+        assert spec.telescope == 'erosita'
+        assert spec.obs_id != 'combined'
+        assert spec.instrument == 'TM1'
+
+    def test_srctool_spectrum_combine_insts_t_combine_obs_f(self):
+        """
+        Testing srctool_spectrum with the arguments combine_insts=True and combine_obs=False
+        """
+        srctool_spectrum(self.test_src, 'r500', combine_tm=True, combine_obs=False)
+
+        spec = self.test_src.get_spectra('r500', telescope='erosita', inst='combined')
+
+        assert isinstance(spec, Spectrum)
+        assert spec.telescope == 'erosita'
+        assert spec.obs_id != 'combined'
+        assert spec.instrument == 'combined'
+
+    def test_srctool_spectrum_combine_insts_f_combine_obs_t(self):
+        """
+        Testing srctool_spectrum with the arguments combine_insts=False and combine_obs=True
+        """
+        srctool_spectrum(self.test_src, 'r500', combine_tm=False, combine_obs=True)
+
+        spec = self.test_src.get_combined_spectra('r500', telescope='erosita', inst='TM1')
+
+        assert isinstance(spec, Spectrum)
+        assert spec.telescope == 'erosita'
+        assert spec.obs_id == 'combined'
+        assert spec.instrument == 'TM1'
+
+    def test_srctool_spectrum_combine_insts_t_combine_obs_t(self):
+        """
+        Testing srctool_spectrum with the arguments combine_insts=True and combine_obs=True
+        """
+        srctool_spectrum(self.test_src, 'r500', combine_tm=True, combine_obs=True)
+
+        spec = self.test_src.get_combined_spectra('r500', telescope='erosita')
+
+        assert isinstance(spec, Spectrum)
+        assert spec.telescope == 'erosita'
+        assert spec.obs_id == 'combined'
+        assert spec.instrument == 'combined'
+    
+
+
 
 # TODO test that annular spectra with combined obs but individual instruments are stored correctly
