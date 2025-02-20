46957f4b418a6558d0c27d0955dd89cfb7a75999
Jessica Pilling <jp735@sussex.ac.uk>
2025-02-20 13:44:32 +0000

    testing that an old bug has been fixed

--- tests/test_generate/test_esass_phot.py
+++ tests/test_generate/test_esass_phot.py
@@ -3,10 +3,10 @@ import unittest
 from astropy.units import Quantity
 
 import xga
-from xga.sources import GalaxyCluster
+from xga.samples import ClusterSample
 from xga.generate.esass.phot import evtool_image, expmap
 
-from .. import SRC_ALL_TELS
+from .. import SRC_ALL_TELS, SRC_INFO, SUPP_SRC_INFO
 
 class TestEsassPhotFuncs(unittest.TestCase):
     def test_evtool_image(self):
@@ -48,6 +48,22 @@ class TestEsassPhotFuncs(unittest.TestCase):
         assert exp.telescope == 'erosita'
         assert exp.energy_bounds[0] == Quantity(0.5, 'keV')
         assert exp.energy_bounds[1] == Quantity(3, 'keV')
+    
+    def test_evtool_image_w_sample_w_odd_telescopes(self):
+        """
+        There was an old bug that occured when product generation functions were run with samples
+        with sources that didnt all have the same telescopes. So this is testing that this bug has
+        been fixed!
+        """
+        test_smp = ClusterSample([SRC_INFO['ra'], SUPP_SRC_INFO['dec']],
+                                [SRC_INFO['dec'], SUPP_SRC_INFO['dec']],
+                                [SRC_INFO['z'], SUPP_SRC_INFO['z']],
+                                r500=Quantity([500, 500], 'kpc'),
+                                name=[SRC_INFO['name'], SUPP_SRC_INFO['name']],
+                                use_peak=False)
+        
+        test_smp[0].disassociate_obs('erosita')
+        evtool_image(test_smp)
 
 if __name__ == "__main__":
      unittest.main()
