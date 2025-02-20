cae6266e5d07def1ff47bf26382b20eb9f42a8fd
Jessica Pilling <jp735@sussex.ac.uk>
2025-02-20 13:40:09 +0000

    testing clustersample

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,22 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
 #  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
 from astropy.units import Quantity
-from astropy.coordinates import SkyCoord
-from daxa.mission import XMMPointed, eRASS1DE
-from daxa.archive import Archive
-from daxa.process.simple import full_process_xmm, full_process_erosita
+
+from xga.sources import GalaxyCluster
 
 # Tests can be run on two modes which is controlled here
 # TEST_MODE = DEV, this assumes that the data to test on is already downloaded and in the right 
 # place, it will also not delete the xga_output folder in /tests/test_data
 # TEST_MODE = RUN, this assumes that no data has been downloaded and will delete the xga_output
 # folder after running the tests, and will delete all the data that has been downloaded
-TEST_MODE = 'DEV'
+TEST_MODE = 'RUN'
 
 # This is the main source for running the tests on
 SRC_INFO = {'ra': 226.0318, 'dec': -2.8046, 'z': 0.2093, 'name': "1eRASS_J150407.6-024816"}
+
+# This is another cluster that we can use to test ClusterSample objects
+SUPP_SRC_INFO = {'ra': 226.03181, 'dec': -2.80458, 'z': 0.209, 'name': '1eRASS J150407.6-024816'}
+
+SRC_ALL_TELS = GalaxyCluster(SRC_INFO['RA'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
+                                     name=SRC_INFO['name'], use_peak=False,
+                                     search_distance={'erosita': Quantity(3.6, 'deg')})
---
+++ tests/test_samples/test_base.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+
+from astropy.units import Quantity
+
+from xga.samples import ClusterSample
+
+from .. import SRC_INFO, SUPP_SRC_INFO
+
+class TestBaseSample(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.test_smp = ClusterSample([SRC_INFO['ra'], SUPP_SRC_INFO['dec']],
+                                     [SRC_INFO['dec'], SUPP_SRC_INFO['dec']],
+                                     [SRC_INFO['z'], SUPP_SRC_INFO['z']],
+                                     r500=Quantity([500, 500], 'kpc'),
+                                     name=[SRC_INFO['name'], SUPP_SRC_INFO['name']],
+                                     use_peak=False)
+
+    def test_smp_names(self):
+        assert set([SRC_INFO['name'], SUPP_SRC_INFO['name']]) == set(self.test_smp.names)
+
+    def test_smp_redshifts(self):
+        assert set([SRC_INFO['z'], SUPP_SRC_INFO['z']]) == set(self.test_smp.redshifts)
+
+    def test_smp_telescopes(self):
+        assert set(['erosita', 'xmm']) == set(self.test_smp.telescopes)
+    
+    def test_smp_src_telescopes(self):
+        assert self.test_smp.src_telescopes[SRC_INFO['name']] == set(['xmm', 'erosita'])
+        assert self.test_smp.src_telescopes[SUPP_SRC_INFO['name']] == set(['xmm', 'erosita'])
