55b1c49c78498893e6e88cb3d884e7e2a8ccec1f
David Turner <turne540@msu.edu>
2025-02-28 13:57:50 -0500

    Fixed an error in an error message, which wouldn't display properly if only one telescope were associated with a source.

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 13:55. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 13:57. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -269,7 +269,8 @@ class BaseSource:
         cur_obs_nums = {tel: len(self._products[tel]) for tel in self._products}
         if sum(cur_obs_nums.values()) == 0:
             raise NoValidObservationsError("None of the {t} observations identified for this {s} have valid event "
-                                           "lists associated with them.".format(s=self.name, t='/'.join(telescope)))
+                                           "lists associated with them.".format(s=self.name,
+                                                                                t=', '.join(list(obs.keys()))))
         elif 0 in cur_obs_nums.values():
             # Cut out any mention of a telescope with no loaded files
             new_obs = {tel: obs[tel] for tel, num in cur_obs_nums.items() if num != 0}
