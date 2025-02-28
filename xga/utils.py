ffa3662fdcf83e1c43e6829a9909f12013090fdc
David Turner <turne540@msu.edu>
2025-02-28 11:39:23 -0500

    Hopefully made the selective backend checks actually work - I'd forgotten the contents of the USABLE dictionary.

--- xga/utils.py
+++ xga/utils.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 11:37. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 11:39. Copyright (c) The Contributors
 
 import json
 import os
@@ -808,6 +808,7 @@ ABUND_TABLES = ["feld", "angr", "aneb", "grsa", "wilm", "lodd", "aspl"]
 # We will only check for software relevant to those telescopes which XGA setup has deemed usable
 # --- XMM ---
 SAS_VERSION = None
+SAS_AVAIL = False
 if 'xmm' in USABLE and USABLE['xmm']:
     # Next up, we check to see what version of SAS (if any) is installed - for the XMM-Newton mission
     # Here we check to see whether SAS is installed (along with all the necessary paths)
@@ -839,10 +840,10 @@ if 'xmm' in USABLE and USABLE['xmm']:
 
 # --- eROSITA ---
 ESASS_VERSION = None
+eSASS_AVAIL = False
 if ('erosita' in USABLE and USABLE['erosita']) or ('erass' in USABLE and USABLE['erass']):
     # TODO will have to handle the whole eSASS4DR1 and the original eSASS release thing at some point... sigh
     # This checks for an installation of eSASS
-    eSASS_AVAIL = False
     if shutil.which("evtool") is None:
         warn("No eSASS installation detected on system, as such all functions in xga.generate.esass will not work.",
              stacklevel=2)
@@ -856,6 +857,10 @@ if ('erosita' in USABLE and USABLE['erosita']) or ('erass' in USABLE and USABLE[
 CIAO_VERSION = None
 # This checks for an installation of Ciao
 CIAO_AVAIL = False
+# Now for the accompanying CALDB
+CALDB_VERSION = None
+CALDB_AVAIL = False
+
 if 'chandra' in USABLE and USABLE['chandra']:
     ciao_out, ciao_err = Popen("ciaover -v", stdout=PIPE, stderr=PIPE, shell=True).communicate()
     # Just turn those pesky byte outputs into strings
@@ -876,10 +881,6 @@ if 'chandra' in USABLE and USABLE['chandra']:
 
     # Finally, we check to see what version of CALDB (if any) is installed - for the Chandra mission
     # Here we check to see whether CALDB is installed (along with all the necessary paths)
-    CALDB_VERSION = None
-    # This checks for an installation of Ciao
-    CALDB_AVAIL = False
-
     if 'not installed' in split_out[5].lower():
         warn("A Chandra CALDB installation cannot be identified on your system, and as such "
              "Chandra data cannot be processed.", stacklevel=2)
