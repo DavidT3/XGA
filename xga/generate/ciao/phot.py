e5e5f33e9c2d5649835781e860f0c76336b2d3d5
David Turner <turne540@msu.edu>
2025-02-28 20:06:02 -0500

    Made sure there is a telescope entry in the extra_info dictionary from chandra_image_expmap, and also added the other two output product types to the sources_types list that is returned

--- xga/generate/ciao/phot.py
+++ xga/generate/ciao/phot.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 19:19. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 20:06. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -161,7 +161,8 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
                 "lo_en": lo_en,
                 "hi_en": hi_en,
                 "effective_energy": (lo_en + hi_en) / 2,
-                "bin_size": 4
+                "bin_size": 4,
+                "telescope": "chandra"
             })
 
         sources_cmds.append(np.array(cmds))
@@ -169,7 +170,8 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
         # This contains any other information that will be needed to instantiate the class
         # once the CIAO cmd has run.
         sources_extras.append(np.array(extra_info))
-        sources_types.append(np.full(len(cmds), "image"))
+        # If there are multiple output types then the numpy full call will need to specify the two dimensional shape
+        sources_types.append(np.full((len(cmds), 3), ["image", "expmap", "ratemap"]))
 
     # I only return num_cores here so it has a reason to be passed to this function, really
     # it could just be picked up in the decorator.
