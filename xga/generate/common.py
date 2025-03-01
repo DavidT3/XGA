687b101ab4e5f84a4cf92101730be1f67467a017
David Turner <turne540@msu.edu>
2025-02-28 22:40:11 -0500

    .

--- xga/generate/common.py
+++ xga/generate/common.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:34. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:40. Copyright (c) The Contributors
 
 import os
 import sys
@@ -66,8 +66,6 @@ def execute_cmd(cmd: str, p_type: Union[str, List[str]], p_path: list, extra_inf
     prods = []
     for p_ind, cur_p_path in enumerate(p_path):
         cur_p_type = p_type[p_ind]
-
-        print(cur_p_path)
 
         # This part for defining an image object used to make sure that the src wasn't a NullSource, as defining product
         #  objects is wasteful considering the purpose of a NullSource, but generating exposure maps requires a
