57b36997224691e7b0b1f1dfc1dd342f6e476066
David Turner <turne540@msu.edu>
2025-02-28 22:13:10 -0500

    Spoke to soon about execute_cmd

--- xga/generate/common.py
+++ xga/generate/common.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:13. Copyright (c) The Contributors
 
 import os
 import sys
@@ -50,6 +50,13 @@ def execute_cmd(cmd: str, p_type: Union[str, List[str]], p_path: list, extra_inf
 
     # Catch any mistakes that will be easy to make in developing new interface functions with
     #  backend telescope software
+
+    print(p_type)
+    print(p_path)
+
+    print(type(p_type))
+    print(type(p_path))
+
     if type(p_type) != type(p_path):
         raise XGADeveloperError("Both the p_type and p_path arguments must be of the same type (both string or "
                                 "both list).")
