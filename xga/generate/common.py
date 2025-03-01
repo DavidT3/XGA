124c6a84ac5130a189409296fd28a75dd6fa09e1
David Turner <turne540@msu.edu>
2025-02-28 22:21:59 -0500

    Now getting a FileNotFoundError when declaring the image I think? Trying to diagnose

--- xga/generate/common.py
+++ xga/generate/common.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:13. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 22:21. Copyright (c) The Contributors
 
 import os
 import sys
@@ -74,6 +74,8 @@ def execute_cmd(cmd: str, p_type: Union[str, List[str]], p_path: list, extra_inf
     for p_ind, cur_p_path in enumerate(p_path):
         cur_p_type = p_type[p_ind]
 
+        print(cur_p_path)
+
         # This part for defining an image object used to make sure that the src wasn't a NullSource, as defining product
         #  objects is wasteful considering the purpose of a NullSource, but generating exposure maps requires a
         #  pre-existing image
@@ -126,6 +128,8 @@ def execute_cmd(cmd: str, p_type: Union[str, List[str]], p_path: list, extra_inf
         if cur_p_type == "annular spectrum set components":
             prod.annulus_ident = extra_info["ann_ident"]
             prod.set_ident = extra_info["set_ident"]
+
+        print(prod.usable, prod.not_usable_reasons)
 
         # Put the current prod in the prods list
         prods.append(prod)
