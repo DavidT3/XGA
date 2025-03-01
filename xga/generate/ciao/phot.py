58c0fa07e1438746341c985c5b0971c7f609195e
David Turner <turne540@msu.edu>
2025-02-28 21:26:46 -0500

    Restoring chandra_image_expmap sources_types back to how they should be, with multiple entries. For issue #1264

--- xga/generate/ciao/phot.py
+++ xga/generate/ciao/phot.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 21:16. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 21:26. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -171,8 +171,8 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
         # once the CIAO cmd has run.
         sources_extras.append(np.array(extra_info))
         # If there are multiple output types then the numpy full call will need to specify the two-dimensional shape
-        # sources_types.append(np.full((len(cmds), 3), ["image", "expmap", "ratemap"]))
-        sources_types.append(np.full(len(cmds), "image"))
+        sources_types.append(np.full((len(cmds), 3), ["image", "expmap", "ratemap"]))
+        # sources_types.append(np.full(len(cmds), "image"))
 
     # I only return num_cores here so it has a reason to be passed to this function, really
     # it could just be picked up in the decorator.
