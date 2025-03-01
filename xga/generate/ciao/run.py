747c3920b5d6aa6dd23afb40974f173cc6039485
David Turner <turne540@msu.edu>
2025-02-28 21:30:48 -0500

    Stopped ciao_call making the (possible list) of ptypes into a string. For issue #1264 (indirectly)

--- xga/generate/ciao/run.py
+++ xga/generate/ciao/run.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 21:26. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/02/2025, 21:30. Copyright (c) The Contributors
 
 from functools import wraps
 from multiprocessing.dummy import Pool
@@ -74,9 +74,6 @@ def ciao_call(ciao_func):
         # Making sure something is defined for this variable
         prod_type_str = ""
         if to_execute and len(all_run) > 0:
-
-            print(all_type)
-
             # Will run the commands locally in a pool
             prod_type_str = ", ".join(set(all_type))
             with tqdm(total=len(all_run), desc="Generating products of type(s) " + prod_type_str,
@@ -129,10 +126,9 @@ def ciao_call(ciao_func):
                     # Just defined like this to save on line length for apply_async call.
                     exp_type = all_type[cmd_ind]
                     exp_path = all_path[cmd_ind]
-                    print(exp_path)
                     ext = all_extras[cmd_ind]
                     src = source_rep[cmd_ind]
-                    pool.apply_async(execute_cmd, args=(str(cmd), str(exp_type), exp_path, ext, src),
+                    pool.apply_async(execute_cmd, args=(str(cmd), exp_type, exp_path, ext, src),
                                      error_callback=err_callback, callback=callback)
                 pool.close()  # No more tasks can be added to the pool
                 pool.join()  # Joins the pool, the code will only move on once the pool is empty.
