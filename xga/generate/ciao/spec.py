3914fed5868c090989a3e9c15a2016312b9e9478
rz-wang <rzwang@umich.edu>
2025-03-07 12:15:38 -0500

    Fixed a typo (#1265)
    Signed-off-by: rz-wang <rzwang@umich.edu>

--- xga/generate/ciao/spec.py
+++ xga/generate/ciao/spec.py
@@ -9,6 +9,7 @@ from typing import Union, List
 
 import numpy as np
 from astropy.units import Quantity
+from astropy.coordinates import SkyCoord
 from tqdm import tqdm
 
 from xga import OUTPUT, NUM_CORES
