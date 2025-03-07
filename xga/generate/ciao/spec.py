4e31cc700846db54fe111421e5cba966d44ba0ee
rz-wang <rzwang@umich.edu>
2025-03-07 12:05:44 -0500

    I updated a test version of the Chandra spec extraction function to ensure it properly handles regions and locates the necessary files (#1265).
    Signed-off-by: rz-wang <rzwang@umich.edu>

--- xga/generate/ciao/spec.py
+++ xga/generate/ciao/spec.py
@@ -49,7 +49,8 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
     
     stack = False  # This tells the ciao_call routine that this command won't be part of a stack.
     execute = True  # This should be executed immediately.
-    # This function supports passing both individual sources and sets of sources.
+    # This function supports passing both individual sources and samples - but if it is a source we like to be able
+    #  to iterate over it, so we put it in a list.
     if isinstance(sources, (BaseSource, NullSource)):
         sources = [sources]
 
@@ -59,6 +60,12 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
     elif isinstance(sources, list) and "chandra" not in sources[0].telescopes:
         raise TelescopeNotAssociatedError("There are no Chandra data associated with the sample.")
 
+    # TODO CHECK THESE ARE GOOD ENERGY RANGES AND THEN UNCOMMENT OR UPDATE THEN UNCOMMENT
+    # # Checking user's lo_en and hi_en inputs are in the valid energy range for Chandra
+    # if ((lo_en < Quantity(0.5, 'keV') or lo_en > Quantity(7.0, 'keV')) or
+    #         (hi_en < Quantity(0.5, 'keV') or hi_en > Quantity(7.0, 'keV'))):
+    #     raise ValueError("The 'lo_en' and 'hi_en' value must be between 0.5 keV and 7 keV.")
+
     # These lists are to contain the lists of commands/paths/etc for each of the individual sources passed
     # to this function.    
     sources_cmds = []
@@ -67,11 +74,14 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
     # once the CIAO cmd has run.
     sources_extras = []
     sources_types = []
-    for source in tqdm(sources, disable=disable_progress, desc="Processing sources"):
+    for source in sources:
+        # Explicitly states that source is at very least a BaseSource instance - useful for code completion in IDEs
+        source: BaseSource
+
         cmds = []
         final_paths = []
         extra_info = []
-
+        
         # Skip sources that do not have Chandra data.
         if 'chandra' not in source.telescopes:
             sources_cmds.append(np.array(cmds))
@@ -79,81 +89,159 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
             sources_extras.append(np.array(extra_info))
             sources_types.append(np.full(len(cmds), fill_value="spectrum"))
             continue
+        
+        # Iterate through Chandra event lists associated with the source.
+        for product in source.get_products("events", telescope="chandra", just_obj=True):
+            # Getting the current ObsID, instrument, and event file path
+            evt_file = product
+            obs_id = evt_file.obs_id
+            inst = evt_file.instrument
 
-        # Process each observation associated with the source.
-        for pack in source.get_products("events", just_obj=False, telescope='chandra'):
-            obs_id = pack[1]
-            inst = pack[2]
-            evt_list = pack[-1]
-            # Define output directory and create it if needed.
-            dest_dir = os.path.join(OUTPUT, 'chandra', obs_id)
-            os.makedirs(dest_dir, exist_ok=True)
+            # Grabbing the attitude and badpix files, which the CIAO command we aim to run will want as an input
+            att_file = source.get_att_file(obs_id, 'chandra')
 
-            # Create a temporary directory for region files.
-            temp_reg_dir = os.path.join(dest_dir, "cent_reg_temp")
-            os.makedirs(temp_reg_dir, exist_ok=True)
-            reg_file = os.path.join(temp_reg_dir, "cent_temp.reg")
-            bkg_reg_file = os.path.join(temp_reg_dir, "bkg_temp.reg")
+            # We haven't added a particular get method for bad-pixel files, as they are not as universal as attitude
+            #  files - the badpix files have been stored in the source product storage framework however, as we loaded
+            #  them along with all the files specified in the config file
+            badpix_prod = source.get_products("badpix", telescope="chandra", obs_id=obs_id, inst=inst)
+            if len(badpix_prod) > 1:
+                raise ValueError("Found multiple bad pixel files for Chandra {o}-{i}; this should not be "
+                                 "possible, please contact the developer.".format(o=obs_id, i=inst))
+            elif len(badpix_prod) == 0:
+                raise ValueError("No bad pixel has been read in for Chandra {o}-{i}, please check the bad pixel path"
+                                 " entered in the XGA configuration file - "
+                                 "{bpf}".format(o=obs_id, i=inst,
+                                                bpf=xga_conf['CHANDRA_FILES']['{i}_badpix_file'.format(i=inst)]))
 
-            # Define region file contents.
-            ra = source.default_coord[0].value
-            dec = source.default_coord[1].value
-            inner_rad = inner_radius.to('arcsec').value
-            outer_rad = outer_radius.to('arcsec').value
+            # Now we've established that we have retrieved a single bad pixel product, we extract the path from it
+            badpix_file = badpix_prod[0].path
 
-            reg_content = (
-                "# Region file format: DS9\n"
-                "physical\n"
-                f"annulus({ra},{dec},{inner_rad},{outer_rad})\n"
-            )
+            # Setting up the top level path for the eventual destination of the products to be generated here
+            dest_dir = os.path.join(OUTPUT, "chandra", obs_id)
+            print('dest_dir:', dest_dir)
+              
+            print('badpix:', badpix_prod)
+            print()
             
-            bkg_reg_content = (
-                "# Region file format: DS9\n"
-                "physical\n"
-                f"annulus({ra},{dec},{outer_rad},{outer_rad + (outer_rad - inner_rad)})"
-            )
-            
-            # Write the region file.
-            with open(reg_file, "w") as f:
-                f.write(reg_content)
-
-            with open(bkg_reg_file, "w") as f:
-                f.write(bkg_reg_content)
-            
-            # Define file paths for spectrum, response files, and background spectrum.
-            spec_file = os.path.join(dest_dir, f"{obs_id}_{inst}_spectrum.pi")
+            # Just for group spec at this point, but need to add ungrouped later
+            spec_file = os.path.join(dest_dir, f"{obs_id}_{inst}_grp.pi")
             arf_file = os.path.join(dest_dir, f"{obs_id}_{inst}.arf")
             rmf_file = os.path.join(dest_dir, f"{obs_id}_{inst}.rmf")
-            bkg_file = os.path.join(dest_dir, f"{obs_id}_{inst}_background.pi")
+            bkg_spec_file = os.path.join(dest_dir, f"{obs_id}_{inst}_bkg.pi")
+            bkg_arf_file = os.path.join(dest_dir, f"{obs_id}_{inst}_bkg.arf")
+            bkg_rmf_file = os.path.join(dest_dir, f"{obs_id}_{inst}_bkg.rmf")
+            print('spec_path:', spec_file)
             
-            # Skip generation if the files already exist.
-            if os.path.exists(spec_file) and not force_gen:
-                continue
+            # Temporary directory for fluximage.
+            temp_dir = os.path.join(dest_dir, f"temp_{randint(0, int(1e8))}")
+            os.makedirs(temp_dir, exist_ok=True)
             
-            # Construct the specextract command for spectrum extraction.
-            # !!! Maybe we need to change to 'weight=no' if we're not interested in the spatial variation of
-            # the effect area or for point-source analysis
-            # !!! Also, need to consider the binspec (Source spectrum grouping specification) 
-            #  and grouptype (Source spectrum grouping type)
+            print('temp_dir:',temp_dir)
+            
+            coord = SkyCoord(ra=source.default_coord[0], dec=source.default_coord[1], frame='icrs')
+
+            ra_hms = coord.ra.to_string(unit=u.hour, sep=':', precision=5)
+            dec_dms = coord.dec.to_string(unit=u.deg, sep=':', precision=5, alwayssign=True)
+
+            inner_r_arc = inner_radius.to(u.arcmin).value
+            outer_r_arc = outer_radius.to(u.arcmin).value
+            bkg_inner_r_arc = outer_r_arc * source.background_radius_factors[0]
+            bkg_outer_r_arc = outer_r_arc * source.background_radius_factors[1]
+            print(source.background_radius_factors[0], source.background_radius_factors[1])
+            print(inner_r_arc, outer_r_arc, bkg_inner_r_arc, bkg_outer_r_arc)
+            
+            # Ensure the directory exists
+            temp_region_dir = os.path.join(dest_dir, f"temp_region")
+            os.makedirs(temp_region_dir, exist_ok=True)
+            
+            print()
+            print(temp_region_dir)
+
+            # Define file paths
+            spec_ext_reg_path = os.path.join(temp_region_dir, f"{obs_id}_{inst}_spec_ext_temp.reg")
+            spec_bkg_reg_path = os.path.join(temp_region_dir, f"{obs_id}_{inst}_spec_bkg_temp.reg")
+            
+            print(spec_ext_reg_path)
+            print(spec_bkg_reg_path)
+
+            ext_inter_reg = src.regions_within_radii(inner_radius,
+                                                     outer_radius,
+                                                     "chandra", source.default_coord)
+
+            # Write the extraction region file (annulus between inner_r and outer_r)
+            with open(spec_ext_reg_path, 'w') as ext_reg:
+                ext_reg.write("# Region file format: DS9 version 4.1\n")
+                ext_reg.write("fk5\n")
+                ext_reg.write(f"annulus({ra_hms},{dec_dms},{inner_r_arc}',{outer_r_arc}')\n")
+                # Add exclusion regions if provided
+                for region in ext_inter_reg:
+                    reg_ra = region.center.ra.to_string(unit=u.hour, sep=':', precision=5)
+                    reg_dec = region.center.dec.to_string(unit=u.deg, sep=':', precision=5, alwayssign=True)
+
+                    # Convert width and height to arcmin
+                    width_arc = region.width.to(u.arcmin).value
+                    height_arc = region.height.to(u.arcmin).value
+                    angle = region.angle.to(u.deg).value
+
+                    # Write the exclusion region in ellipse format
+                    ext_reg.write(f"-ellipse({reg_ra},{reg_dec},{width_arc}',{height_arc}',{angle})\n")
+
+            bkg_inter_reg = src.regions_within_radii(outer_radius * source.background_radius_factors[0],
+                                                     outer_radius * source.background_radius_factors[1],
+                                                     "chandra", source.default_coord)
+            # Write the background region file (annulus between outer_r and bkg_r)
+            with open(spec_bkg_reg_path, 'w') as bkg_reg:
+                bkg_reg.write("# Region file format: DS9 version 4.1\n")
+                bkg_reg.write("fk5\n")
+                bkg_reg.write(f"annulus({ra_hms},{dec_dms},{bkg_inner_r_arc}',{bkg_outer_r_arc}')\n")
+
+                # Add exclusion regions if provided
+                for region in bkg_inter_reg:
+                    reg_ra = region.center.ra.to_string(unit=u.hour, sep=':', precision=5)
+                    reg_dec = region.center.dec.to_string(unit=u.deg, sep=':', precision=5, alwayssign=True)
+
+                    # Convert width and height to arcmin
+                    width_arcmin = region.width.to(u.arcmin).value
+                    height_arcmin = region.height.to(u.arcmin).value
+                    angle = region.angle.to(u.deg).value
+
+                    # Write the exclusion region in ellipse format
+                    bkg_reg.write(f"-ellipse({reg_ra},{reg_dec},{width_arc}',{height_arc}',{angle})\n")
+
+
+            # Build specextract command - making sure to set parallel to no, seeing as we're doing our
+            #  own parallelization
             specextract_cmd = (
-                f"specextract infile=\"{evt_list.path}[@{reg_file}]\" outroot={dest_dir}/{obs_id}_{inst} "
-                f"bkgfile=\"{evt_list.path}[@{bkg_reg_file}]\" clobber=yes weight=no grouptype=NUM_CTS binspec=5"
+                f"cd {temp_dir}; specextract infile=\"{evt_list.path}[@{spec_ext_reg_path}]\" "
+                f"outroot={obs_id}_{inst} bkgfile=\"{evt_list.path}[@{spec_bkg_reg_path}]\" "
+                f"asp={att_file} badpixfile={badpix_file} grouptype=NUM_CTS binspec={min_counts} "
+                f"weight=\"yes\" weight_rmf=\"no\" verbose=4 clobber=True parallel=no; "
+                f"mv * {dest_dir}; cd ..; rm -r {temp_dir}"
             )
-            print(specextract_cmd)
             cmds.append(specextract_cmd)
-            final_paths.append(spec_file)
+            print()
+            print('cmd:', cmds)
             
-            # Store metadata related to the extracted spectrum.
+            
+            # This is the products final resting place, if it exists at the end of this command.
+            final_paths.append([spec_file, arf_file, rmf_file, bkg_spec_file, bkg_arf_file, bkg_rmf_file])
             extra_info.append({
-                "obs_id": obs_id, "instrument": inst, "rmf_path": rmf_file, "arf_path": arf_file,
-                "b_spec_path": bkg_file, "grouped": group_spec, "min_counts": min_counts, "min_sn": min_sn,
-                "over_sample": over_sample, "telescope": 'chandra'
+                "obs_id": obs_id,
+                "instrument": inst,
+                "bin_size": 4,
+                "telescope": "chandra"
             })
-        
-        # Append processed data for each source.
+            
+            
         sources_cmds.append(np.array(cmds))
         sources_paths.append(np.array(final_paths))
+        # This contains any other information that will be needed to instantiate the class
+        # once the CIAO cmd has run.
         sources_extras.append(np.array(extra_info))
-        sources_types.append(np.full(len(cmds), fill_value="spectrum"))
-    
-    return sources_cmds, stack, execute, num_cores, sources_types, sources_paths, sources_extras, disable_progress
+        # If there are multiple output types then the numpy full call will need to specify the two-dimensional shape
+        sources_types.append(np.full((len(cmds), 6), ["spec", "spec_arf", "spec_rmf", 
+                                                      "bkg_spec", "bkg_spec_arf", "bkg_spec_rmf"]))
+        
+        # I only return num_cores here so it has a reason to be passed to this function, really
+        # it could just be picked up in the decorator.
+        return sources_cmds, stack, execute, num_cores, sources_types, sources_paths, sources_extras, disable_progress
