75eb239d9657defe8ea4bd75ddef62c601cfeff9
David Turner <turne540@msu.edu>
2025-03-10 21:02:34 -0400

    Fixed an xga_conf import problem in ciao.spec

--- xga/generate/ciao/spec.py
+++ xga/generate/ciao/spec.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 10/03/2025, 15:22. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 10/03/2025, 21:02. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -10,7 +10,7 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy.units import Quantity
 
-from xga import OUTPUT, NUM_CORES
+from xga import OUTPUT, NUM_CORES, xga_conf
 from xga.exceptions import TelescopeNotAssociatedError
 from xga.samples.base import BaseSample
 from xga.sources import BaseSource
