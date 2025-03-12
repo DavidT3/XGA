23f0ef22041f38d9ccb19b785f10c4f657b4fa25
David Turner <turne540@msu.edu>
2025-03-12 17:01:41 -0400

    Trying to make the new fit configuration part of _get_prof_prod work as expected.

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 12/03/2025, 16:56. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 12/03/2025, 17:01. Copyright (c) The Contributors
 
 import os
 import pickle
@@ -4152,7 +4152,9 @@ class BaseSource:
                 try:
                     cur_gen_fit_conf = fit_conf_from_function(FIT_FUNC_MODEL_NAMES[p.spec_model], spec_fit_conf)
                     print(cur_gen_fit_conf)
+                    print('')
                     print(p.spec_fit_conf)
+                    print('\n\n')
                     if cur_gen_fit_conf == p.spec_fit_conf:
                         new_matched_prods.append(p)
 
