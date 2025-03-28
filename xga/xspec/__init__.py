42f8eb1c1ae4b28da170dd8094162fb53492d83a
David Turner <turne540@msu.edu>
2025-03-28 11:34:00 -0400

    Added the XSPEC top level init import of double_temp_apec. For issue #1335

--- xga/xspec/__init__.py
+++ xga/xspec/__init__.py
@@ -1,6 +1,6 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 27/04/2023, 12:16. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 28/03/2025, 11:34. Copyright (c) The Contributors
 
 from .fit import single_temp_apec, power_law, single_temp_apec_profile, blackbody, multi_temp_dem_apec, \
-    single_temp_mekal
+    single_temp_mekal, double_temp_apec
 from .run import execute_cmd, xspec_call
