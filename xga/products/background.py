dc17a82e310ffa128e11129199e917fe641022f9
David Turner <turne540@msu.edu>
2025-03-31 16:05:55 -0400

    Added a background section to products - and defined the name of a class (though literally nothing else).

---
+++ xga/products/background.py
@@ -0,0 +1,7 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (turne540@msu.edu) 31/03/2025, 16:05. Copyright (c) The Contributors
+
+
+class SpectralBackground:
+    def __init__(self):
+        pass
