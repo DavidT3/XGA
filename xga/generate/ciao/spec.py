0b31e91cd21cd4250465ec94b59817b44d36b6c3
rz-wang <rzwang@umich.edu>
2025-04-01 04:04:11 -0400

    Added a `ciao_spectrum_set` function that should be able to generate annular spectra through CIAO; need to test (#1339)
    Signed-off-by: rz-wang <rzwang@umich.edu>

--- xga/generate/ciao/spec.py
+++ xga/generate/ciao/spec.py
@@ -2,6 +2,7 @@
 #  Last modified by David J Turner (turne540@msu.edu) 26/03/2025, 10:57. Copyright (c) The Contributors
 
 import os
+from copy import copy
 from random import randint
 from typing import Union
 
@@ -16,6 +17,7 @@ from xga.generate.sas._common import region_setup
 from xga.samples.base import BaseSample
 from xga.sources import BaseSource
 from xga.sources.base import NullSource
+from .misc import cifbuild
 from .run import ciao_call
 
 
@@ -295,7 +297,7 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
         sources_cmds.append(np.array(cmds))
         sources_paths.append(np.array(final_paths))
         # This contains any other information that will be needed to instantiate the class
-        #  once the SAS cmd has run
+        #  once the CIAO cmd has run
         sources_extras.append(np.array(extra_info))
         sources_types.append(np.full(sources_cmds[-1].shape, fill_value="spectrum"))
 
@@ -332,9 +334,273 @@ def specextract_spectrum(sources: Union[BaseSource, BaseSample], outer_radius: U
     :param float min_sn: If generating a grouped spectrum, this is the minimum signal-to-noise in each channel.
         To disable minimum signal-to-noise set this parameter to None.
     :param int num_cores: The number of cores to use, default is set to 90% of available.
-    :param bool disable_progress: Setting this to true will turn off the SAS generation progress bar.
+    :param bool disable_progress: Setting this to true will turn off the CIAO generation progress bar.
     """
     # All the workings of this function are in _chandra_spec_cmds so that the annular spectrum set generation function
     #  can also use them
     return _chandra_spec_cmds(sources, outer_radius, inner_radius, group_spec, min_counts, min_sn,
                               num_cores, disable_progress)
+
+
+@ciao_call
+def ciao_spectrum_set(sources: Union[BaseSource, BaseSample], radii: Union[List[Quantity], Quantity],
+                      group_spec: bool = True, min_counts: int = 5, min_sn: float = None, over_sample: float = None,
+                      one_rmf: bool = True, num_cores: int = NUM_CORES, force_regen: bool = False,
+                      disable_progress: bool = False):
+    """
+    This function can be used to produce 'sets' of XGA Spectrum objects, generated in concentric circular annuli.
+    Such spectrum sets can be used to measure projected spectroscopic quantities, or even be de-projected to attempt
+    to measure spectroscopic quantities in a three dimensional space.
+
+    :param BaseSource/BaseSample sources: A single source object, or a sample of sources.
+    :param List[Quantity]/Quantity radii: A list of non-scalar quantities containing the boundary radii of the
+        annuli for the sources. A single quantity containing at least three radii may be passed if one source
+        is being analysed, but for multiple sources there should be a quantity (with at least three radii), PER
+        source.
+    :param bool group_spec: A boolean flag that sets whether generated spectra are grouped or not.
+    :param float min_counts: If generating a grouped spectrum, this is the minimum number of counts per channel.
+        To disable minimum counts set this parameter to None.
+    :param float min_sn: If generating a grouped spectrum, this is the minimum signal-to-noise in each channel.
+        To disable minimum signal-to-noise set this parameter to None.
+    :param float over_sample: The minimum energy resolution for each group, set to None to disable. e.g. if
+        over_sample=3 then the minimum width of a group is 1/3 of the resolution FWHM at that energy.
+    :param bool one_rmf: This flag tells the method whether it should only generate one RMF for a particular
+        ObsID-instrument combination - this is much faster in some circumstances, however the RMF does depend
+        slightly on position on the detector.
+    :param int num_cores: The number of cores to use, default is set to 90% of available.
+    :param bool force_regen: This will force all the constituent spectra of the set to be regenerated, use this
+        if your call to this function was interrupted and an incomplete AnnularSpectrum is being read in.
+    :param bool disable_progress: Setting this to true will turn off the CIAO generation progress bar.
+    """
+    # We check to see whether there is an Chandra entry in the 'telescopes' property. If sources is a Source object, then
+    #  that property contains the telescopes associated with that source, and if it is a Sample object then
+    #  'telescopes' contains the list of unique telescopes that are associated with at least one member source.
+    # Clearly if Chandra isn't associated at all, then continuing with this function would be pointless
+    if ((not isinstance(sources, list) and 'chandra' not in sources.telescopes) or
+            (isinstance(sources, list) and 'chandra' not in sources[0].telescopes)):
+        raise TelescopeNotAssociatedError("There are no Chandra data associated with the source/sample, as such `Chandra` "
+                                          "spectra cannot be generated.")
+
+    # If it's a single source I put it into an iterable object (i.e. a list), just for convenience
+    if isinstance(sources, BaseSource):
+        sources = [sources]
+    elif isinstance(sources, list) and not all([isinstance(s, BaseSource) for s in sources]):
+        raise TypeError("If a list is passed, each element must be a source.")
+    # And the only other option is a BaseSample instance, so if it isn't that then we get angry
+    elif not isinstance(sources, (BaseSample, list)):
+        raise TypeError("Please only pass source or sample objects for the 'sources' parameter of this function")
+
+    # I just want to make sure that nobody passes anything daft for the radii
+    if isinstance(radii, Quantity) and len(sources) != 1:
+        raise TypeError("You may only pass a Quantity for the radii parameter if you are only analysing "
+                        "one source. You are attempting to generate spectrum sets for {0} sources, so please pass "
+                        "a list of {0} non-scalar quantities.".format(len(sources)))
+    elif isinstance(radii, Quantity):
+        pass
+    elif isinstance(radii, (list, np.ndarray)) and len(sources) != len(radii):
+        raise ValueError("The list of quantities passed for the radii parameter must be the same length as the "
+                         "number of sources which you are analysing.")
+
+    # If we've made it to this point then the radii type is fine, but I want to make sure that radii is a list
+    #  of quantities - as expected by the rest of the function
+    if isinstance(radii, Quantity):
+        radii = [radii]
+
+    # Check that all radii are passed in the units, I could convert them and make sure but I can't
+    #  be bothered
+    if len(set([r.unit for r in radii])) != 1:
+        raise ValueError("Please pass all radii sets in the same units.")
+
+    # I'm also going to check to make sure that every annulus N+1 is further out then annulus N. There is a check
+    #  for this in the spec setup function but if I catch it here I can give a more informative error message
+    for s_ind, source in enumerate(sources):
+        # I'll also check that the quantity passed for the radii isn't scalar, and isn't only two long - that's not
+        #  a set of annuli, they should just use evselect_spectrum for that
+        cur_rad = radii[s_ind]
+        src_name = source.name
+        if cur_rad.isscalar:
+            raise ValueError("The radii quantity you have passed for {s} only has one value in it, this function is "
+                             "for generating a set of multiple annular spectra, I need at least three "
+                             "entries.".format(s=src_name))
+        elif len(cur_rad) < 3:
+            raise ValueError("The radii quantity have you passed for {s} must have at least 3 entries, this "
+                             "would generate a set of 2 annular spectra and is the minimum for this "
+                             "function.".format(s=src_name))
+
+        # This runs through the radii for this source and makes sure that annulus N+1 is larger than annulus N
+        greater_check = [cur_rad[r_ind] < cur_rad[r_ind+1] for r_ind in range(0, len(cur_rad)-1)]
+        if not all(greater_check):
+            raise ValueError("Not all of the radii passed for {s} are larger than the annulus that "
+                             "precedes them.".format(s=src_name))
+
+    # Just to make sure calibration files have been generated, though I don't actually think they could
+    #  have gotten to this point without them
+    cifbuild(sources, num_cores, disable_progress)
+
+    # This generates a spectra between the innermost and outmost radii for each source, and a universal RMF
+    if one_rmf:
+        innermost_rads = Quantity([r_set[0] for r_set in radii], radii[0].unit)
+        outermost_rads = Quantity([r_set[-1] for r_set in radii], radii[0].unit)
+        _chandra_spec_cmds(sources, outermost_rads, innermost_rads, group_spec, min_counts, min_sn, over_sample,
+                           one_rmf, num_cores, disable_progress)
+
+    # I want to be able to generate all the individual annuli in parallel, but I need them to be associated with
+    #  the correct annuli, which is why I have to iterate through the sources and radii
+
+    # These store the final output information needed to run the commands
+    all_cmds = []
+    all_paths = []
+    all_out_types = []
+    all_extras = []
+    # Iterating through the sources
+    for s_ind, source in enumerate(sources):
+        # This is where the commands/extra information get concatenated from the different annuli
+        src_cmds = np.array([])
+        src_paths = np.array([])
+        src_out_types = []
+        src_extras = np.array([])
+
+        # By this point we know that at least one of the sources has Chandra data associated (we checked that at the
+        #  beginning of this function), we still need to append the empty cmds, paths, extrainfo, and ptypes to
+        #  the final output, so that the cmd_list and input argument 'sources' have the same length, which avoids
+        #  bugs occuring in the ciao_call wrapper
+        if 'chandra' not in source.telescopes:
+            all_cmds.append(np.array(src_cmds))
+            all_paths.append(np.array(src_paths))
+            # This contains any other information that will be needed to instantiate the class
+            # once the CIAO cmd has run
+            all_extras.append(np.array(src_extras))
+            all_out_types.append(src_out_types)
+
+            # then we can continue with the rest of the sources
+            continue
+
+        # This generates a random integer ID for this set of spectra
+        set_id = randint(0, int(1e+8))
+
+        # I want to be sure that this configuration doesn't already exist
+        if group_spec and min_counts is not None:
+            extra_name = "_mincnt{}".format(min_counts)
+        elif group_spec and min_sn is not None:
+            extra_name = "_minsn{}".format(min_sn)
+        else:
+            extra_name = ''
+
+        # And if it was oversampled during generation then we need to include that as well
+        if over_sample is not None:
+            extra_name += "_ovsamp{ov}".format(ov=over_sample)
+
+        # Combines the annular radii into a string
+        ann_rad_str = "_".join(source.convert_radius(radii[s_ind], 'deg').value.astype(str))
+        spec_storage_name = "ra{ra}_dec{dec}_ar{ar}_grp{gr}"
+        spec_storage_name = spec_storage_name.format(ra=source.default_coord[0].value,
+                                                     dec=source.default_coord[1].value, ar=ann_rad_str, gr=group_spec)
+
+        spec_storage_name += extra_name
+
+        exists = source.get_products('combined_spectrum', extra_key=spec_storage_name, telescope='chandra')
+        if len(exists) == 0:
+            # If it doesn't exist then we do need to call evselect_spectrum
+            generate_spec = True
+        else:
+            # If it already exists though we don't need to bother
+            generate_spec = False
+
+        if generate_spec or force_regen:
+            # Here we run through all the requested annuli for the current source
+            for r_ind in range(len(radii[s_ind])-1):
+                # Generate the CIAO commands for the current annulus of the current source, for all observations
+                spec_cmd_out = _chandra_spec_cmds(source, radii[s_ind][r_ind+1], radii[s_ind][r_ind], group_spec, min_counts,
+                                                  min_sn, over_sample, one_rmf, num_cores, disable_progress, True)
+
+                # Read out some of the output into variables to be modified
+                interim_paths = spec_cmd_out[5][0]
+                interim_extras = spec_cmd_out[6][0]
+                interim_cmds = spec_cmd_out[0][0]
+
+                # Modified paths and commands will be stored in here
+                new_paths = []
+                new_cmds = []
+                for p_ind, p in enumerate(interim_paths):
+                    cur_cmd = interim_cmds[p_ind]
+
+                    # Split up the current path, so we only modify the actual file name and not any
+                    #  other part of the string
+                    split_p = p.split('/')
+                    # We add the set and annulus identifiers
+                    new_spec = split_p[-1].replace("_spec.fits", "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) \
+                               + "_spec.fits"
+                    # Not enough just to change the name passed through XGA, it has to be changed in
+                    #  the CIAO commands as well
+                    cur_cmd = cur_cmd.replace(split_p[-1], new_spec)
+
+                    # Add the new filename back into the split spec file path
+                    split_p[-1] = new_spec
+
+                    # Add an annulus identifier to the extra_info dictionary
+                    interim_extras[p_ind].update({"set_ident": set_id, "ann_ident": r_ind})
+
+                    # Only need to modify the RMF paths if the universal RMF HASN'T been used
+                    if "universal" not in interim_extras[p_ind]['rmf_path']:
+                        # Much the same process as with the spectrum name
+                        split_r = copy(interim_extras[p_ind]['rmf_path']).split('/')
+                        # split_br = copy(interim_extras[p_ind]['b_rmf_path']).split('/')
+                        new_rmf = split_r[-1].replace('.rmf', "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) + ".rmf"
+                        # new_b_rmf = split_br[-1].replace('_back.rmf', "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) \
+                        #             + "_back.rmf"
+
+                        # Replacing the names in the CIAO commands
+                        cur_cmd = cur_cmd.replace(split_r[-1], new_rmf)
+                        # cur_cmd = cur_cmd.replace(split_br[-1], new_b_rmf)
+
+                        split_r[-1] = new_rmf
+                        # split_br[-1] = new_b_rmf
+
+                        # Adding the new RMF paths into the extra info dictionary
+                        # interim_extras[p_ind].update({"rmf_path": "/".join(split_r),
+                        # "b_rmf_path": "/".join(split_br)})
+                        interim_extras[p_ind].update({"rmf_path": "/".join(split_r)})
+
+                    # Same process as RMFs but for the ARF, background ARF, and background spec
+                    split_a = copy(interim_extras[p_ind]['arf_path']).split('/')
+                    # split_ba = copy(interim_extras[p_ind]['b_arf_path']).split('/')
+                    split_bs = copy(interim_extras[p_ind]['b_spec_path']).split('/')
+                    new_arf = split_a[-1].replace('.arf', "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) + ".arf"
+                    # new_b_arf = split_ba[-1].replace('_back.arf', "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) \
+                    #             + "_back.arf"
+                    new_b_spec = split_bs[-1].replace('_backspec.fits', "_ident{si}_{ai}".format(si=set_id, ai=r_ind)) \
+                                + "_backspec.fits"
+
+                    # New names into the commands
+                    cur_cmd = cur_cmd.replace(split_a[-1], new_arf)
+                    # cur_cmd = cur_cmd.replace(split_ba[-1], new_b_arf)
+                    cur_cmd = cur_cmd.replace(split_bs[-1], new_b_spec)
+
+                    split_a[-1] = new_arf
+                    # split_ba[-1] = new_b_arf
+                    split_bs[-1] = new_b_spec
+
+                    # Update the extra info dictionary some more
+                    # interim_extras[p_ind].update({"arf_path": "/".join(split_a), "b_arf_path": "/".join(split_ba),
+                    #                               "b_spec_path": "/".join(split_bs)})
+                    interim_extras[p_ind].update({"arf_path": "/".join(split_a), "b_spec_path": "/".join(split_bs)})
+
+                    # Add the new paths and commands to their respective lists
+                    new_paths.append("/".join(split_p))
+                    new_cmds.append(cur_cmd)
+
+                src_paths = np.concatenate([src_paths, new_paths])
+                # Go through and concatenate things to the source lists defined above
+                src_cmds = np.concatenate([src_cmds, new_cmds])
+                src_out_types += ['annular spectrum set components'] * len(spec_cmd_out[4][0])
+                src_extras = np.concatenate([src_extras, interim_extras])
+        src_out_types = np.array(src_out_types)
+
+        # This adds the current sources final commands to the 'all sources' lists
+        all_cmds.append(src_cmds)
+        all_paths.append(src_paths)
+        all_out_types.append(src_out_types)
+        all_extras.append(src_extras)
+
+    # This gets passed back to the ciao call function and is used to run the commands
+    return all_cmds, False, True, num_cores, all_out_types, all_paths, all_extras, disable_progress
