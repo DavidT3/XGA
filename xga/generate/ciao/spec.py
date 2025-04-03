751b99b7fb9d8a73c5d083643d6b6e13916e90f1
rz-wang <rzwang@umich.edu>
2025-04-03 15:42:47 -0400

    # Changed the format of region files back (#1265)
    Signed-off-by: rz-wang <rzwang@umich.edu>

--- xga/generate/ciao/spec.py
+++ xga/generate/ciao/spec.py
@@ -228,8 +228,8 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
 
             # Write the extraction region file (annulus between inner_r and outer_r)
             with open(spec_ext_reg_path, 'w') as ext_reg:
-                # ext_reg.write("# Region file format: DS9 version 4.1\n")
-                # ext_reg.write("fk5\n")
+                ext_reg.write("# Region file format: DS9 version 4.1\n")
+                ext_reg.write("fk5\n")
                 ext_reg.write(f"annulus({ra_hms},{dec_dms},{inner_r_arc.value}',{outer_r_arc.value}')\n")
                 # Add exclusion regions if provided
                 for region in ext_inter_reg:
@@ -249,8 +249,8 @@ def _chandra_spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Uni
                                                         "chandra", source.default_coord)
             # Write the background region file (annulus between outer_r and bkg_r)
             with open(spec_bkg_reg_path, 'w') as bkg_reg:
-                # bkg_reg.write("# Region file format: DS9 version 4.1\n")
-                # bkg_reg.write("fk5\n")
+                bkg_reg.write("# Region file format: DS9 version 4.1\n")
+                bkg_reg.write("fk5\n")
                 bkg_reg.write(f"annulus({ra_hms},{dec_dms},{bkg_inner_r_arc.value}',{bkg_outer_r_arc.value}')\n")
 
                 # Add exclusion regions if provided
