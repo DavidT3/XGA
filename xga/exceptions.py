35315e46986d26cfb58ea044db388edb9424e991
David Turner <turne540@msu.edu>
2025-04-03 11:10:43 -0400

    Added a SASVersionError to XGA

--- xga/exceptions.py
+++ xga/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 26/03/2025, 18:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 03/04/2025, 11:10. Copyright (c) The Contributors
 
 
 class HeasoftError(Exception):
@@ -649,3 +649,24 @@ class FitConfNotAssociatedError(Exception):
             return '{}'.format(self.message)
         else:
             return 'FitConfNotAssociatedError has been raised'
+
+
+class SASVersionError(Exception):
+    def __init__(self, *args):
+        """
+        Exception raised if the XMM Scientific Analysis System located on the system is a version
+        that is not compatible with XGA, or a specific XGA task.
+
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        else:
+            return 'SASVersionError has been raised'
