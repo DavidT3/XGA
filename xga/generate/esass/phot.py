f6585d8d616f46086189a9c5078ecf85b669fc71
David Turner <turne540@msu.edu>
2025-04-10 17:20:50 -0400

    Debugging a 'OverflowError: cannot convert float infinity to integer' in _img_params_from_evtlist

--- xga/generate/esass/phot.py
+++ xga/generate/esass/phot.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 10/04/2025, 17:16. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 10/04/2025, 17:20. Copyright (c) The Contributors
 
 import os
 from random import randint
@@ -95,7 +95,7 @@ def _img_params_from_evtlist(evt_list: EventList):
             xcen = int(xmin + (xsep/2))
             ycen = int(ymin + (ysep/2))
         except OverflowError:
-            raise ValueError(" - ".join([evt_list.obs_id, evt_list.instrument, evt_list.path]))
+            raise ValueError(" - ".join([evt_list.src_name, evt_list.obs_id, evt_list.instrument, evt_list.path]))
 
         centre_pos = f"{xcen} {ycen}"
     
