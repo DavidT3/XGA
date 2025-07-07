6a0ba872f5f4f49c22f41152e32bc49c232e3b9e
David Turner <turne540@msu.edu>
2025-07-07 13:40:50 -0400

    Oooops made a mistake in generate.multimission.__init__.py - did from _common rather than from ._common. Indirectly for issue #1371

--- xga/generate/multitelescope/__init__.py
+++ xga/generate/multitelescope/__init__.py
@@ -1,4 +1,4 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 07/07/2025, 13:03. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 07/07/2025, 13:40. Copyright (c) The Contributors
 
-from _common import check_tel_associated
+from ._common import check_tel_associated
