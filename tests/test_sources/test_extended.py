a3ff027644b605f48ef40a9d19296274b64e04ee
Jessica Pilling <jp735@sussex.ac.uk>
2025-07-08 15:39:59 +0100

    remove extra imports

--- tests/test_sources/test_extended.py
+++ tests/test_sources/test_extended.py
@@ -5,15 +5,8 @@ import os
 from astropy.units import Quantity
 
 import xga
-from xga.sources import GalaxyCluster
-from xga.generate.esass import evtool_image
-from xga.products.phot import Image
-from xga.products.spec import Spectrum
-from xga.generate.esass import srctool_spectrum
-
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 from .. import SRC_ALL_TELS
 
