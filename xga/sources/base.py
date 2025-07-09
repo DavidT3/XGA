0eec85ba392bc34cb4131db93de1522163412c4f
David Turner <turne540@msu.edu>
2025-07-08 22:59:58 -0400

    Related to issue #1386 - changes can't stay but corrected a problem with the changes made this morning, as otherwise there can be a len() of just an image object, which errors

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 08/07/2025, 20:23. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 08/07/2025, 22:28. Copyright (c) The Contributors
 import gc
 import os
 import pickle
@@ -3900,10 +3900,13 @@ class BaseSource:
         # I assume that if no ObsID is supplied, then the user wishes to have a mask for the combined data
         if obs_id is None:
             comb_images = self.get_combined_images(lo_en=lo_en, hi_en=hi_en, telescope=telescope)
-            if len(comb_images) != 0:
+            # TODO THESE CHANGES SHOULD NOT STAY, BUT I HAVE TO MAKE THIS SAFE - BEFORE I ALTERED THIS
+            #  IT COULD TRY TO DO A LEN OF AN IMAGE
+            if isinstance(comb_images, list):
                 mask_image = comb_images[0]
             else:
-                raise NoProductAvailableError("There are no combined products available to generate a mask for.")
+                mask_image = comb_images
+
         else:
             mask_image = self.get_images(obs_id=obs_id, lo_en=lo_en, hi_en=hi_en, inst=inst,
                                          telescope=telescope)
