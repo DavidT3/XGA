2322568242f5e68d7f7d6c4fbd52cfeb7af75619
David Turner <turne540@msu.edu>
2025-07-09 15:16:04 -0400

    Testing to see the memory usage effect of the change made to the .shape property of Image (got the _generate_interloper_mask method of BaseSource to pull the shape from the mask image data array rather than the image instance itself). Testing issue #1383

--- xga/sources/base.py
+++ xga/sources/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 09/07/2025, 14:08. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 09/07/2025, 15:16. Copyright (c) The Contributors
 import gc
 import os
 import pickle
@@ -2063,7 +2063,7 @@ class BaseSource:
 
         # This is the array that the mask gets built in - initially all ones and as we move through the regions we
         #  start to set the bits that need to be excluded to zero
-        mask = np.ones(mask_image.shape)
+        mask = np.ones(mask_image.data.shape)
 
         # If a region inclusion distance has not been set, then we create a mask of all True so that all regions
         #  are included in the final mask
