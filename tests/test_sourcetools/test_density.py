537091028c1d5b12147f786e064132244aa7155b
Jessica Pilling <jp735@sussex.ac.uk>
2025-07-11 15:55:46 +0100

    ok new way of tracing memory trial

--- tests/test_sourcetools/test_density.py
+++ tests/test_sourcetools/test_density.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import unittest
+import tracemalloc
 
 from astropy.units import Quantity
 
@@ -14,48 +15,49 @@ from xga.sourcetools.density import _dens_setup, _run_sb, inv_abel_fitted_model,
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import tracemalloc
-
-tracemalloc.start()
-
 from .. import SRC_ALL_TELS
 
-class TestDensityFuncs(unittest.TestCase):
-    def test_dens_setup(self):
-        res = _dens_setup(SRC_ALL_TELS, 'angr',Quantity(0.5, 'keV'), Quantity(2, 'keV'), stacked_spectra=True)
-        
-        assert type(res[0][0]) == GalaxyCluster
-        assert set(res[2].keys()) == set(['erosita', 'xmm'])
-        assert set(res[3].keys()) == set(['erosita', 'xmm'])
+def trace_memory(fn):
+    def wrapper(*args, **kwargs):
+        snapshot1 = tracemalloc.take_snapshot()
+        result = fn(*args, **kwargs)
+        snapshot2 = tracemalloc.take_snapshot()
+        stats = snapshot2.compare_to(snapshot1, 'lineno')
+        print(f"Memory usage for {fn.__name__}")
+        for stat in stats[:5]:
+            print(stat)
+        return result
+    return wrapper
 
-    def test_run_sb(self):
-        res = _run_sb(SRC_ALL_TELS, 'erosita', Quantity(600, 'kpc'), False, Quantity(0.5, 'keV'), 
-                Quantity(2, 'keV'), False, None, None, None, None, 1, 0.0, None, None)
+class TestDensityFuncs(unittest.TestCase):
+#    def test_dens_setup(self):        
+#        res = _dens_setup(SRC_ALL_TELS, 'angr',Quantity(0.5, 'keV'), Quantity(2, 'keV'), stacked_spectra=True)
+#        assert type(res[0][0]) == GalaxyCluster
+#        assert set(res[2].keys()) == set(['erosita', 'xmm'])
+#        assert set(res[3].keys()) == set(['erosita', 'xmm'])
+
+#    def test_run_sb(self):
+#        res = _run_sb(SRC_ALL_TELS, 'erosita', Quantity(600, 'kpc'), False, Quantity(0.5, 'keV'), 
+#                Quantity(2, 'keV'), False, None, None, None, None, 1, 0.0, None, None)
         
-        assert type(res) == SurfaceBrightness1D
+#        assert type(res) == SurfaceBrightness1D
     
     def test_inv_abel_fitted_model(self):
-        res = inv_abel_fitted_model(SRC_ALL_TELS, 'beta', use_peak=False, psf_corr=False, stacked_spectra=True)
+        @trace_memory
+        inv_abel_fitted_model(SRC_ALL_TELS, 'beta', use_peak=False, psf_corr=False, stacked_spectra=True)
 
-        assert type(res['erosita'][0]) == GasDensity3D
-        assert type(res['xmm'][0]) == GasDensity3D
+#        assert type(res['erosita'][0]) == GasDensity3D
+#        assert type(res['xmm'][0]) == GasDensity3D
 
-    def test_inv_abel_fitted_model_stacked_spectrum_F(self):
-        res = inv_abel_fitted_model(SRC_ALL_TELS, 'beta', use_peak=False, psf_corr=False, stacked_spectra=False)
-
-        print(res)
-        assert type(res['erosita'][0]) == GasDensity3D
-        assert type(res['xmm'][0]) == GasDensity3D
+#   def test_inv_abel_fitted_model_stacked_spectrum_F(self):
+#        
+#        res = inv_abel_fitted_model(SRC_ALL_TELS, 'beta', use_peak=False, psf_corr=False, stacked_spectra=False)
+#        print(res)
+#        assert type(res['xmm'][0]) == GasDensity3D
+#        assert type(res['erosita'][0]) == GasDensity3D
     
-    def test_ann_spectra_apec_norm(self):
-        res = ann_spectra_apec_norm(SRC_ALL_TELS, Quantity(600, 'kpc'), stacked_spectra=True)
+#    def test_ann_spectra_apec_norm(self):
+#        res = ann_spectra_apec_norm(SRC_ALL_TELS, Quantity(600, 'kpc'), stacked_spectra=True)
 
-        assert type(res['erosita'][0]) == GasDensity3D
-        assert type(res['xmm'][0]) == GasDensity3D
-
-
-snapshot = tracemalloc.take_snapshot()
-top_stats = snapshot.statistics('lineno')
-
-for stat in top_stats[:10]:
-    print(stat)
+#        assert type(res['erosita'][0]) == GasDensity3D
+#        assert type(res['xmm'][0]) == GasDensity3D
