34ea21b6a68b50d160c7e5840dc9816eb30671db
David Turner <turne540@msu.edu>
2025-08-25 13:58:20 -0400

    Added NoTelescopeDataError and InvalidTelescopeError exceptions from the multi-mission XGA branch. For issue #1430

--- xga/exceptions.py
+++ xga/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 26/03/2025, 18:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 25/08/2025, 13:58. Copyright (c) The Contributors
 
 
 class HeasoftError(Exception):
@@ -649,3 +649,41 @@ class FitConfNotAssociatedError(Exception):
             return '{}'.format(self.message)
         else:
             return 'FitConfNotAssociatedError has been raised'
+
+class NoTelescopeDataError(Exception):
+    def __init__(self, *args):
+        """
+        Raised when a part of XGA that directly accesses telescope data is used, but there are no relevant
+        telescopes setup in the configuration file.
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{}'.format(self.message)
+        else:
+            return 'NoTelescopeDataError has been raised'
+
+
+class InvalidTelescopeError(Exception):
+    def __init__(self, *args):
+        """
+        Raised when the name of a telescope has been passed to a function, but it is not recognised or supported.
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{}'.format(self.message)
+        else:
+            return 'InvalidTelescopeError has been raised'
