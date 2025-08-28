c7c31dcec95f670b3b5818509d8c4298ce63ac3a
David Turner <turne540@msu.edu>
2025-08-27 21:40:22 -0400

    Added a sky pixel limits property to event list, as well as adding a couple more useful keys to the mission_event_column_name_map.json file

--- xga/products/__init__.py
+++ xga/products/__init__.py
@@ -1,8 +1,8 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 27/08/2025, 21:40. Copyright (c) The Contributors
 
-from .base import BaseProduct, BaseAggregateProduct, BaseProfile1D, BaseAggregateProfile1D
 from .misc import EventList
+from .base import BaseProduct, BaseAggregateProduct, BaseProfile1D, BaseAggregateProfile1D
 from .phot import Image, ExpMap, RateMap, PSF, PSFGrid
 from .relation import ScalingRelation
 from .spec import Spectrum, AnnularSpectra
