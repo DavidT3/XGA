41eaf828887bb4f0d1362033dec77aa1ba69380e
David Turner <turne540@msu.edu>
2025-09-09 22:59:55 -0400

    Added a not implemented error to the image generation of EventList - purely for the merge into main prior to a v0.6 release.

--- xga/products/misc.py
+++ xga/products/misc.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 09/09/2025, 19:05. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 09/09/2025, 22:59. Copyright (c) The Contributors
 import os.path
 from typing import List, Tuple
 
@@ -486,6 +486,7 @@ class EventList(BaseProduct):
         :return:
         :rtype:
         """
+        raise NotImplementedError("Intrinsic generation of images from EventList instances is not fully implemented.")
         #
         if self.telescope.lower() in MISSION_COL_DB:
             rel_miss_info = MISSION_COL_DB[self.telescope.lower()]
