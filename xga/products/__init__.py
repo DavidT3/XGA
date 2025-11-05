37255ea906dbc5a9ee2457a2b04fa8ddec17643e
David Turner <turne540@msu.edu>
2025-11-05 15:22:13 -0500

    Made LightCurve and AggregateLightCurve importable directly from xga.products rather than having to go to xga.products.lightcurve.

--- xga/products/__init__.py
+++ xga/products/__init__.py
@@ -1,15 +1,16 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 27/08/2025, 21:42. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 05/11/2025, 15:22. Copyright (c) The Contributors
 
 from .base import BaseProduct, BaseAggregateProduct, BaseProfile1D, BaseAggregateProfile1D
-from .phot import Image, ExpMap, RateMap, PSF, PSFGrid
+from .lightcurve import LightCurve, AggregateLightCurve
 from .misc import EventList
+from .phot import Image, ExpMap, RateMap, PSF, PSFGrid
 from .relation import ScalingRelation
 from .spec import Spectrum, AnnularSpectra
 
 # Defining a dictionary to map from string product names to their associated classes
 PROD_MAP = {"image": Image, "expmap": ExpMap, "events": EventList, "spectrum": Spectrum, "psf": PSF,
-            "psfgrid": PSFGrid}
+            "psfgrid": PSFGrid, "lightcurve": LightCurve}
 
 
 
