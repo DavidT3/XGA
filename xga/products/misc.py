cb2639f1834f3b2dfa6004c980fa79f102e7503a
David Turner <turne540@msu.edu>
2025-11-05 15:28:51 -0500

    Altered imports in products.misc to avoid circular import errors.

--- xga/products/misc.py
+++ xga/products/misc.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 05/11/2025, 14:27. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 05/11/2025, 15:28. Copyright (c) The Contributors
 import os.path
 from typing import List, Tuple
 
@@ -12,7 +12,8 @@ from astropy.table import Table
 from astropy.units import Quantity, UnitConversionError
 from astropy.wcs import WCS
 
-from . import BaseProduct, Image
+from .base import BaseProduct
+from .phot import Image
 from .. import MISSION_COL_DB
 from ..exceptions import XGADeveloperError
 
