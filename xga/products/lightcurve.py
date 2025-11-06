0b5d048a9052ead3de18fa92d88d83c2c70fc5dd
David Turner <turne540@msu.edu>
2025-11-06 11:46:42 -0500

    Removed a todo

--- xga/products/lightcurve.py
+++ xga/products/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 06/11/2025, 11:43. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 06/11/2025, 11:46. Copyright (c) The Contributors
 import re
 from datetime import datetime
 from typing import Union, List, Tuple
@@ -1532,7 +1532,6 @@ class AggregateLightCurve(BaseAggregateProduct):
                     low_lim = min([np.nanmin(lc.count_rate-lc.count_rate_err) for lc in all_rel_lcs]).value*0.95
                     upp_lim = max([np.nanmax(lc.count_rate+lc.count_rate_err) for lc in all_rel_lcs]).value*1.05
                 elif y_lims is None and fracexp_corr:
-                    # TODO THIS MIGHT BE TEMPORARY, WILL USE GET_DATA METHOD OF LIGHTCURVE WHEN WRITTEN
                     low_lim = min([np.nanmin(lc.count_rate/lc.frac_exp - lc.count_rate_err/lc.frac_exp)
                                    for lc in all_rel_lcs]).value * 0.95
                     upp_lim = max([np.nanmax(lc.count_rate/lc.frac_exp + lc.count_rate_err/lc.frac_exp)
