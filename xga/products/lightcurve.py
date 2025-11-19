1a0b88ee985e8deb4e640fc10539f26d53f4b53f
David Turner <turne540@msu.edu>
2025-11-18 22:47:41 -0500

    Really trying to make these stupid time format validation checks work in check_times_within_gti. For issue #1472

--- xga/products/lightcurve.py
+++ xga/products/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 18/11/2025, 22:42. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 18/11/2025, 22:47. Copyright (c) The Contributors
 import re
 from datetime import datetime
 from typing import Union, List, Tuple, Dict
@@ -1935,7 +1935,7 @@ class AggregateLightCurve(BaseAggregateProduct):
                                                    all([isinstance(en, datetime) for en in times])))
               or isinstance(times, datetime)):
             times = (Time(times) - self.ref_times[telescope]).to('s')
-        elif isinstance(times, np.ndarray):
+        elif not isinstance(times, Quantity) and isinstance(times, np.ndarray):
             raise TypeError("If an array is passed to the 'times' argument, every element must be of type 'datetime'.")
 
         # If the 'times' argument is a single value, turn it into an array
