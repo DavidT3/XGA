7fa7f58d9a252b86927a49503c3a4fe0da73e3dd
David Turner <turne540@msu.edu>
2025-11-18 22:37:03 -0500

    Some of the time input normalization in check_times_within_gti didn't work. Forgot that quantities are an instance of numpy arrays. For issue #1472

--- xga/products/lightcurve.py
+++ xga/products/lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 18/11/2025, 22:25. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 18/11/2025, 22:37. Copyright (c) The Contributors
 import re
 from datetime import datetime
 from typing import Union, List, Tuple, Dict
@@ -1913,7 +1913,9 @@ class AggregateLightCurve(BaseAggregateProduct):
 
         Times must be in the form of seconds from reference time of the telescope of interest.
 
-        :param Quantity/np.ndarray/Time times: Times to be checked against the GTIs. Must be a scalar or an array of values.
+        :param Quantity/np.ndarray/Time times: Times to be checked against the GTIs. Can be scalar or an array.
+            Passing astropy Time objects, Python datetime objects (or arrays of datetimes), or seconds from
+            reference time are all supported.
         :param bool src_gti: Flag indicating whether to use source GTIs (True) or background GTIs (False).
             Defaults to True.
         :param str inst: The instrument whose light curve GTIs we are to compare the input times with. If
@@ -1929,8 +1931,8 @@ class AggregateLightCurve(BaseAggregateProduct):
 
         if isinstance(times, Time):
             times = (times - self.ref_times[telescope]).to('s')
-        elif ((isinstance(times, np.ndarray) and all([isinstance(en, datetime) for en in times]))
-              or isinstance(times, datetime)):
+        elif ((isinstance(times, np.ndarray) and not isinstance(times, Quantity) and
+               all([isinstance(en, datetime) for en in times])) or isinstance(times, datetime)):
             times = (Time(times) - self.ref_times[telescope]).to('s')
         elif isinstance(times, np.ndarray):
             raise TypeError("If an array is passed to the 'times' argument, every element must be of type 'datetime'.")
