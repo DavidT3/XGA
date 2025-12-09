1c6bf5726c03683a20d3ed8d72f9413299c7bcd4
David Turner <turne540@msu.edu>
2025-12-09 11:57:24 -0500

    Added a _common.py file to the generate.esass submodule - at the moment just contains constant definitions of the low and high energy bounds of the image to be used as an extmap for eROSITA product generation.

---
+++ xga/generate/esass/_common.py
@@ -0,0 +1,7 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (turne540@msu.edu) 09/12/2025, 11:57. Copyright (c) The Contributors
+
+from astropy.units import Quantity
+
+EROSITA_EXTMAP_LO_EN = Quantity(0.2, 'keV')
+EROSITA_EXTMAP_HI_EN = Quantity(10.0, 'keV')
