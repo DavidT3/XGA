16c928873c4566764d232a8394897d0aad8282ff
David Turner <turne540@msu.edu>
2025-12-10 10:56:49 -0500

    Really smashing up the _make_spec_cmd_info and _spec_cmds functions to figure out why combined spec are being deleted.

--- xga/generate/esass/spec.py
+++ xga/generate/esass/spec.py
@@ -1,5 +1,5 @@
 #  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 10/12/2025, 10:36. Copyright (c) The Contributors
+#  Last modified by David J Turner (djturner@umbc.edu) 10/12/2025, 10:56. Copyright (c) The Contributors
 
 import os
 from copy import deepcopy, copy
@@ -337,10 +337,14 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
             # We make sure to remove the 'merged spectra' output of srctool - which is identical to the
             #  instrument one if we generate for one spectrum at a time. Though only if the user hasn't actually
             #  ASKED for the merged spectrum
+            print('TM combo', combine_tm)
+
             if combine_tm:
-                cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf, remove_all_but_merged_cmd])
+                # cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf, remove_all_but_merged_cmd])
+                cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf])
             else:
-                cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf, remove_merged_cmd])
+                # cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf, remove_merged_cmd])
+                cmd_str = ";".join([s_cmd_str, rename_spec, rename_rmf, rename_arf])
 
             # This currently ensures that there is a ';' divider between these two chunks of commands - hopefully
             #  we'll neaten it up at some point
@@ -348,10 +352,13 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
 
             # Removing the 'merged spectra' output of srctool, for the background spectra in this case
             if combine_tm:
+                # cmd_str += ";".join([sb_cmd_str, rename_b_spec, rename_b_rmf, rename_b_arf,
+                #                      remove_all_but_merged_cmd])
                 cmd_str += ";".join([sb_cmd_str, rename_b_spec, rename_b_rmf, rename_b_arf,
-                                     remove_all_but_merged_cmd])
+                                     ])
             else:
-                cmd_str += ";".join([sb_cmd_str, rename_b_spec, rename_b_rmf, rename_b_arf, remove_merged_cmd])
+                # cmd_str += ";".join([sb_cmd_str, rename_b_spec, rename_b_rmf, rename_b_arf, remove_merged_cmd])
+                cmd_str += ";".join([sb_cmd_str, rename_b_spec, rename_b_rmf, rename_b_arf])
 
             # If the user wants to group the spectrum, then this command should be added
             if group_spec:
@@ -503,6 +510,7 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
         back_sp_cmd += " writeinsts={i}"
         pnt_sp_cmd += " writeinsts={i}"
 
+    # TODO SORT THIS SHIT OUT?!
     # You can't control the whole names of srctool outputs, so this renames it to the XGA format
     rename_cmd = 'mv srctoolout_{i_no}??_{type}* {nn}'
     # Having a string to remove the 'merged' spectra that srctool outputs, even when you only request one instrument
@@ -515,9 +523,7 @@ def _spec_cmds(sources: Union[BaseSource, BaseSample], outer_radius: Union[str, 
     remove_merged_dr1_8 = 'rm *srctoolout_8*'
     remove_merged_dr1_9 = 'rm *srctoolout_9*'
     # We also set up a command that will remove all spectra BUT the combined one
-    # TODO SORT THIS SHIT OUT?!
-    # remove_all_but_merged_cmd = "rm *srctoolout_*"
-    remove_all_but_merged_cmd = ""
+    remove_all_but_merged_cmd = "rm *srctoolout_*"
 
     # Grouping the spectra will be done using the HEASoft tool 'ftgrouppha' - we make sure to remove the original
     #  ungrouped file. I think maybe that approach is safer than turning clobber on and just having the original
