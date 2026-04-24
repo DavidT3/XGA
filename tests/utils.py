eb8ab6faeb75eaaa8ca1e43e7f05de4ec0126197
David Turner <djturner@umbc.edu>
2026-04-24 10:28:34 -0400

    Added a utils.py to the tests directory - it contains definitions for decorators that can be applied to specific tests so that they only run if the correct backend software are available.

---
+++ tests/utils.py
@@ -0,0 +1,42 @@
+#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by Claude 23/04/2026. Copyright (c) The Contributors
+
+import unittest
+from functools import wraps
+from xga import SAS_AVAIL, ESASS_AVAIL, CIAO_AVAIL
+
+
+def require_sas(test_func):
+    """
+    Decorator to skip tests if SAS is not available.
+    """
+    @wraps(test_func)
+    def wrapper(*args, **kwargs):
+        if not SAS_AVAIL:
+            raise unittest.SkipTest("SAS is not available.")
+        return test_func(*args, **kwargs)
+    return wrapper
+
+
+def require_esass(test_func):
+    """
+    Decorator to skip tests if eSASS is not available.
+    """
+    @wraps(test_func)
+    def wrapper(*args, **kwargs):
+        if not ESASS_AVAIL:
+            raise unittest.SkipTest("eSASS is not available.")
+        return test_func(*args, **kwargs)
+    return wrapper
+
+
+def require_ciao(test_func):
+    """
+    Decorator to skip tests if CIAO is not available.
+    """
+    @wraps(test_func)
+    def wrapper(*args, **kwargs):
+        if not CIAO_AVAIL:
+            raise unittest.SkipTest("CIAO is not available.")
+        return test_func(*args, **kwargs)
+    return wrapper
