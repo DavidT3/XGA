bfc31e2e4a5b93e447d7c554273c5bd1f0a57002
David Turner <djturner@umbc.edu>
2026-05-05 22:08:03 -0400

    Added a very basic single source global apec test
    Signed-off-by: David Turner <djturner@umbc.edu>

--- tests/test_xspec/test_fit_general.py
+++ tests/test_xspec/test_fit_general.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 4/27/26, 5:27 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 5/5/26, 10:07 PM. Copyright (c) The Contributors.
 
 import unittest
 
@@ -8,7 +8,34 @@ from astropy.units import Quantity
 
 from xga.samples import ClusterSample
 from xga.xspec.fit.general import single_temp_apec
-from .. import CLUSTER_SMP
+from .. import CLUSTER_SMP, get_test_source
+
+
+class TestXSPECSingleSource(unittest.TestCase):
+    """
+    This class houses tests that use XGA's XSPEC functionality on source class instances, as opposed to
+    sample class instances.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_src = get_test_source('all')
+
+    def test_global_single_temp_apec(self):
+        par_combs = [
+            {"stacked_spectra": True},
+            {"stacked_spectra": False},
+        ]
+
+        out_rad = Quantity(500, 'kpc')
+
+        for cur_pc in par_combs:
+            with self.subTest(msg=f"Test global single_temp_apec with {cur_pc}", **cur_pc):
+
+                single_temp_apec(self.test_src, out_rad, **cur_pc)
+
+                for cur_tel in self.test_src.telescopes:
+                    cur_tx = self.test_src.get_temperature(out_rad, cur_tel, **cur_pc)
 
 
 class TestBaseSample(unittest.TestCase):
@@ -34,7 +61,7 @@ class TestBaseSample(unittest.TestCase):
 
     def test_Lx_Tx_w_odd_telescope_sample(self):
         """
-        Testing that for samples were sources dont have the same telescopes assigned, the Lx and Tx
+        Testing that for samples where sources don't have the same telescopes assigned, the Lx and Tx
         can be retrieved.
         """
         single_temp_apec(self.smp_odd_tels, 'r500', stacked_spectra=True, spectrum_checking=False)
