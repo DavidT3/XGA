1b27fc42b6dcf74f4a17a98d9cab6b1310f5d374
David Turner <djturner@umbc.edu>
2026-05-08 12:32:25 -0400

    Improved the test_existing_prods_loaded_in_img test, and the combined version. Now will fail when a noproductavailable error is raised (which hopefully it won't be!!) and uses the get_test_source method
    Signed-off-by: David Turner <djturner@umbc.edu>

--- tests/test_sources/test_base.py
+++ tests/test_sources/test_base.py
@@ -1,10 +1,11 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 5/5/26, 11:37 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 5/8/26, 12:31 PM. Copyright (c) The Contributors.
 
 import unittest
 
 from astropy.units import Quantity
 
+from xga.exceptions import NoProductAvailableError
 from xga.generate.esass import evtool_image
 from xga.generate.esass import srctool_spectrum
 from xga.products.phot import Image
@@ -31,33 +32,48 @@ class TestBaseSource(unittest.TestCase):
         """
         Testing _existing_xga_products() in BaseSource for images.
         """
+        # Make a new local reference to the source class
         src = self.src
+        # We make some images that won't already exist
         evtool_image(src, Quantity(0.3, 'keV'), Quantity(3, 'keV'))
 
-        del(src)
+        # Use the get_test_source function we defined in the test init, but have it
+        #  define a new instance of the test GalaxyCluster, so that it will
+        #  reload the new products
+        redef_src = get_test_source("all", shared=False)
 
-        src = GalaxyCluster(SRC_INFO['ra'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
-                                     name=SRC_INFO['name'], use_peak=False,
-                                     search_distance={'erass': Quantity(3.6, 'deg')})
+        try:
+            img = redef_src.get_images(lo_en=Quantity(0.3, 'keV'),
+                                       hi_en=Quantity(3, 'keV'),
+                                       telescope='erass')
+        except NoProductAvailableError:
+            self.fail("NoProductAvailableError raised.")
 
-        img = src.get_images(lo_en=Quantity(0.3, 'keV'), hi_en=Quantity(3, 'keV'), telescope='erass')
-
+        # For this test source there should be multiple images retrieved, so if
+        #  this isn't a list we have a problem.
+        assert type(img) == list
         assert all([isinstance(im, Image) for im in img])
 
     def test_existing_prods_loaded_in_img_combined(self):
         """
         Testing _existing_xga_products() in BaseSource for combined images.
         """
+        # Make a new local reference to the source class
         src = self.src
+        # We make some COMBINED images that won't already exist
         evtool_image(src, Quantity(0.3, 'keV'), Quantity(3, 'keV'), combine_obs=True)
 
-        del(src)
+        # Use the get_test_source function we defined in the test init, but have it
+        #  define a new instance of the test GalaxyCluster, so that it will
+        #  reload the new products
+        redef_src = get_test_source("all", shared=False)
 
-        src = GalaxyCluster(SRC_INFO['ra'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
-                                     name=SRC_INFO['name'], use_peak=False,
-                                     search_distance={'erass': Quantity(3.6, 'deg')})
-
-        img = src.get_combined_images(lo_en=Quantity(0.3, 'keV'), hi_en=Quantity(3, 'keV'), telescope='erass')
+        try:
+            img = redef_src.get_combined_images(lo_en=Quantity(0.3, 'keV'),
+                                                hi_en=Quantity(3, 'keV'),
+                                                telescope='erass')
+        except NoProductAvailableError:
+            self.fail("NoProductAvailableError raised.")
 
         assert isinstance(img, Image)
         assert img.obs_id == 'combined'
@@ -114,7 +130,6 @@ class TestBaseSource(unittest.TestCase):
         assert isinstance(spec, Spectrum)
         assert spec.instrument == 'tm1'
 
-# TODO combined_spectrum should get things with instruments and obsids
 
 if __name__ == "__main__":
      unittest.main()
