2ac90c47c508591cc975f303d223319e3b6ae850
David Turner <djturner@umbc.edu>
2026-05-08 17:40:18 -0400

    The test_srctool_lightcurve_combine_insts_f_combine_obs_t test now handles exceptions and reports a test fail (though hopefully that won't happen). Also tests retrieval of both a particular TM light curve (TM1), and all TM light curves.
    Signed-off-by: David Turner <djturner@umbc.edu>

--- tests/test_generate/test_esass_lightcurve.py
+++ tests/test_generate/test_esass_lightcurve.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 5/8/26, 5:23 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 5/8/26, 5:28 PM. Copyright (c) The Contributors.
 
 import unittest
 
@@ -55,32 +55,51 @@ class TestEsassLcFuncs(unittest.TestCase):
         #  lc variable should be a list of LightCurve instances
         assert type(lc) == list
 
-        for l in lc:
-            assert isinstance(l, LightCurve)
-            assert l.telescope == 'erass'
-            assert l.obs_id != 'combined'
-            assert l.instrument == 'combined'
-
+        for cur_lc in lc:
+            assert isinstance(cur_lc, LightCurve)
+            assert cur_lc.telescope == 'erass'
+            assert cur_lc.obs_id != 'combined'
+            assert cur_lc.instrument == 'combined'
 
     @require_esass
     def test_srctool_lightcurve_combine_insts_f_combine_obs_t(self):
         """
         Testing srctool_lightcurve with the arguments combine_insts=False and combine_obs=True
         """
+        # Run the light curve generation - one light curve per eROSITA TM, combining all ObsIDs
+        #  associated with this source
         srctool_lightcurve(self.src, 'r500', combine_tm=False, combine_obs=True)
 
-        lc = self.src.get_combined_lightcurves('r500', telescope='erass', inst='tm1')
+        try:
+            lc = self.src.get_combined_lightcurves('r500', telescope='erass', inst='tm1')
+        except NoProductAvailableError:
+            self.fail("NoProductAvailableError raised when retrieving TM1 combined-ObsID light curve.")
 
         assert isinstance(lc, LightCurve)
         assert lc.telescope == 'erass'
         assert lc.obs_id == 'combined'
         assert lc.instrument == 'tm1'
 
+        try:
+            all_tm_lc = self.src.get_combined_lightcurves('r500', telescope='erass', inst=None)
+        except NoProductAvailableError:
+            self.fail("NoProductAvailableError raised when retrieving combined-ObsID "
+                      "light curves for every separate TM.")
+
+        assert type(all_tm_lc) == list
+
+        for cur_lc in all_tm_lc:
+            assert isinstance(cur_lc, LightCurve)
+            assert cur_lc.telescope == 'erass'
+            assert cur_lc.obs_id == 'combined'
+            assert cur_lc.instrument[:2] == 'tm'
+
     @require_esass
     def test_srctool_lightcurve_combine_insts_t_combine_obs_t(self):
         """
         Testing srctool_lightcurve with the arguments combine_insts=True and combine_obs=True
         """
+
         srctool_lightcurve(self.src, 'r500', combine_tm=True, combine_obs=True)
 
         lc = self.src.get_combined_lightcurves('r500', telescope='erass', inst='combined')
@@ -96,5 +115,3 @@ class TestEsassLcFuncs(unittest.TestCase):
             assert lc.telescope == 'erass'
             assert lc.obs_id == 'combined'
             assert lc.instrument == 'combined'
-
-
