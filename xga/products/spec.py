2a714c4785f26e029e92259c3173a5aba0147dad
David Turner <david.j.turner@umbc.edu>
2026-05-11 13:32:40 -0400

    Replaced unnecessary use of subplots with figure()

--- xga/products/spec.py
+++ xga/products/spec.py
@@ -1686,7 +1686,8 @@ class Spectrum(BaseProduct):
             fig, (ax, ax_res) = plt.subplots(2, 1, figsize=figsize, sharex=True,
                                              gridspec_kw={'height_ratios': [3, 1]})
         else:
-            fig, ax = plt.subplots(1, 1, figsize=figsize)
+            fig = plt.figure(figsize=figsize)
+            ax = plt.gca()
 
         plt.sca(ax)
 
