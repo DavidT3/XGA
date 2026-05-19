f097b00ba1979262410236a1d0d4dabc40564420
David Turner <djturner@umbc.edu>
2026-05-19 15:31:10 -0400

    Added a load_fits argument to get_test_source, to make it possible for tests to instantiate a non-shared version of one of the test sources with load_fits=False.
    Signed-off-by: David Turner <djturner@umbc.edu>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 5/5/26, 11:32 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 5/19/26, 3:30 PM. Copyright (c) The Contributors.
 import numpy as np
 import pandas as pd
 from astropy.units import Quantity
@@ -20,16 +20,21 @@ CLUSTER_SMP[['ra', 'dec', 'z', 'r500']] = CLUSTER_SMP[['ra', 'dec', 'z', 'r500']
 #  and we don't want to do it at import time, as the configuration might not be set up yet.
 _CACHED_SOURCES = {}
 
-def get_test_source(telescope: str = 'all', shared: bool = True) -> GalaxyCluster:
+def get_test_source(telescope: str = 'all', shared: bool = True, load_fits: bool = True) -> GalaxyCluster:
     """
     A factory function to provide test sources. This is used to avoid instantiating them at import time.
 
     :param str telescope: The telescope for which we want a source. Options are 'all', 'xmm', 'erass'.
     :param bool shared: Whether to return a shared (cached) instance or a fresh one.
+    :param bool load_fits: Only applied if shared=False, and controls whether the fresh GalaxyCluster
+        instance is set to load existing XSPEC fit information from disk.
     :return: The requested source.
     :rtype: GalaxyCluster
     """
     global _CACHED_SOURCES
+
+    # Reset the load_fits argument to True if shared=True
+    load_fits = True if shared else load_fits
 
     # If a shared instance is requested, and we have one cached, return it
     if shared and telescope in _CACHED_SOURCES:
@@ -40,17 +45,17 @@ def get_test_source(telescope: str = 'all', shared: bool = True) -> GalaxyCluste
         src = GalaxyCluster(SRC_INFO['ra'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
                             name=SRC_INFO['name'], use_peak=False,
                             search_distance={'erass': Quantity(3.6, 'deg')},
-                            load_profiles=False)
+                            load_profiles=False, load_fits=load_fits)
     elif telescope == 'xmm':
         src = GalaxyCluster(SRC_INFO['ra'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
                             name=SRC_INFO['name'], use_peak=False,
-                            telescope='xmm', load_profiles=False)
+                            telescope='xmm', load_profiles=False, load_fits=load_fits)
     elif telescope == 'erass' or telescope == 'erosita':
         src = GalaxyCluster(SRC_INFO['ra'], SRC_INFO['dec'], SRC_INFO['z'], r500=Quantity(500, 'kpc'),
                             name=SRC_INFO['name'], use_peak=False,
                             telescope='erass',
                             search_distance={'erass': Quantity(3.6, 'deg')},
-                            load_profiles=False)
+                            load_profiles=False, load_fits=load_fits)
     else:
         raise ValueError(f"Unknown mission name: {telescope}")
 
