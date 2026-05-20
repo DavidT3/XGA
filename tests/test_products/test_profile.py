8d7ded39d4ed62df72010be9443bc2e7f1d4dbb2
David Turner <djturner@umbc.edu>
2026-05-20 13:00:47 -0400

    Added drawing rads and vals test for aggregate profile views
    Signed-off-by: David Turner <djturner@umbc.edu>

--- tests/test_products/test_profile.py
+++ tests/test_products/test_profile.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 5/20/26, 12:56 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 5/20/26, 1:00 PM. Copyright (c) The Contributors.
 
 import os
 import unittest
@@ -34,6 +34,8 @@ class TestProfileView(unittest.TestCase):
                                                "an-inst",
                                                deg_radii=deg_radii)
 
+        cls.tp_agg = cls.tp_one + cls.tp_two
+
     def test_default_view(self):
         test_out_path = os.path.join(MISC_OUTPUT_TESTS, self.id())
         os.makedirs(test_out_path, exist_ok=True)
@@ -44,9 +46,8 @@ class TestProfileView(unittest.TestCase):
         test_out_path = os.path.join(MISC_OUTPUT_TESTS, self.id())
         os.makedirs(test_out_path, exist_ok=True)
 
-        agg_prof = self.tp_one + self.tp_two
 
-        agg_prof.view(save_path=os.path.join(test_out_path, "tp_one_two_agg_default.png"))
+        self.tp_agg.view(save_path=os.path.join(test_out_path, "tp_one_two_agg_default.png"))
 
     def test_draw_rad_val_view(self):
         test_out_path = os.path.join(MISC_OUTPUT_TESTS, self.id())
@@ -62,6 +63,23 @@ class TestProfileView(unittest.TestCase):
                               draw_vals={r'$T_{2500}$': Quantity([2.3, 0.5, 0.1], 'keV')})
 
         self.tp_one.save_view(os.path.join(test_out_path, "tp_one_draw_rad_val.png"),
+                              draw_rads={r'$R_{2500}$': Quantity(134, 'kpc')},
+                              draw_vals={r'$T_{2500}$': Quantity(2.3, 'keV')})
+
+    def test_draw_rad_val_aggregate_view(self):
+        test_out_path = os.path.join(MISC_OUTPUT_TESTS, self.id())
+        os.makedirs(test_out_path, exist_ok=True)
+
+        self.tp_agg.view(save_path=os.path.join(test_out_path, "tp_agg_draw_rad.png"),
+                              draw_rads={r'$R_{2500}$': Quantity(134, 'kpc')})
+
+        self.tp_agg.view(save_path=os.path.join(test_out_path, "tp_agg_draw_val.png"),
+                              draw_vals={r'$T_{2500}$': Quantity(2.3, 'keV')})
+
+        self.tp_agg.view(save_path=os.path.join(test_out_path, "tp_agg_draw_val_werr.png"),
+                              draw_vals={r'$T_{2500}$': Quantity([2.3, 0.5, 0.1], 'keV')})
+
+        self.tp_agg.view(save_path=os.path.join(test_out_path, "tp_agg_draw_rad_val.png"),
                               draw_rads={r'$R_{2500}$': Quantity(134, 'kpc')},
                               draw_vals={r'$T_{2500}$': Quantity(2.3, 'keV')})
 
