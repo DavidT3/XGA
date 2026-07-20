630e5b41c9d928d61d4a74ea997a66dcda852233
David Turner <djturner@umbc.edu>
2026-07-20 13:44:26 -0400

    Added a misc.py shim back to the products module and updated the docs
    Signed-off-by: David Turner <djturner@umbc.edu>

---
+++ xga/products/misc.py
@@ -0,0 +1,10 @@
+#  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (djturner@umbc.edu) 7/20/26, 1:34 PM. Copyright (c) The Contributors.
+
+import warnings
+
+warnings.warn(
+    "xga.products.misc is deprecated and will be removed in a future version. "
+    "Please import from xga.products.events instead.",
+    DeprecationWarning, stacklevel=2
+)
