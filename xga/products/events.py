cfcbfedf56bb493b5fe8d4cb07172c973b426f52
David Turner <djturner@umbc.edu>
2026-07-21 13:11:45 -0400

    Added a not usable error to _read_header_on_demand of EventList.
    Signed-off-by: David Turner <djturner@umbc.edu>

--- xga/products/events.py
+++ xga/products/events.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 7/21/26, 12:42 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 7/21/26, 1:06 PM. Copyright (c) The Contributors.
 
 import os.path
 from typing import List, Tuple, Optional, Union
@@ -15,7 +15,7 @@ from astropy.units import Quantity, UnitConversionError
 from astropy.wcs import WCS
 
 from xga import MISSION_COL_DB, DEFAULT_IMAGE_BINNING, ALT_INST_NAMES
-from xga.exceptions import ProductGenerationError, XGADeveloperError
+from xga.exceptions import ProductGenerationError, XGADeveloperError, ProductNotUsableError
 from xga.products.base import BaseProduct
 from xga.products.phot import Image
 
@@ -856,6 +856,10 @@ class EventList(BaseProduct):
             table = self._evt_tab_name
         else:
             raise ValueError("The 'table' argument must be either 'primary' or 'event'.")
+
+        if not self.usable:
+            raise ProductNotUsableError(f"This event list has been flagged as 'not usable' for the "
+                                        f"following reason: {self.not_usable_reasons}.")
 
         # We could likely treat the remote and local file access identically, but we're doing it this way for
         #  now out of an abundance of caution - I don't know how local files would behave using fsspec
