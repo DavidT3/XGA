356ff14e8efc7bff4b16b71c7076e26bd086e3f4
David Turner <djturner@umbc.edu>
2026-07-22 18:01:35 -0400

    Added the general_smooth function to the imports in the imagetools __init__.py. Also removed the pizza brightness profile import, given that function has been 'NotImplementedError'ed for years. For issue #1545
    Signed-off-by: David Turner <djturner@umbc.edu>

--- xga/imagetools/__init__.py
+++ xga/imagetools/__init__.py
@@ -1,7 +1,7 @@
-#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 20/02/2023, 14:04. Copyright (c) The Contributors
+#  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (djturner@umbc.edu) 7/22/26, 6:00 PM. Copyright (c) The Contributors.
 
 from .misc import pix_deg_scale, data_limits
-from .profile import ann_radii, radial_brightness, pizza_brightness, annular_mask
-
+from .profile import ann_radii, radial_brightness, annular_mask
+from .smooth import general_smooth
 
