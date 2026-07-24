9e8b9076b2263955139e2f614813e8fa3d7749e4
David Turner <djturner@umbc.edu>
2026-07-24 16:21:02 -0400

    Made the try except in `chandra_image_expmap` except a specific error, rather than just Exception (which was raising a warning in Bandit, and is very bad practise). For issue #1555
    Signed-off-by: David Turner <djturner@umbc.edu>

--- xga/generate/ciao/phot.py
+++ xga/generate/ciao/phot.py
@@ -1,27 +1,36 @@
-#  This code is a part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (turne540@msu.edu) 10/03/2025, 14:30. Copyright (c) The Contributors
+#  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
+#  Last modified by David J Turner (djturner@umbc.edu) 7/24/26, 4:19 PM. Copyright (c) The Contributors.
+"""
+This module provides functions to generate Chandra images and exposure maps.
+
+It implements XGA wrapper functions to generate photometric (i.e. images, exposure maps, etc.)
+data products from Chandra's backend CIAO software.
+"""
 
 import os
 from random import randint
-from typing import Union
+from typing import Any
 
 import numpy as np
-from astropy.units import Quantity, deg, UnitConversionError
+from astropy.units import Quantity, UnitConversionError
 
-from xga import OUTPUT, NUM_CORES, xga_conf
+from xga import NUM_CORES, OUTPUT, xga_conf
 from xga.exceptions import NoProductAvailableError, TelescopeNotAssociatedError
+from xga.products import BaseProduct
 from xga.samples.base import BaseSample
-from xga.sources.base import NullSource
+from xga.sources import BaseSource, NullSource
+
 from .run import ciao_call
-from ...sources import BaseSource
 
 
 @ciao_call
-def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample], 
-                         lo_en: Quantity = Quantity(0.5, "keV"),
-                         hi_en: Quantity = Quantity(2.0, "keV"),
-                         num_cores: int = NUM_CORES,
-                         disable_progress: bool = False):
+def chandra_image_expmap(
+    sources: BaseSource | list[BaseSource] | NullSource | BaseSample,
+    lo_en: Quantity = Quantity(0.5, "keV"),
+    hi_en: Quantity = Quantity(2.0, "keV"),
+    num_cores: int = NUM_CORES,
+    disable_progress: bool = False,
+) -> tuple[list[Any], bool, bool, int, list[Any], list[Any], list[Any], bool]:
     """
     Generates Chandra images, exposure maps, and rate maps using CIAO's fluximage in rate mode.
 
@@ -30,7 +39,8 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
     :param Quantity hi_en: Upper energy bound (default = 2.0 keV).
     :param int num_cores: Number of cores for parallel processing (default = NUM_CORES).
     :param bool disable_progress: Disable progress bar if True.
-    :return: A tuple with commands, stack, execute, num_cores, product types, paths, metadata, and disable_progress flag.
+    :return: A tuple with commands, stack, execute, num_cores, product types, paths, metadata, and
+        disable_progress flag.
     """
     stack = False  # This tells the ciao_call routine that this command won't be part of a stack.
     execute = True  # This should be executed immediately.
@@ -47,19 +57,21 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
 
     # Validate energy bounds.
     if not isinstance(lo_en, Quantity) or not isinstance(hi_en, Quantity):
-        raise TypeError("The lo_en and hi_en arguments must be astropy quantities in units "
-                        "that can be converted to keV.")
+        raise TypeError(
+            "The lo_en and hi_en arguments must be astropy quantities in units that can be converted to keV."
+        )
     # Have to make sure that the energy bounds are in units that can be converted to keV (which is what fluximage
     #  expects for these arguments).
-    elif not lo_en.unit.is_equivalent('eV') or not hi_en.unit.is_equivalent('eV'):
-        raise UnitConversionError("The lo_en and hi_en arguments must be astropy quantities in units "
-                                  "that can be converted to keV.")
+    elif not lo_en.unit.is_equivalent("eV") or not hi_en.unit.is_equivalent("eV"):
+        raise UnitConversionError(
+            "The lo_en and hi_en arguments must be astropy quantities in units that can be converted to keV."
+        )
     elif lo_en >= hi_en:
         raise ValueError("The lower energy bound ('lo_en') must be less than the upper energy bound ('hi_en').")
     # Converting to the right unit
     else:
-        lo_en = lo_en.to('keV')
-        hi_en = hi_en.to('keV')
+        lo_en = lo_en.to("keV")
+        hi_en = hi_en.to("keV")
 
     # TODO CHECK THESE ARE GOOD ENERGY RANGES AND THEN UNCOMMENT OR UPDATE THEN UNCOMMENT
     # # Checking user's lo_en and hi_en inputs are in the valid energy range for Chandra
@@ -68,7 +80,7 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
     #     raise ValueError("The 'lo_en' and 'hi_en' value must be between 0.5 keV and 7 keV.")
 
     # These lists are to contain the lists of commands/paths/etc for each of the individual sources passed
-    # to this function.    
+    # to this function.
     sources_cmds = []
     sources_paths = []
     # This contains any other information that will be needed to instantiate the class
@@ -76,52 +88,63 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
     sources_extras = []
     sources_types = []
     for source in sources:
-        # Explicitly states that source is at very least a BaseSource instance - useful for code completion in IDEs
-        source: BaseSource
+        # Skip sources that do not have Chandra data.
+        if "chandra" not in source.telescopes:
+            # Append empty arrays with an explicit dtype
+            sources_cmds.append(np.array([], dtype=str))
+            sources_paths.append(np.array([], dtype=str))
+            sources_extras.append(np.array([], dtype=str))
+            sources_types.append(np.empty((0, 3), dtype=str))
+            continue
 
+        # Initialize the lists ONLY for sources that actually have Chandra data
         cmds = []
         final_paths = []
         extra_info = []
 
-        # Skip sources that do not have Chandra data.
-        if 'chandra' not in source.telescopes:
-            sources_cmds.append(np.array(cmds))
-            sources_paths.append(np.array(final_paths))
-            sources_extras.append(np.array(extra_info))
-            sources_types.append(np.full((len(cmds), 3), ["image", "expmap", "ratemap"]))
-            continue
-
         # Iterate through Chandra event lists associated with the source.
         for product in source.get_products("events", telescope="chandra", just_obj=True):
             # Getting the current ObsID, instrument, and event file path
+            # TODO neaten all of this when a get method for events is implemented
             evt_file = product
+            # Tell Mypy (static type checker) we know exactly what type get_products returned in this specific case
+            if not isinstance(evt_file, BaseProduct):
+                raise TypeError(f"Expected evt_file to be an instance of BaseProduct, but got {type(evt_file)}.")
+
             obs_id = evt_file.obs_id
             inst = evt_file.instrument
 
             # Grabbing the attitude and badpix files, which the CIAO command we aim to run will want as an input
-            att_file = source.get_att_file(obs_id, 'chandra')
+            att_file = source.get_att_file(obs_id, "chandra")
 
             # We haven't added a particular get method for bad-pixel files, as they are not as universal as attitude
             #  files - the badpix files have been stored in the source product storage framework however, as we loaded
             #  them along with all the files specified in the config file
             badpix_prod = source.get_products("badpix", telescope="chandra", obs_id=obs_id, inst=inst)
+            # Tell Mypy (static type checker) we know exactly what type get_products returned in this specific case
+            if not isinstance(badpix_prod[0], BaseProduct):
+                raise TypeError(f"Expected badpix_prod to be a BaseProduct, but got {type(badpix_prod[0])}.")
+
             if len(badpix_prod) > 1:
-                raise ValueError("Found multiple bad pixel files for Chandra {o}-{i}; this should not be "
-                                 "possible, please contact the developer.".format(o=obs_id, i=inst))
+                raise ValueError(
+                    f"Found multiple bad pixel files for Chandra {obs_id}-{inst}; this should not be "
+                    "possible, please contact the developer."
+                )
             elif len(badpix_prod) == 0:
-                raise ValueError("No bad pixel has been read in for Chandra {o}-{i}, please check the bad pixel path"
-                                 " entered in the XGA configuration file - "
-                                 "{bpf}".format(o=obs_id, i=inst,
-                                                bpf=xga_conf['CHANDRA_FILES']['{i}_badpix_file'.format(i=inst)]))
+                raise ValueError(
+                    "No bad pixel has been read in for Chandra {o}-{i}, please check the bad pixel path"
+                    " entered in the XGA configuration file - "
+                    "{bpf}".format(o=obs_id, i=inst, bpf=xga_conf["CHANDRA_FILES"][f"{inst}_badpix_file"])
+                )
 
             # Now we've established that we have retrieved a single bad pixel product, we extract the path from it
             badpix_file = badpix_prod[0].path
 
             # Do we actually need to run this generation? If matching products already exist then we won't bother
             try:
-                source.get_images(obs_id, inst, lo_en, hi_en, telescope='chandra')
-                source.get_expmaps(obs_id, inst, lo_en, hi_en, telescope='chandra')
-                source.get_ratemaps(obs_id, inst, lo_en, hi_en, telescope='chandra')
+                source.get_images(obs_id, inst, lo_en, hi_en, telescope="chandra")
+                source.get_expmaps(obs_id, inst, lo_en, hi_en, telescope="chandra")
+                source.get_ratemaps(obs_id, inst, lo_en, hi_en, telescope="chandra")
                 # If the expected outputs from this function do exist for the current ObsID, we'll just
                 #  move on to the next one
                 continue
@@ -149,9 +172,10 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
             # If it does, use its size for the subsequent photometric products.
             img_exist = False
             try:
-                img_temp = source.get_images(obs_id, inst, telescope='chandra')
+                img_temp = source.get_images(obs_id, inst, telescope="chandra")
                 img_exist = True
-            except Exception:
+            except NoProductAvailableError:
+                # In this case we definitely have to continue
                 pass
 
             if img_exist:
@@ -175,8 +199,9 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
             flux_cmd = (
                 f"cd {temp_dir}; fluximage infile={evt_file.path} outroot={obs_id}_{inst} "
                 f"bands={lo_en.value}:{hi_en.value}:{(lo_en + hi_en).value / 2} binsize=4 asolfile={att_file} "
-                f"badpixfile={badpix_file} units=time tmpdir={temp_dir} cleanup=yes verbose=4 parallel=no xygrid={xygrid_dir}; "
-                f"mv {out_image_file} {final_image_file}; mv {out_expmap_file} {final_expmap_file}; "
+                f"badpixfile={badpix_file} units=time tmpdir={temp_dir} cleanup=yes verbose=4 parallel=no "
+                f"xygrid={xygrid_dir}; mv {out_image_file} {final_image_file}; "
+                f"mv {out_expmap_file} {final_expmap_file}; "
                 f"mv {out_ratemap_file} {final_ratemap_file}; cd ..; rm -r {temp_dir}"
             )
             # print(flux_cmd)
@@ -184,15 +209,17 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
 
             # This is the products final resting place, if it exists at the end of this command.
             final_paths.append([final_image_file, final_expmap_file, final_ratemap_file])
-            extra_info.append({
-                "obs_id": obs_id,
-                "instrument": inst,
-                "lo_en": lo_en,
-                "hi_en": hi_en,
-                "effective_energy": (lo_en + hi_en) / 2,
-                "bin_size": 4,
-                "telescope": "chandra"
-            })
+            extra_info.append(
+                {
+                    "obs_id": obs_id,
+                    "instrument": inst,
+                    "lo_en": lo_en,
+                    "hi_en": hi_en,
+                    "effective_energy": (lo_en + hi_en) / 2,
+                    "bin_size": 4,
+                    "telescope": "chandra",
+                }
+            )
 
         sources_cmds.append(np.array(cmds))
         sources_paths.append(np.array(final_paths))
@@ -201,7 +228,6 @@ def chandra_image_expmap(sources: Union[BaseSource, NullSource, BaseSample],
         sources_extras.append(np.array(extra_info))
         # If there are multiple output types then the numpy full call will need to specify the two-dimensional shape
         sources_types.append(np.full((len(cmds), 3), ["image", "expmap", "ratemap"]))
-
 
     # I only return num_cores here so it has a reason to be passed to this function, really
     # it could just be picked up in the decorator.
