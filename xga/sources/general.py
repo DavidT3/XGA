0729858c0af343c55f3e163e7715d28b72c7fbe4
David Turner <djturner@umbc.edu>
2026-07-27 14:19:16 -0400

    Made sure check_telescope_choices has the correct type hints.
    Signed-off-by: David Turner <djturner@umbc.edu>

--- xga/sources/general.py
+++ xga/sources/general.py
@@ -1,5 +1,5 @@
 #  This code is part of X-ray: Generate and Analyse (XGA), a module designed for the XMM Cluster Survey (XCS).
-#  Last modified by David J Turner (djturner@umbc.edu) 7/27/26, 1:04 PM. Copyright (c) The Contributors.
+#  Last modified by David J Turner (djturner@umbc.edu) 7/27/26, 1:52 PM. Copyright (c) The Contributors.
 """
 This module defines the generic PointSource and ExtendedSource classes, which are usable by themselves, but more
 importantly exist to be inherited by classes representing specific astrophysical objects (e.g. galaxy clusters).
@@ -319,7 +319,7 @@ class ExtendedSource(BaseSource):
         :return: A numpy array of the positions of points of the chosen cluster (not 'galaxy cluster',
             a cluster of points). A list of numpy arrays with the same information for all the other clusters
             that were found
-        :rtype: Tuple[ndarray, List[ndarray]]
+        :rtype: tuple[dict[str, ndarray], dict[str, list[ndarray]]]
         """
         return self._chosen_peak_cluster, self._other_peak_clusters
 
